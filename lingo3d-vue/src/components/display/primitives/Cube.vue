caf67c32d5298282f8ff8e3e8e891909bb72c098
Xz33M Hask <pecevgligor@protonmail.com>
2022-04-07 14:35:01 +0200

    camera props

--- lingo3d-vue/src/components/display/primitives/Cube.vue
+++ lingo3d-vue/src/components/display/primitives/Cube.vue
@@ -1,46 +1,44 @@
 <script setup lang="ts">
-import { Cube } from "lingo3d";
-import IPrimitive, { primitiveDefaults } from "lingo3d/lib/interface/IPrimitive";
+import { Cube } from "lingo3d"
+import IPrimitive, { primitiveDefaults } from "lingo3d/lib/interface/IPrimitive"
 import useManager from "../../../hooks/useManager"
 
 const options = {
-    x: {
-        type: Number,
-        default: 0
-    },
-    y: {
-        type: Number,
-        default: 0
-    },
-    z: {
-        type: Number,
-        default: 0
-    },
-    rotationX: {
-        type: Number,
-        default: 0
-    },
-    rotationY: {
-        type: Number,
-        default: 0
-    },
-    rotationZ: {
-        type: Number,
-        default: 0
-    },
-    color: {
-        type: String,
-        default: "#ffffff"
-    }
+  x: {
+    type: Number,
+    default: 0,
+  },
+  y: {
+    type: Number,
+    default: 0,
+  },
+  z: {
+    type: Number,
+    default: 0,
+  },
+  rotationX: {
+    type: Number,
+    default: 0,
+  },
+  rotationY: {
+    type: Number,
+    default: 0,
+  },
+  rotationZ: {
+    type: Number,
+    default: 0,
+  },
+  color: {
+    type: String,
+    default: "#ffffff",
+  },
 }
 
-const props = defineProps<Partial<IPrimitive>>()
+// const props = defineProps<Partial<IPrimitive>>()
 
-
-
-useManager(props, Cube)
+// useManager(props, Cube)
 </script>
 
 <template>
-    <slot />
+  <slot />
 </template>
--- lingo3d-vue/src/props/managerProps.ts
+++ lingo3d-vue/src/props/managerProps.ts
@@ -1,14 +1,34 @@
 import { objectManagerDefaults } from "lingo3d/lib/interface/IObjectManager"
 
 const props = {
-    x: {
-        type: Number,
-        default: objectManagerDefaults.x
-    },
-    y: {
-        type: Number,
-        default: objectManagerDefaults.y
-    }
+  innerRotationX: {
+    type: Number,
+    default: objectManagerDefaults.innerRotationX,
+  },
+  innerRotationY: {
+    type: Number,
+    default: objectManagerDefaults.innerRotationY,
+  },
+  innerRotationZ: {
+    type: Number,
+    default: objectManagerDefaults.innerRotationZ,
+  },
+  innerRotation: {
+    type: Number,
+    default: objectManagerDefaults.innerRotation,
+  },
+  innerX: {
+    type: Number,
+    default: objectManagerDefaults.innerX,
+  },
+  innerY: {
+    type: Number,
+    default: objectManagerDefaults.innerY,
+  },
+  innerZ: {
+    type: Number,
+    default: objectManagerDefaults.innerZ,
+  },
 }
 
 export default props
