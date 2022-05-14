6f46fe809a13a9c4998f3ecc48cdc3cb3cc3d43b
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-05-14 18:46:57 +0800

    improved onKeyClear

--- lingo3d/src/events/onKeyClear.ts
+++ lingo3d/src/events/onKeyClear.ts
@@ -8,14 +8,25 @@ window.addEventListener("focus", () => emitKeyClear())
 document.addEventListener("visibilitychange", () => emitKeyClear())
 
 const isPressed = new Set<string>()
+onKeyClear(() => isPressed.clear())
+
+let queueMouseDownClear = false
 document.addEventListener("keydown", e => {
     if (e.key === "Shift" || e.key === "Meta")
         isPressed.add(e.key)
         
-    if (isPressed.has("Meta") && isPressed.has("Shift"))
+    if (isPressed.has("Meta") && isPressed.has("Shift")) {
         emitKeyClear()
+        queueMouseDownClear = true
+    }
 })
 document.addEventListener("keyup", e => {
     if (e.key === "Shift" || e.key === "Meta")
         isPressed.delete(e.key)
+})
+document.addEventListener("mousedown", () => {
+    if (queueMouseDownClear) {
+        queueMouseDownClear = false
+        emitKeyClear()
+    }
 })
