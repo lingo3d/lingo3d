3e5105ddb191d8617443fa65a6a1e58bd08ad842
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-05-14 15:45:13 +0800

    fixed multiple selection circular dependency

--- lingo3d/src/display/Group.ts
+++ lingo3d/src/display/Group.ts
@@ -1,7 +1,16 @@
-import { Group as ThreeGroup } from "three"
+import { createEffect } from "@lincode/reactivity"
+import { Group as ThreeGroup, Object3D } from "three"
+import scene from "../engine/scene"
+import { onEditorGroupItems } from "../events/onEditorGroupItems"
+import { emitSelectionTarget } from "../events/onSelectionTarget"
 import IGroup, { groupDefaults, groupSchema } from "../interface/IGroup"
+import { getMultipleSelectionEnabled } from "../states/useMultipleSelectionEnabled"
+import { getMultipleSelectionTargets, multipleSelectionGroupManagers } from "../states/useMultipleSelectionTargets"
+import { setSelectionTarget } from "../states/useSelectionTarget"
 import ObjectManager from "./core/ObjectManager"
+import SimpleObjectManager from "./core/SimpleObjectManager"
 import fitContent from "./utils/fitContent"
+import { box3, vector3 } from "./utils/reusables"
 
 export default class Group extends ObjectManager<ThreeGroup> implements IGroup {
     public static componentName = "group"
@@ -17,3 +26,56 @@ export default class Group extends ObjectManager<ThreeGroup> implements IGroup {
         fitContent(this)
     }
 }
+
+createEffect(() => {
+    const enabled = getMultipleSelectionEnabled()
+    const targets = getMultipleSelectionTargets()
+    if (!targets.length || !enabled) return
+    
+    const group = new ThreeGroup()
+    scene.add(group)
+    
+    const groupManager = new SimpleObjectManager(group)
+    multipleSelectionGroupManagers.add(groupManager)
+    setSelectionTarget(groupManager)
+    
+    const parentEntries: Array<[Object3D, Object3D]> = []
+    for (const { outerObject3d: target } of targets) {
+        if (!target.parent) continue
+        parentEntries.push([target, target.parent])
+        group.attach(target)
+    }
+
+    box3.setFromObject(group)
+    for (const [object, parent] of parentEntries)
+        parent.attach(object)
+
+    group.position.copy(box3.getCenter(vector3))
+    for (const [object] of parentEntries)
+        group.attach(object)
+
+    let consolidated = false
+    const handle = onEditorGroupItems(() => {
+        if (!targets.length || consolidated) return
+        consolidated = true
+
+        const consolidatedGroup = new Group()
+        consolidatedGroup.outerObject3d.position.copy(group.position)
+        for (const target of targets)
+            consolidatedGroup.attach(target)
+
+        emitSelectionTarget(consolidatedGroup)
+    })
+
+    return () => {
+        emitSelectionTarget(undefined)
+
+        if (!groupManager.done && !consolidated)
+            for (const [object, parent] of parentEntries)
+                parent.attach(object)
+
+        groupManager.dispose()
+        scene.remove(group)
+        handle.cancel()
+    }
+}, [getMultipleSelectionTargets, getMultipleSelectionEnabled])
--- lingo3d/src/states/useMultipleSelectionTargets.ts
+++ lingo3d/src/states/useMultipleSelectionTargets.ts
@@ -1,14 +1,6 @@
-import store, { createEffect, pull, push, reset } from "@lincode/reactivity"
-import { Group as ThreeGroup, Object3D } from "three"
-import Appendable from "../api/core/Appendable"
-import SimpleObjectManager from "../display/core/SimpleObjectManager"
-import Group from "../display/Group"
-import { box3, vector3 } from "../display/utils/reusables"
-import scene from "../engine/scene"
-import { onEditorGroupItems } from "../events/onEditorGroupItems"
-import { emitSelectionTarget } from "../events/onSelectionTarget"
-import { getMultipleSelectionEnabled } from "./useMultipleSelectionEnabled"
-import { setSelectionTarget } from "./useSelectionTarget"
+import store, { pull, push, reset } from "@lincode/reactivity"
+import type Appendable from "../api/core/Appendable"
+import type SimpleObjectManager from "../display/core/SimpleObjectManager"
 
 export const [setMultipleSelectionTargets, getMultipleSelectionTargets] = store<Array<SimpleObjectManager>>([])
 
@@ -17,56 +9,3 @@ export const pullMultipleSelectionTargets = pull(setMultipleSelectionTargets, ge
 export const resetMultipleSelectionTargets = reset(setMultipleSelectionTargets, getMultipleSelectionTargets)
 
 export const multipleSelectionGroupManagers = new WeakSet<Appendable>()
-
-createEffect(() => {
-    const enabled = getMultipleSelectionEnabled()
-    const targets = getMultipleSelectionTargets()
-    if (!targets.length || !enabled) return
-    
-    const group = new ThreeGroup()
-    scene.add(group)
-    
-    const groupManager = new SimpleObjectManager(group)
-    multipleSelectionGroupManagers.add(groupManager)
-    setSelectionTarget(groupManager)
-    
-    const parentEntries: Array<[Object3D, Object3D]> = []
-    for (const { outerObject3d: target } of targets) {
-        if (!target.parent) continue
-        parentEntries.push([target, target.parent])
-        group.attach(target)
-    }
-
-    box3.setFromObject(group)
-    for (const [object, parent] of parentEntries)
-        parent.attach(object)
-
-    group.position.copy(box3.getCenter(vector3))
-    for (const [object] of parentEntries)
-        group.attach(object)
-
-    let consolidated = false
-    const handle = onEditorGroupItems(() => {
-        if (!targets.length || consolidated) return
-        consolidated = true
-
-        const consolidatedGroup = new Group()
-        consolidatedGroup.outerObject3d.position.copy(group.position)
-        for (const target of targets)
-            consolidatedGroup.attach(target)
-
-        emitSelectionTarget(consolidatedGroup)
-    })
-
-    return () => {
-        emitSelectionTarget(undefined)
-
-        if (!groupManager.done && !consolidated)
-            for (const [object, parent] of parentEntries)
-                parent.attach(object)
-
-        groupManager.dispose()
-        scene.remove(group)
-        handle.cancel()
-    }
-}, [getMultipleSelectionTargets, getMultipleSelectionEnabled])
