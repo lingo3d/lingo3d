aa4a047839e469d0a40bc2cd280504adb9e182c1
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-05-25 14:57:48 +0800

    added contextMenu buttons

--- lingo3d/src/editor/ContextMenu/index.tsx
+++ lingo3d/src/editor/ContextMenu/index.tsx
@@ -1,17 +1,42 @@
 import { preventTreeShake } from "@lincode/utils"
 import { h } from "preact"
 import register from "preact-custom-element"
-import { useEffect } from "preact/hooks"
-import { container } from "../../engine/renderLoop/renderSetup"
+import { useEffect, useState } from "preact/hooks"
+import { mouseEvents } from "../../api/mouse"
+import { onSelectionTarget } from "../../events/onSelectionTarget"
 
 preventTreeShake(h)
 
 const ContextMenu = () => {
+    const [pos, setPos] = useState<{ x: number, y: number } | undefined>(undefined)
+
     useEffect(() => {
+        let [clientX, clientY] = [0, 0]
+        const cb = (e: MouseEvent) => [clientX, clientY] = [e.clientX, e.clientY]
+        document.addEventListener("mousemove", cb)
+
+        const handle0 = mouseEvents.on("down", () => setPos(undefined))
+
+        const handle = onSelectionTarget(({ target, rightClick }) => {
+            rightClick && setPos({ x: clientX, y: clientY })
+        })
+        return () => {
+            handle0.cancel()
+            handle.cancel()
+            document.removeEventListener("mousemove", cb)
+        }
     }, [])
 
+    if (!pos) return null
+
     return (
-        <div></div>
+        <div
+         className="lingo3d-ui"
+         style={{ position: "absolute", left: pos.x, top: pos.y, zIndex: 9999, background: "rgb(40, 41, 46)", padding: 6 }}
+        >
+            <div style={{ padding: 6 }}>inspect</div>
+            <div style={{ padding: 6 }}>show in SceneGraph</div>
+        </div>
     )
 }
 
