87b6859b69f01a1a9dc61f5b99d6b725284e0f64
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-06-03 01:23:13 +0800

    updated CharacterCamera append and attach mechanism

--- lingo3d/src/display/core/CharacterCamera.ts
+++ lingo3d/src/display/core/CharacterCamera.ts
@@ -3,6 +3,7 @@ import { Cancellable } from "@lincode/promiselikes"
 import { Reactive } from "@lincode/reactivity"
 import { debounce } from "@lincode/utils"
 import { Quaternion } from "three"
+import Appendable from "../../api/core/Appendable"
 import PositionedItem from "../../api/core/PositionedItem"
 import { loop } from "../../engine/eventLoop"
 import scene from "../../engine/scene"
@@ -14,6 +15,8 @@ import { getTransformControlsMode } from "../../states/useTransformControlsMode"
 import Camera from "../cameras/Camera"
 import { euler, quaternion, quaternion_ } from "../utils/reusables"
 import MeshItem, { isMeshItem } from "./MeshItem"
+
+const attachSet = new WeakSet<Appendable>()
 
 export default class CharacterCamera extends Camera implements ICharacterCamera {
     public static override defaults = characterCameraDefaults
@@ -102,26 +105,31 @@ export default class CharacterCamera extends Camera implements ICharacterCamera 
 
     private retarget = debounce(() => {
         let target: MeshItem | undefined
-        for (const child of this.children ?? []) {
+        for (const child of this.children ?? [])
             if (target) {
                 if (child.outerObject3d.parent !== this.camera)
-                    this.camera.attach(child.outerObject3d)
+                    this.camera[attachSet.has(child) ? "attach" : "add"](child.outerObject3d)
             }
-            else if (isMeshItem(child))
+            else if (isMeshItem(child)) {
                 target = child
-        }
-        target && this.outerObject3d.parent?.attach(target.outerObject3d)
+                const { parent } = this.outerObject3d
+                if (parent && child.outerObject3d.parent !== parent)
+                    parent[attachSet.has(target) ? "attach" : "add"](target.outerObject3d)
+            }
+
         this.targetState.set(target)
 
     }, 0, "trailing")
 
     public override append(object: PositionedItem) {
-        super.append(object)
+        this._append(object)
+        attachSet.delete(object)
         this.retarget()
     }
     
     public override attach(object: PositionedItem) {
-        super.attach(object)
+        this._append(object)
+        attachSet.add(object)
         this.retarget()
     }
     
