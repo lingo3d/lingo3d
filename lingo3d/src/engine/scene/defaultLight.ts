a44d0356d8f337bbbcc7e476deca8afb9e39e584
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-06-04 04:57:45 +0800

    in progress: studio environment

--- lingo3d/src/engine/scene/defaultLight.ts
+++ lingo3d/src/engine/scene/defaultLight.ts
@@ -1,7 +1,5 @@
-import { Cancellable } from "@lincode/promiselikes"
 import { createEffect } from "@lincode/reactivity"
 import { HemisphereLight, DirectionalLight, EquirectangularReflectionMapping } from "three"
-import { appendableRoot } from "../../api/core/Appendable"
 import loadTexture from "../../display/utils/loaders/loadTexture"
 import { getDefaultLight } from "../../states/useDefaultLight"
 import { getDefaultLightScale } from "../../states/useDefaultLightScale"
@@ -10,28 +8,13 @@ import scene from "./scene"
 export default {}
 
 createEffect(() => {
-    const defaultLight = getDefaultLight()
+    let defaultLight = getDefaultLight()
     if (!defaultLight) return
 
     if (typeof defaultLight === "string" && defaultLight !== "default") {
-        if (defaultLight === "studio") {
-            const handle = new Cancellable()
-            import("../../display/lights/BoxLight").then(({ default: BoxLight }) => {
-                if (handle.done) return
+        if (defaultLight === "studio")
+            defaultLight = "https://unpkg.com/lingo3d-textures@1.0.0/assets/studio.jpg"
 
-                const light = new BoxLight()
-                appendableRoot.delete(light)
-                handle.then(() => light.dispose())
-
-                handle.watch(getDefaultLightScale(scale => {
-                    light.area = scale
-                    light.scale = scale
-                }))
-            })
-            return () => {
-                handle.cancel()
-            }
-        }
         let proceed = true
         const texture = loadTexture(defaultLight, () => proceed && (scene.environment = texture))
         texture.mapping = EquirectangularReflectionMapping
