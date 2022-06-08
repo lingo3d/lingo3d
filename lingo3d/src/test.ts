b3afc3e8bbb1c519cc8efa66b6492cae2384d80e
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-06-08 19:36:27 +0800

    updated dependencies

--- lingo3d/src/test.ts
+++ lingo3d/src/test.ts
@@ -1,19 +1,19 @@
 import index from "."
-import test from "./tests/testBVH"
+import test from "./tests/testIK"
 import { preventTreeShake } from "@lincode/utils"
 import settings from "./api/settings"
 import "./editor"
 
 preventTreeShake([index, test])
 
-settings.autoMount = true
+settings.autoMount = "#world"
 
-// document.body.innerHTML = `
-//     <div style="width: 100%; height: 100%; position: absolute; left: 0px; top: 0px; display: flex">
-//         <lingo3d-toolbar></lingo3d-toolbar>
-//         <lingo3d-scenegraph></lingo3d-scenegraph>
-//         <lingo3d-editor></lingo3d-editor>
-//         <lingo3d-library></lingo3d-library>
-//         <div id="world" style="height: 100%; flex-grow: 1; position: relative"></div>
-//     </div>
-// `
+document.body.innerHTML = `
+    <div style="width: 100%; height: 100%; position: absolute; left: 0px; top: 0px; display: flex">
+        <lingo3d-toolbar></lingo3d-toolbar>
+        <lingo3d-scenegraph></lingo3d-scenegraph>
+        <lingo3d-editor></lingo3d-editor>
+        <lingo3d-library></lingo3d-library>
+        <div id="world" style="height: 100%; flex-grow: 1; position: relative"></div>
+    </div>
+`
