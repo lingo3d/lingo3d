bd01244f2e7873ca016df9753d8c6023df8875c5
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-06-12 23:01:12 +0800

    update vite config

--- lingo3d/vite.config.ts
+++ lingo3d/vite.config.ts
@@ -3,11 +3,5 @@ import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
-  build: {
-    lib: {
-      entry: 'src/index.ts',
-      formats: ["es"]
-    }
-  }
+  plugins: [preact()]
 })
