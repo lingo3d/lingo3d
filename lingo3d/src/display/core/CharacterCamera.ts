96508bec36a70e6a74d2de7cd6232323684097df
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-06-24 21:40:34 +0800

    better OrbitCamera

--- lingo3d/src/display/core/CharacterCamera.ts
+++ lingo3d/src/display/core/CharacterCamera.ts
@@ -1,8 +1,4 @@
-import { Reactive } from "@lincode/reactivity"
-import { debounce } from "@lincode/utils"
 import { PerspectiveCamera } from "three"
-import Appendable from "../../api/core/Appendable"
-import PositionedItem from "../../api/core/PositionedItem"
 import { camNear, camFar } from "../../engine/constants"
 import scene from "../../engine/scene"
 import { onBeforeRender } from "../../events/onBeforeRender"
@@ -11,13 +7,11 @@ import ICharacterCamera, { characterCameraDefaults, characterCameraSchema, LockT
 import { getSelectionTarget } from "../../states/useSelectionTarget"
 import { getTransformControlsDragging } from "../../states/useTransformControlsDragging"
 import { getTransformControlsMode } from "../../states/useTransformControlsMode"
+import OrbitCameraBase from "../cameras/OrbitCamera/OrbitCameraBase"
 import { euler, quaternion } from "../utils/reusables"
-import CameraBase from "./CameraBase"
-import MeshItem, { isMeshItem } from "./MeshItem"
+import MeshItem from "./MeshItem"
 
-const attachSet = new WeakSet<Appendable>()
-
-export default class CharacterCamera extends CameraBase<PerspectiveCamera> implements ICharacterCamera {
+export default class CharacterCamera extends OrbitCameraBase implements ICharacterCamera {
     public static defaults = characterCameraDefaults
     public static schema = characterCameraSchema
 
@@ -35,11 +29,6 @@ export default class CharacterCamera extends CameraBase<PerspectiveCamera> imple
             if ("frustumCulled" in target)
                 target.frustumCulled = false
 
-            const handle = onSceneGraphChange(() => target.parent !== this && this.retarget())
-            
-            return () => {
-                handle.cancel()
-            }
         }, [this.targetState.get])
 
         const followTarget = (target: MeshItem, slerp: boolean) => {
@@ -125,36 +114,4 @@ export default class CharacterCamera extends CameraBase<PerspectiveCamera> imple
     }
 
     public lockTargetRotation: LockTargetRotationValue = true
-
-    protected targetState = new Reactive<MeshItem | undefined>(undefined)
-
-    private retarget = debounce(() => {
-        let target: MeshItem | undefined
-        for (const child of this.children ?? [])
-            if (target) {
-                if (child.outerObject3d.parent !== this.camera)
-                    this.camera[attachSet.has(child) ? "attach" : "add"](child.outerObject3d)
-            }
-            else if (isMeshItem(child)) {
-                target = child
-                const { parent } = this.outerObject3d
-                if (parent && child.outerObject3d.parent !== parent)
-                    parent[attachSet.has(target) ? "attach" : "add"](target.outerObject3d)
-            }
-
-        this.targetState.set(target)
-
-    }, 0, "trailing")
-
-    public override append(object: PositionedItem) {
-        this._append(object)
-        attachSet.delete(object)
-        this.retarget()
-    }
-    
-    public override attach(object: PositionedItem) {
-        this._append(object)
-        attachSet.add(object)
-        this.retarget()
-    }
 }
