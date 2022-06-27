a5e79af9e5fab5b5be05be59bc94d67b44c81129
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-06-28 05:09:13 +0800

    removed Loaded addToRaycastSet override

--- lingo3d/src/tests/testIK.ts
+++ lingo3d/src/tests/testIK.ts
@@ -7,33 +7,17 @@ export default {}
 const player = new Dummy()
 player.id = "player"
 player.src = "player2.glb"
-player.boxVisible = true
-
-const dummy = new Dummy()
-dummy.x = 100
-dummy.id = "dummy"
-dummy.boxVisible = true
 
 const cam = new Camera()
 cam.z = 500
 cam.active = true
-cam.id = "hello"
 
 const cam0 = new OrbitCamera()
 cam0.targetId = "player"
 cam0.innerZ = 200
 cam0.transition = true
 
-const cam1 = new OrbitCamera()
-cam1.targetId = "dummy"
-cam1.innerZ = 200
-cam1.transition = true
-
 player.onClick = () => {
     cam0.activate()
     console.log("player")
-}
-
-dummy.onClick = () => {
-    cam1.activate()
 }
