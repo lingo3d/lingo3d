23130fd57b8a9dcba8bc9f3b3d4065aa8915e68d
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-07-18 19:21:08 +0800

    updates

--- lingo3d/src/display/lights/AreaLight.ts
+++ lingo3d/src/display/lights/AreaLight.ts
@@ -1,6 +1,9 @@
 import { Color, RectAreaLight } from "three"
 import { RectAreaLightHelper } from "three/examples/jsm/helpers/RectAreaLightHelper"
-import IAreaLight, { areaLightDefaults, areaLightSchema } from "../../interface/IAreaLight"
+import IAreaLight, {
+    areaLightDefaults,
+    areaLightSchema
+} from "../../interface/IAreaLight"
 import { lazy } from "@lincode/utils"
 import ObjectManager from "../core/ObjectManager"
 import mainCamera from "../../engine/mainCamera"
@@ -13,7 +16,9 @@ import { getSelectionTarget } from "../../states/useSelectionTarget"
 import { getCameraRendered } from "../../states/useCameraRendered"
 
 const lazyInit = lazy(async () => {
-    const { RectAreaLightUniformsLib } = await import("three/examples/jsm/lights/RectAreaLightUniformsLib.js")
+    const { RectAreaLightUniformsLib } = await import(
+        "three/examples/jsm/lights/RectAreaLightUniformsLib.js"
+    )
     RectAreaLightUniformsLib.init()
 })
 
@@ -27,23 +32,25 @@ export default class AreaLight extends ObjectManager implements IAreaLight {
     public constructor() {
         super()
 
-        ;(async () => {
-            await lazyInit()
-
+        lazyInit().then(() => {
             if (this.done) return
 
-            const light = this.light = new RectAreaLight(
+            const light = (this.light = new RectAreaLight(
                 this._color,
                 this._intensity,
                 this.width * this.scaleX * scaleDown,
                 this.height * this.scaleY * scaleDown
-            )
+            ))
             this.object3d.add(light)
 
             this.then(() => light.dispose())
 
             this.createEffect(() => {
-                if (getTransformControlsMode() !== "scale" || getSelectionTarget() !== this) return
+                if (
+                    getTransformControlsMode() !== "scale" ||
+                    getSelectionTarget() !== this
+                )
+                    return
 
                 const handle = onTransformControls(() => {
                     const { x, y } = this.outerObject3d.scale
@@ -54,19 +61,23 @@ export default class AreaLight extends ObjectManager implements IAreaLight {
                     handle.cancel()
                 }
             }, [getTransformControlsMode, getSelectionTarget])
-            
+
             this.createEffect(() => {
-                if (getCameraRendered() !== mainCamera || !this.helperState.get()) return
-    
+                if (
+                    getCameraRendered() !== mainCamera ||
+                    !this.helperState.get()
+                )
+                    return
+
                 const helper = new RectAreaLightHelper(light)
                 scene.add(helper)
-    
+
                 return () => {
                     helper.dispose()
                     scene.remove(helper)
                 }
             }, [getCameraRendered, this.helperState.get])
-        })()
+        })
     }
 
     public shadowResolution = 512
@@ -136,11 +147,9 @@ export default class AreaLight extends ObjectManager implements IAreaLight {
     public override get depth() {
         return 0
     }
-    public override set depth(_) {
-    }
+    public override set depth(_) {}
     public override get scaleZ() {
         return 0
     }
-    public override set scaleZ(_) {
-    }
+    public override set scaleZ(_) {}
 }
--- lingo3d/src/engine/eventLoop.ts
+++ lingo3d/src/engine/eventLoop.ts
@@ -3,16 +3,15 @@ import { Clock } from "three"
 import { getPaused } from "../states/usePaused"
 import { getRenderer } from "../states/useRenderer"
 
-let paused = getPaused()
-getPaused(val => paused = val)
+let paused = false
+getPaused((val) => (paused = val))
 
 export const timer = (time: number, repeat: number, cb: () => void) => {
     let count = 0
     const handle = setInterval(() => {
         if (document.hidden || paused) return
         cb()
-        if (repeat !== -1 && ++count >= repeat)
-            clearInterval(handle)
+        if (repeat !== -1 && ++count >= repeat) clearInterval(handle)
     }, time)
     return new Cancellable(() => clearInterval(handle))
 }
@@ -21,16 +20,17 @@ let count = 0
 const callbacks = new Set<() => void>()
 const clock = new Clock()
 
-getRenderer(renderer => {
+getRenderer((renderer) => {
     renderer?.setAnimationLoop(() => {
         const fps = 1 / clock.getDelta()
 
-        if (paused || ++count < Math.round(fps / 60)) return
+        if (++count < Math.round(fps / 60)) return
         count = 0
 
-        for (const cb of callbacks)
-            cb()
-    })  
+        if (paused) return
+
+        for (const cb of callbacks) cb()
+    })
 })
 
 export const loop = (cb: () => void) => {
