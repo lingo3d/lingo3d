a2b224cbeac566e0fbc6c95de9452ba34b59798f
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-07-21 16:19:42 +0800

    updates

--- lingo3d-vue/src/App.vue
+++ lingo3d-vue/src/App.vue
@@ -1,13 +1,31 @@
 <script setup lang="ts">
-import { World, Cube, ThirdPersonCamera } from "./index"
-import LingoEditor from "./components/editor/LingoEditor.vue";
+import { World, Model, LingoEditor, Setup, DirectionalLight } from "./index"
 </script>
 
 <template>
   <World>
-    <ThirdPersonCamera active>
-      <Cube :animation="{ rotationY: [0, 180, 360] }" />
-    </ThirdPersonCamera>
+    <Model
+      :x="10588.61"
+      :y="9758.37"
+      :z="-14228.96"
+      :width="128.53"
+      :height="100"
+      :depth="174.43"
+      :scale-x="200"
+      :scale-y="200"
+      :scale-z="200"
+      src="afterrain.glb"
+      :roughness-factor="0"
+      :metalness-factor="1"
+    />
+    <DirectionalLight :x="-1000" :y="1000" :z="1000" />
+    <DirectionalLight
+      :x="-1000"
+      :y="1000"
+      :z="1000"
+      :shadow-distance="5000"
+    />
     <LingoEditor />
+    <Setup default-light="env.hdr" skybox="env.hdr" :exposure="0.6" ambient-occlusion />
   </World>
 </template>
