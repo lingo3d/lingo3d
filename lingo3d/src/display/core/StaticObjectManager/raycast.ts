94a91d5463d1b191ef50417999be8d5659b4cf72
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-08-04 19:05:46 +0800

    refactored raycast

--- lingo3d/src/display/core/StaticObjectManager/raycast.ts
+++ lingo3d/src/display/core/StaticObjectManager/raycast.ts
@@ -23,7 +23,6 @@ import {
 } from "../../../events/onSelectionTarget"
 import { LingoMouseEvent } from "../../../interface/IMouse"
 import { scaleUp } from "../../../engine/constants"
-import { Cancellable } from "@lincode/promiselikes"
 import { vec2Point } from "../../utils/vec2Point"
 import { getTransformControlsDragging } from "../../../states/useTransformControlsDragging"
 import { debounce } from "@lincode/utils"
@@ -96,56 +95,6 @@ export const mouseOverSet = new Set<Object3D>()
 export const mouseOutSet = new Set<Object3D>()
 export const mouseMoveSet = new Set<Object3D>()
 
-const enableMouseEvents = () => {
-    const handle = new Cancellable()
-
-    handle.watch(pickable("click", clickSet, (obj, e) => obj.onClick?.(e)))
-    handle.watch(
-        pickable("down", mouseDownSet, (obj, e) => obj.onMouseDown?.(e))
-    )
-    handle.watch(pickable("up", mouseUpSet, (obj, e) => obj.onMouseUp?.(e)))
-
-    let moveSet = new Set<StaticObjectManager>()
-    let moveSetOld = new Set<StaticObjectManager>()
-
-    handle.watch(
-        pickable("move", mouseOverSet, (obj, e) => {
-            moveSet.add(obj)
-            obj.outerObject3d.userData.eMove = e
-        })
-    )
-    handle.watch(
-        pickable("move", mouseOutSet, (obj, e) => {
-            moveSet.add(obj)
-            obj.outerObject3d.userData.eMove = e
-        })
-    )
-    handle.watch(
-        pickable("move", mouseMoveSet, (obj, e) => {
-            moveSet.add(obj)
-            obj.outerObject3d.userData.eMove = e
-        })
-    )
-    handle.watch(
-        mouseEvents.on("move", () => {
-            for (const obj of moveSet) {
-                if (!moveSetOld.has(obj))
-                    obj.onMouseOver?.(obj.outerObject3d.userData.eMove)
-
-                obj.onMouseMove?.(obj.outerObject3d.userData.eMove)
-            }
-
-            for (const obj of moveSetOld)
-                if (!moveSet.has(obj))
-                    obj.onMouseOut?.(obj.outerObject3d.userData.eMove)
-
-            moveSetOld = moveSet
-            moveSet = new Set()
-        })
-    )
-    return handle
-}
-
 createEffect(() => {
     const multipleSelection = getMultipleSelection()
     const firstMultipleSelection = createRef(true)
@@ -158,10 +107,54 @@ createEffect(() => {
         resetMultipleSelectionTargets()
         setSelectionTarget(undefined)
 
-        const handle = enableMouseEvents()
+        const handle0 = pickable("click", clickSet, (obj, e) =>
+            obj.onClick?.(e)
+        )
+        const handle1 = pickable("down", mouseDownSet, (obj, e) =>
+            obj.onMouseDown?.(e)
+        )
+        const handle2 = pickable("up", mouseUpSet, (obj, e) =>
+            obj.onMouseUp?.(e)
+        )
+
+        let moveSet = new Set<StaticObjectManager>()
+        let moveSetOld = new Set<StaticObjectManager>()
+
+        const handle3 = pickable("move", mouseOverSet, (obj, e) => {
+            moveSet.add(obj)
+            obj.outerObject3d.userData.eMove = e
+        })
+        const handle4 = pickable("move", mouseOutSet, (obj, e) => {
+            moveSet.add(obj)
+            obj.outerObject3d.userData.eMove = e
+        })
+        const handle5 = pickable("move", mouseMoveSet, (obj, e) => {
+            moveSet.add(obj)
+            obj.outerObject3d.userData.eMove = e
+        })
+        const handle6 = mouseEvents.on("move", () => {
+            for (const obj of moveSet) {
+                if (!moveSetOld.has(obj))
+                    obj.onMouseOver?.(obj.outerObject3d.userData.eMove)
+
+                obj.onMouseMove?.(obj.outerObject3d.userData.eMove)
+            }
+            for (const obj of moveSetOld)
+                if (!moveSet.has(obj))
+                    obj.onMouseOut?.(obj.outerObject3d.userData.eMove)
+
+            moveSetOld = moveSet
+            moveSet = new Set()
+        })
 
         return () => {
-            handle.cancel()
+            handle0.cancel()
+            handle1.cancel()
+            handle2.cancel()
+            handle3.cancel()
+            handle4.cancel()
+            handle5.cancel()
+            handle6.cancel()
         }
     }
 
@@ -218,8 +211,6 @@ createEffect(() => {
                 : target
         )
     })
-    !multipleSelection && handle0.watch(enableMouseEvents())
-
     return () => {
         handle0.cancel()
         handle1.cancel()
