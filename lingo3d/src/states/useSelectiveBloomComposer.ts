1edb1aa86853344e8cb5dbe5956c95799995411b
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-08-24 00:14:52 +0800

    updates

--- lingo3d/src/states/useSelectiveBloomComposer.ts
+++ lingo3d/src/states/useSelectiveBloomComposer.ts
@@ -1,5 +1,6 @@
 import store, { createEffect } from "@lincode/reactivity"
 import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer"
+import { getPixelRatio } from "./usePixelRatio"
 import { getRenderer } from "./useRenderer"
 import { getResolution } from "./useResolution"
 
@@ -23,5 +24,5 @@ createEffect(() => {
 
     const [w, h] = getResolution()
     selectiveBloomComposer.setSize(w, h)
-    selectiveBloomComposer.setPixelRatio(1)
-}, [getSelectiveBloomComposer, getResolution])
+    selectiveBloomComposer.setPixelRatio(Math.min(getPixelRatio(), 1))
+}, [getSelectiveBloomComposer, getResolution, getPixelRatio])
