b1135b6c03e6e0fce9427928363c1223e4544d78
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-09-10 14:57:05 +0800

    hotkeys hook

---
+++ lingo3d/src/editor/Editor/useHotkeys.ts
@@ -0,0 +1,73 @@
+import { useEffect } from "react"
+import { isPositionedItem } from "../../api/core/PositionedItem"
+import deserialize from "../../api/serializer/deserialize"
+import serialize from "../../api/serializer/serialize"
+import applyCentripetalQuaternion from "../../display/utils/applyCentripetalQuaternion"
+import { emitEditorCenterView } from "../../events/onEditorCenterView"
+import { onKeyClear } from "../../events/onKeyClear"
+import { emitSelectionTarget } from "../../events/onSelectionTarget"
+import { getCentripetal } from "../../states/useCentripetal"
+import {
+    setMultipleSelection,
+    getMultipleSelection
+} from "../../states/useMultipleSelection"
+import { getMultipleSelectionTargets } from "../../states/useMultipleSelectionTargets"
+import { getSelectionTarget } from "../../states/useSelectionTarget"
+import { setTransformControlsSpace } from "../../states/useTransformControlsSpace"
+import deleteSelected from "./deleteSelected"
+
+export default () => {
+    useEffect(() => {
+        const handleKeyDown = (e: KeyboardEvent) => {
+            if (e.key === "Shift" || e.key === "Meta" || e.key === "Control")
+                setMultipleSelection(true)
+
+            if (e.key === "Backspace" || e.key === "Delete") {
+                e.preventDefault()
+                !getMultipleSelection() && deleteSelected()
+                return
+            }
+
+            const keyLowerCase = e.key.toLocaleLowerCase()
+            const target = getSelectionTarget()
+
+            if (e.metaKey || e.ctrlKey) {
+                e.preventDefault()
+
+                if (keyLowerCase === "s") {
+                    //mark
+                } else if (target) {
+                    if (keyLowerCase === "c") {
+                        const targets = getMultipleSelectionTargets()
+                        if (targets.length) {
+                            //todo: copy multiple
+                        } else {
+                            const [item] = deserialize(serialize(target))
+                            if (target.parent && item) {
+                                target.parent.attach(item)
+                                emitSelectionTarget(item)
+                            }
+                        }
+                    } else if (e.key === "ArrowUp" && getCentripetal()) {
+                        applyCentripetalQuaternion(target)
+                        setTransformControlsSpace("local")
+                    }
+                }
+            } else if (keyLowerCase === "c")
+                isPositionedItem(target) && emitEditorCenterView(target)
+        }
+        const handleKeyUp = (e: KeyboardEvent) => {
+            if (e.key === "Shift" || e.key === "Meta" || e.key === "Control")
+                setMultipleSelection(false)
+        }
+        document.addEventListener("keydown", handleKeyDown)
+        document.addEventListener("keyup", handleKeyUp)
+        const handle1 = onKeyClear(() => setMultipleSelection(false))
+
+        return () => {
+            document.removeEventListener("keydown", handleKeyDown)
+            document.removeEventListener("keyup", handleKeyUp)
+            handle1.cancel()
+        }
+    }, [])
+}
