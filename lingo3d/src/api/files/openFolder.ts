876bc2665a970309e1e670236aed0b94e7c557cc
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-09-12 19:53:54 +0800

    added scene file scanning

--- lingo3d/src/api/files/openFolder.ts
+++ lingo3d/src/api/files/openFolder.ts
@@ -1,6 +1,9 @@
 import { directoryOpen } from "browser-fs-access"
 import { setFileBrowser } from "../../states/useFileBrowser"
+import { setFileHandle } from "../../states/useFileHandle"
 import { setFiles } from "../../states/useFiles"
+import { appendableRoot } from "../core/Appendable"
+import deserialize from "../serializer/deserialize"
 
 export default async () => {
     const files = await directoryOpen({
@@ -16,7 +19,14 @@ export default async () => {
     for (const file of files) {
         if (!file.name.toLowerCase().endsWith(".json")) continue
 
-        const text = await file.text()
-        console.log(text)
+        try {
+            const text = await file.text()
+            if (text.includes(`"type": "lingo3d"`)) {
+                for (const child of appendableRoot) child.dispose()
+                deserialize(JSON.parse(text))
+                setFileHandle(file.handle)
+                return
+            }
+        } catch {}
     }
 }
