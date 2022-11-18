b140de40ae4b5db18c181392761cc7016bc0e7e0
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-11-18 17:12:27 +0800

    updates

--- lingo3d/src/editor/states.ts
+++ lingo3d/src/editor/states.ts
@@ -81,14 +81,13 @@ export const [useSceneGraphExpanded, setSceneGraphExpanded] = preactStore<
 const [useTimelineScrollLeft, _setTimelineScrollLeft, getTimelineScrollLeft] =
     preactStore(0)
 export { useTimelineScrollLeft }
-export const addTimelineScrollLeft = (deltaX: number) => {
+export const addTimelineScrollLeft = (deltaX: number) =>
     _setTimelineScrollLeft(
         Math.min(
             Math.max(getTimelineScrollLeft() + deltaX, 0),
             getTimelineFrameNum() * FRAME_WIDTH - 520
         )
     )
-}
 export const [useTimelineFrameNum, , getTimelineFrameNum] = preactStore(1000)
 const [
     useTimelineExpandedUUIDs,
