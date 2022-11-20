ed76a83033e2621a36bdce15b260f0dabdf0880c
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-11-20 19:21:38 +0800

    updates

--- lingo3d/src/editor/index.tsx
+++ lingo3d/src/editor/index.tsx
@@ -3,18 +3,22 @@ import "./SceneGraph"
 import "./Toolbar"
 import "./Library"
 import "./HUD"
-import "./LingoEditor"
+import LingoEditor from "./LingoEditor"
 import { Disposable } from "@lincode/promiselikes"
+import { render } from "preact"
 
-export default class LingoEditor extends Disposable {
+export default class extends Disposable {
     public constructor() {
         super()
 
-        const el = document.createElement("lingo3d-lingoeditor")
+        const el = document.createElement("div")
         document.body.appendChild(el)
+
+        render(<LingoEditor />, el)
 
         this.then(() => {
             document.body.removeChild(el)
+            render(undefined, el)
         })
     }
 }
