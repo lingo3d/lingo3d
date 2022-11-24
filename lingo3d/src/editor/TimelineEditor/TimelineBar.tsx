3fb18e0332d538124608710f482c62dc5714ecd6
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-11-24 21:38:55 +0800

    Add audio button

--- lingo3d/src/editor/TimelineEditor/TimelineBar.tsx
+++ lingo3d/src/editor/TimelineEditor/TimelineBar.tsx
@@ -1,7 +1,18 @@
 import AppBar from "../component/bars/AppBar"
+import AppBarButton from "../component/bars/AppBarButton"
+import Button from "../component/Button"
+import Tab from "../component/tabs/Tab"
+import AudioIcon from "./icons/AudioIcon"
 
 const TimelineBar = () => {
-    return <AppBar></AppBar>
+    return (
+        <AppBar>
+            <Button>
+                <AudioIcon />
+                Add audio
+            </Button>
+        </AppBar>
+    )
 }
 
 export default TimelineBar
---
+++ lingo3d/src/editor/component/Button.tsx
@@ -0,0 +1,29 @@
+import { ComponentChildren } from "preact"
+
+export type ButtonProps = {
+    children?: ComponentChildren
+    disabled?: boolean
+    onClick?: () => void
+}
+
+const Button = ({ children, disabled, onClick }: ButtonProps) => {
+    return (
+        <div
+            className="lingo3d-bg lingo3d-flexcenter"
+            style={{
+                opacity: disabled ? 0.1 : 1,
+                height: 20,
+                padding: 12,
+                background: "rgba(255, 255, 255, 0.1)",
+                border: "1px solid rgba(255, 255, 255, 0.2)",
+                display: "flex",
+                gap: 10
+            }}
+            onClick={disabled ? undefined : onClick}
+        >
+            {children}
+        </div>
+    )
+}
+
+export default Button
