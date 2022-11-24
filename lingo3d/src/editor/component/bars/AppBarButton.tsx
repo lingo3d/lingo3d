0e39fb38c310d29d356e63e8ae1ce435c3e9d2ec
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-11-24 21:14:50 +0800

    updates

--- lingo3d/src/editor/component/bars/AppBarButton.tsx
+++ lingo3d/src/editor/component/bars/AppBarButton.tsx
@@ -27,7 +27,7 @@ const AppBarButton = ({
             className="lingo3d-flexcenter"
             style={{
                 width: APPBAR_HEIGHT,
-                height: APPBAR_HEIGHT,
+                height: APPBAR_HEIGHT - 4,
                 marginRight: fill ? 8 : 2,
                 opacity: disabled ? 0.1 : 0.5,
                 cursor: disabled ? "default" : "pointer",
