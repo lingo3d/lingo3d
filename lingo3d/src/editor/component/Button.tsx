aad37c299622bf9c3d59c574b4e2f9bbac16b347
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-11-24 22:21:37 +0800

    updates

--- lingo3d/src/editor/component/Button.tsx
+++ lingo3d/src/editor/component/Button.tsx
@@ -14,8 +14,8 @@ const Button = ({ children, disabled, onClick }: ButtonProps) => {
                 opacity: disabled ? 0.1 : 1,
                 height: 20,
                 padding: 12,
-                background: "rgba(255, 255, 255, 0.1)",
-                border: "1px solid rgba(255, 255, 255, 0.2)",
+                background: "rgba(255, 255, 255, 0.05)",
+                border: "1px solid rgba(255, 255, 255, 0.1)",
                 display: "flex",
                 gap: 10
             }}
--- lingo3d/src/editor/component/IconButton.tsx
+++ lingo3d/src/editor/component/IconButton.tsx
@@ -26,7 +26,8 @@ const IconButton = ({ children, onClick, disabled, fill }: IconButtonProps) => {
                 marginRight: fill ? 8 : 2,
                 opacity: disabled ? 0.1 : 0.5,
                 cursor: disabled ? "default" : "pointer",
-                background: fill ? "rgba(255, 255, 255, 0.1)" : undefined
+                background: fill ? "rgba(255, 255, 255, 0.1)" : undefined,
+                border: fill ? "1px solid rgba(255, 255, 255, 0.2)" : undefined
             }}
         >
             {children}
