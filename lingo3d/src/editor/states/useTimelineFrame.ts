d328753616e54c3429c7bd8cb5d2649ad7348e4c
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-11-26 23:59:25 +0800

    updates

--- lingo3d/src/editor/states/useTimelineFrame.ts
+++ lingo3d/src/editor/states/useTimelineFrame.ts
@@ -4,17 +4,25 @@ import { getTimeline } from "./useTimeline"
 export const [useTimelineFrame, setTimelineFrame, getTimelineFrame] =
     preactStore(0)
 
-export const increaseTimelineFrame = () =>
-    setTimelineFrame(getTimelineFrame() + 1)
+export const increaseTimelineFrame = () => {
+    const timeline = getTimeline()
+    timeline && setTimelineFrame((timeline.frame = getTimelineFrame() + 1))
+}
 
-export const decreaseTimelineFrame = () =>
-    setTimelineFrame(Math.max(getTimelineFrame() - 1, 0))
+export const decreaseTimelineFrame = () => {
+    const timeline = getTimeline()
+    timeline &&
+        setTimelineFrame((timeline.frame = Math.max(getTimelineFrame() - 1, 0)))
+}
 
-export const firstTimelineFrame = () => setTimelineFrame(0)
+export const firstTimelineFrame = () => {
+    const timeline = getTimeline()
+    timeline && setTimelineFrame((timeline.frame = 0))
+}
 
 export const lastTimelineFrame = () => {
     const timeline = getTimeline()
-    timeline && setTimelineFrame(timeline.totalFrames)
+    timeline && setTimelineFrame((timeline.frame = timeline.totalFrames))
 }
 
 getTimeline((timeline) => !timeline && setTimelineFrame(0))
