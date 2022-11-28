3deae7ee5462cd113ae9642ec4ef475866f45a8a
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-11-28 21:51:50 +0800

    design update

--- lingo3d/src/editor/TimelineEditor/FrameTween.tsx
+++ lingo3d/src/editor/TimelineEditor/FrameTween.tsx
@@ -1,5 +1,17 @@
-import { memo } from "preact/compat"
+import { memo, useMemo } from "preact/compat"
 import { FRAME_WIDTH, FRAME_HEIGHT } from "../../globals"
+
+const colors = [
+    "#880E4F",
+    "#311B92",
+    "#0D47A1",
+    "#006064",
+    "#1B5E20",
+    "#827717",
+    "#FF6F00",
+    "#BF360C"
+]
+let colorIndex = 0
 
 type FrameTweenProps = {
     frameNum: number
@@ -8,8 +20,14 @@ type FrameTweenProps = {
 }
 
 const FrameTween = ({ frameNum, frameNums, index }: FrameTweenProps) => {
+    const background = useMemo(() => {
+        if (++colorIndex === colors.length) colorIndex = 0
+        return colors[colorIndex]
+    }, [])
+
     return (
         <div
+            className="lingo3d-flexcenter"
             style={{
                 width:
                     Math.max((frameNums[index + 1] ?? frameNum) - frameNum, 1) *
@@ -19,7 +37,16 @@ const FrameTween = ({ frameNum, frameNums, index }: FrameTweenProps) => {
                 left: frameNum * FRAME_WIDTH,
                 opacity: 0.5
             }}
-        />
+        >
+            <div
+                style={{
+                    width: "100%",
+                    height: 10,
+                    borderRadius: 999,
+                    background
+                }}
+            />
+        </div>
     )
 }
 
