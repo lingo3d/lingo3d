0f50e1f008093c7fd49c5849fcebe3d2977529f3
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-12-01 15:19:02 +0800

    reticle choices

--- lingo3d/src/interface/IReticle.ts
+++ lingo3d/src/interface/IReticle.ts
@@ -4,6 +4,7 @@ import IAppendable, {
     appendableDefaults,
     appendableSchema
 } from "./IAppendable"
+import Choices from "./utils/Choices"
 
 export default interface IReticle extends IAppendable {
     variant: 1 | 2 | 3 | 4
@@ -14,6 +15,12 @@ export const reticleSchema: Required<ExtractProps<IReticle>> = {
     variant: Number
 }
 
-export const reticleDefaults = extendDefaults<IReticle>([appendableDefaults], {
-    variant: 1
-})
+export const reticleDefaults = extendDefaults<IReticle>(
+    [appendableDefaults],
+    {
+        variant: 1
+    },
+    {
+        variant: new Choices({ 1: 1, 2: 2, 3: 3, 4: 4 })
+    }
+)
