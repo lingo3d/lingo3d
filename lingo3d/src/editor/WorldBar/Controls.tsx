826e5df434e82502cc460bf0c91dc895ee51af0a
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-12-06 00:14:24 +0800

    implemented play and pause

--- lingo3d/src/editor/WorldBar/Controls.tsx
+++ lingo3d/src/editor/WorldBar/Controls.tsx
@@ -1,14 +1,27 @@
 import IconButton from "../component/IconButton"
 import PlayIcon from "../component/icons/PlayIcon"
 import PauseIcon from "../component/icons/PauseIcon"
+import useSyncState from "../hooks/useSyncState"
+import { getEditorModeComputed } from "../../states/useEditorModeComputed"
+import { setEditorMode } from "../../states/useEditorMode"
 
 const Controls = () => {
+    const mode = useSyncState(getEditorModeComputed)
+
     return (
         <>
-            <IconButton fill>
+            <IconButton
+                fill
+                disabled={mode === "play"}
+                onClick={() => setEditorMode("play")}
+            >
                 <PlayIcon />
             </IconButton>
-            <IconButton fill disabled>
+            <IconButton
+                fill
+                disabled={mode !== "play"}
+                onClick={() => setEditorMode("translate")}
+            >
                 <PauseIcon />
             </IconButton>
         </>
