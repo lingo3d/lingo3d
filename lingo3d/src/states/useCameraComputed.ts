766504b1eec066267589c0df26acf4a1d7f632d2
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-12-06 17:07:59 +0800

    updates

--- lingo3d/src/states/useCameraComputed.ts
+++ lingo3d/src/states/useCameraComputed.ts
@@ -3,9 +3,15 @@ import { last } from "@lincode/utils"
 import mainCamera from "../engine/mainCamera"
 import { getCameraStack } from "./useCameraStack"
 import { getEditorCamera } from "./useEditorCamera"
+import { getTimelinePaused } from "./useTimelinePaused"
 
 export const [setCameraComputed, getCameraComputed] = store(mainCamera)
 
 createEffect(() => {
-    setCameraComputed(getEditorCamera() ?? last(getCameraStack()) ?? mainCamera)
-}, [getCameraStack, getEditorCamera])
+    const activeCamera = last(getCameraStack()) ?? mainCamera
+    if (!getTimelinePaused()) {
+        setCameraComputed(activeCamera)
+        return
+    }
+    setCameraComputed(getEditorCamera() ?? activeCamera)
+}, [getCameraStack, getEditorCamera, getTimelinePaused])
