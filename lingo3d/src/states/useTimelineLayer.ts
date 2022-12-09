0adddd8d953724e65ca5534360b04ce6f56e4bff
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-12-09 22:12:57 +0800

    synchronize with selectionTarget

--- lingo3d/src/states/useTimelineLayer.ts
+++ lingo3d/src/states/useTimelineLayer.ts
@@ -1,5 +1,15 @@
-import store from "@lincode/reactivity"
+import store, { createEffect } from "@lincode/reactivity"
+import { getSelectionTarget } from "./useSelectionTarget"
 
 export const [setTimelineLayer, getTimelineLayer] = store<string | undefined>(
     undefined
 )
+
+createEffect(() => {
+    const target = getSelectionTarget()
+    const layer = getTimelineLayer()
+    if (!layer && target) setTimelineLayer(target.uuid)
+    else if (layer && !target) setTimelineLayer(undefined)
+    else if (layer && target && layer.split(" ")[0] !== target.uuid)
+        setTimelineLayer(target.uuid)
+}, [getTimelineLayer, getSelectionTarget])
