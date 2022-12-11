bf0f96fdcb79a5d06c582e0167efe4a388d9548a
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-12-11 15:28:08 +0800

    convex

--- lingo3d/src/interface/IPhysicsObjectManager.ts
+++ lingo3d/src/interface/IPhysicsObjectManager.ts
@@ -7,7 +7,7 @@ import { ExtractProps } from "./utils/extractProps"
 import { hideSchema } from "./utils/nonEditorSchemaSet"
 import { extendDefaults } from "./utils/Defaults"
 
-export type PhysicsOptions = boolean | "map" | "character"
+export type PhysicsOptions = boolean | "map" | "character" | "convex"
 
 export default interface IPhysicsObjectManager extends ISimpleObjectManager {
     velocity: Point3d
--- lingo3d/src/tests/testPhysX.ts
+++ lingo3d/src/tests/testPhysX.ts
@@ -1,5 +1,6 @@
 import settings from "../api/settings"
 import Cube from "../display/primitives/Cube"
+import Octahedron from "../display/primitives/Octahedron"
 
 const ground = new Cube()
 ground.width = 9999
@@ -11,10 +12,10 @@ ground.roughness = 0
 settings.ssr = true
 
 // setInterval(() => {
-    const box = new Cube()
-    box.y = 200
-    box.color = "red"
-    box.physics = true
-    box.roughness = 0
-    box.metalness = 0.5
+const box = new Octahedron()
+box.y = 200
+box.color = "red"
+box.physics = "convex"
+box.roughness = 0
+box.metalness = 0.5
 // }, 100)
