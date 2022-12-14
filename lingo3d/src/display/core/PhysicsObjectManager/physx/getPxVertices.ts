60c5ecc357b550f08756c18b0adfb44cc93d34bb
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-12-14 16:07:58 +0800

    updates

---
+++ lingo3d/src/display/core/PhysicsObjectManager/physx/getPxVertices.ts
@@ -0,0 +1,45 @@
+import { Object3D, BufferGeometry, Mesh } from "three"
+import { getPhysX } from "../../../../states/usePhysX"
+
+const mergedPxVerticesCache = new Map<
+    string | undefined,
+    readonly [any, number]
+>()
+
+export default (src: string | undefined, loaded: Object3D) => {
+    if (mergedPxVerticesCache.has(src)) return mergedPxVerticesCache.get(src)!
+
+    const { Vector_PxVec3 } = getPhysX()
+
+    let vertexCount = 0
+    const geometries: Array<BufferGeometry> = []
+
+    loaded.updateMatrixWorld()
+    loaded.traverse((c: Object3D | Mesh) => {
+        if (!("geometry" in c)) return
+        const clone = c.geometry.clone()
+        clone.applyMatrix4(c.matrixWorld)
+        geometries.push(clone)
+        vertexCount += clone.attributes.position.count
+    })
+
+    const vec3Vector = new Vector_PxVec3(vertexCount)
+
+    let iTotal = 0
+    for (const geometry of geometries) {
+        const buffer = geometry.attributes.position
+        const vertices = buffer.array
+        for (let i = 0; i < buffer.count; i++) {
+            const pxVec3 = vec3Vector.at(iTotal++)
+            const offset = i * 3
+            pxVec3.set_x(vertices[offset])
+            pxVec3.set_y(vertices[offset + 1])
+            pxVec3.set_z(vertices[offset + 2])
+        }
+    }
+    // vec3Vector.destroy()
+
+    const result = <const>[vec3Vector, vertexCount]
+    mergedPxVerticesCache.set(src, result)
+    return result
+}
