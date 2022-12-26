ebd29761c0097db4bd775665f31f4768ef32b70f
lingosandi <lai@lingo3d.com>
2022-12-26 18:53:56 +0800

    updates

--- lingo3d/src/display/utils/quaternions.ts
+++ lingo3d/src/display/utils/quaternions.ts
@@ -1,20 +1,24 @@
 import { Object3D, Quaternion } from "three"
+import scene from "../../engine/scene"
+import { quaternion } from "./reusables"
 
 export const worldToLocalQuaternion = (
     object3d: Object3D,
     worldQuaternion: Quaternion
 ) => {
-    const localQuaternion = worldQuaternion.clone()
+    const parents: Array<Object3D> = []
     let parent = object3d.parent
-    while (parent) {
-        localQuaternion.premultiply(parent.quaternion.clone().invert())
+    while (parent && parent !== scene) {
+        parents.push(parent)
         parent = parent.parent
     }
-    return localQuaternion
+    for (const parent of parents.reverse())
+        worldQuaternion.premultiply(quaternion.copy(parent.quaternion).invert())
+    return worldQuaternion
 }
 
 export const diffQuaternions = (from: Quaternion, to: Quaternion) =>
     from.clone().invert().multiply(to)
 
 export const complementQuaternion = (to: Quaternion, diff: Quaternion) =>
-    to.multiply(diff.clone().invert())
+    to.multiply(quaternion.copy(diff).invert())
--- lingo3d/src/tests/testCharacter.ts
+++ lingo3d/src/tests/testCharacter.ts
@@ -1,9 +1,4 @@
-import { Point3d } from "@lincode/math"
 import Character from "../display/Character"
-import Cube from "../display/primitives/Cube"
 
-// const character = new Character()
-// character.src = "player2.glb"
-
-const box = new Cube()
-box.lookTo(new Point3d(100, 100, 100), 0.05)
+const character = new Character()
+character.src = "player2.glb"
