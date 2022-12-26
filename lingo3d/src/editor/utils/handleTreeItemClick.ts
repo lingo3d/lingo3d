51584bf550ea0f75b32c97fdab3c82c66d920f73
lingosandi <lai@lingo3d.com>
2022-12-27 03:00:39 +0800

    updates

--- lingo3d/src/editor/utils/handleTreeItemClick.ts
+++ lingo3d/src/editor/utils/handleTreeItemClick.ts
@@ -2,17 +2,17 @@ import { Object3D } from "three"
 import Appendable from "../../api/core/Appendable"
 import { isPositionedItem } from "../../api/core/PositionedItem"
 import { emitSelectionTarget } from "../../events/onSelectionTarget"
-import { setEditorMode } from "../../states/useEditorMode"
 import { getWorldPlayComputed } from "../../states/useWorldPlayComputed"
 import { setSelectionNativeTarget } from "../../states/useSelectionNativeTarget"
 import { getSelectionTarget } from "../../states/useSelectionTarget"
+import { setWorldPlay } from "../../states/useWorldPlay"
 
 export default (
     target?: Appendable | Object3D,
     rightClick?: boolean,
     parent?: Appendable
 ) => {
-    getWorldPlayComputed() && setEditorMode("translate")
+    getWorldPlayComputed() && setWorldPlay(false)
     queueMicrotask(() => {
         if (isPositionedItem(parent) && getSelectionTarget() !== parent)
             emitSelectionTarget(parent, rightClick, true)
