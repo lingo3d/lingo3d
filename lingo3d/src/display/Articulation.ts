e7c8e3ba176dd356eb17f3797d73f7d832317045
lingosandi <lai@lingo3d.com>
2022-12-30 16:46:23 +0800

    articulation update

--- lingo3d/src/display/Articulation.ts
+++ lingo3d/src/display/Articulation.ts
@@ -1,5 +1,6 @@
 import { Reactive } from "@lincode/reactivity"
 import Appendable from "../api/core/Appendable"
+import threeScene from "../engine/scene"
 import { getPhysX } from "../states/usePhysX"
 import PhysicsObjectManager from "./core/PhysicsObjectManager"
 import destroy from "./core/PhysicsObjectManager/physx/destroy"
@@ -13,15 +14,18 @@ export default class Articulation extends Appendable {
             const {
                 physics,
                 PxRigidBodyExt,
-                scene,
                 PxArticulationJointTypeEnum,
                 PxArticulationAxisEnum,
-                PxArticulationMotionEnum
+                PxArticulationMotionEnum,
+                scene
             } = getPhysX()
             if (!physics) return
 
             const [torsoCube] = this.children ?? []
             if (!(torsoCube instanceof PhysicsObjectManager)) return
+
+            threeScene.attach(torsoCube.outerObject3d)
+            torsoCube.physics = false
 
             const articulation = physics.createArticulationReducedCoordinate()
 
@@ -30,8 +34,46 @@ export default class Articulation extends Appendable {
                 assignPxPose(torsoCube.outerObject3d)
             )
             const torsoShape = torsoCube.getPxShape(true, torsoLink)
-            PxRigidBodyExt.prototype.updateMassAndInertia(torsoLink, 3)
+            // PxRigidBodyExt.prototype.updateMassAndInertia(torsoLink, 3)
             managerShapeLinkMap.set(torsoCube, [torsoShape, torsoLink])
+
+            const traverse = (
+                parent: PhysicsObjectManager,
+                parentLink: any
+            ) => {
+                const [headCube] = parent.children ?? []
+                if (!(headCube instanceof PhysicsObjectManager)) return
+
+                threeScene.attach(headCube.outerObject3d)
+                headCube.physics = false
+
+                const headLink = articulation.createLink(
+                    parentLink,
+                    assignPxPose(headCube.outerObject3d)
+                )
+                const headShape = headCube.getPxShape(true, headLink)
+                // PxRigidBodyExt.prototype.updateMassAndInertia(headLink, 1)
+                managerShapeLinkMap.set(headCube, [headShape, headLink])
+
+                const joint = headLink.getInboundJoint()
+                joint.setJointType(PxArticulationJointTypeEnum.eREVOLUTE())
+                joint.setMotion(
+                    PxArticulationAxisEnum.eTWIST(),
+                    PxArticulationMotionEnum.eFREE()
+                )
+                joint.setMotion(
+                    PxArticulationAxisEnum.eSWING1(),
+                    PxArticulationMotionEnum.eFREE()
+                )
+                joint.setMotion(
+                    PxArticulationAxisEnum.eSWING2(),
+                    PxArticulationMotionEnum.eFREE()
+                )
+                traverse(headCube, headLink)
+            }
+            traverse(torsoCube, torsoLink)
+
+            scene.addArticulation(articulation)
 
             return () => {
                 destroy(articulation)
@@ -40,7 +82,9 @@ export default class Articulation extends Appendable {
     }
 
     private refreshState = new Reactive({})
-    public refresh() {
+
+    public override append(child: Appendable) {
+        super.append(child)
         this.refreshState.set({})
     }
 }
--- lingo3d/src/tests/testArticulation.ts
+++ lingo3d/src/tests/testArticulation.ts
@@ -1,5 +1,6 @@
 import Cube from "../display/primitives/Cube"
 import "../display/core/PhysicsObjectManager/physx"
+import Articulation from "../display/Articulation"
 
 const ground = new Cube()
 ground.width = 1000
@@ -9,63 +10,22 @@ ground.y = -300
 ground.color = "black"
 ground.physics = "map"
 
-const headCube = new Cube()
-headCube.scaleX = headCube.scaleY = headCube.scaleZ = 0.25
-headCube.y = 40
-
 const torsoCube = new Cube()
 torsoCube.scaleX = 0.35
 torsoCube.scaleY = 0.45
 torsoCube.scaleZ = 0.2
+
+const headCube = new Cube()
+headCube.scaleX = headCube.scaleY = headCube.scaleZ = 0.25
+headCube.y = 40
+torsoCube.attach(headCube)
 
 const hipCube = new Cube()
 hipCube.scaleX = 0.35
 hipCube.scaleY = 0.1
 hipCube.scaleZ = 0.2
 hipCube.y = -30
+torsoCube.attach(hipCube)
 
-// createEffect(() => {
-//     const {
-//         physics,
-//         PxRigidBodyExt,
-//         scene,
-//         PxArticulationJointTypeEnum,
-//         PxArticulationAxisEnum,
-//         PxArticulationMotionEnum
-//     } = getPhysX()
-//     if (!physics) return
-
-//     const articulation = physics.createArticulationReducedCoordinate()
-
-//     const torsoLink = articulation.createLink(
-//         null,
-//         assignPxPose(torsoCube.outerObject3d)
-//     )
-//     const torsoShape = torsoCube.getPxShape(true, torsoLink)
-//     PxRigidBodyExt.prototype.updateMassAndInertia(torsoLink, 3)
-//     managerShapeLinkMap.set(torsoCube, [torsoShape, torsoLink])
-
-//     const headLink = articulation.createLink(
-//         torsoLink,
-//         assignPxPose(headCube.outerObject3d)
-//     )
-//     const headShape = headCube.getPxShape(true, headLink)
-//     PxRigidBodyExt.prototype.updateMassAndInertia(headLink, 1)
-//     managerShapeLinkMap.set(headCube, [headShape, headLink])
-
-//     const joint = headLink.getInboundJoint()
-//     joint.setJointType(PxArticulationJointTypeEnum.eREVOLUTE())
-//     joint.setMotion(
-//         PxArticulationAxisEnum.eTWIST(),
-//         PxArticulationMotionEnum.eFREE()
-//     )
-//     joint.setMotion(
-//         PxArticulationAxisEnum.eSWING1(),
-//         PxArticulationMotionEnum.eFREE()
-//     )
-//     joint.setMotion(
-//         PxArticulationAxisEnum.eSWING2(),
-//         PxArticulationMotionEnum.eFREE()
-//     )
-//     scene.addArticulation(articulation)
-// }, [getPhysX])
+const articulation = new Articulation()
+articulation.append(torsoCube)
