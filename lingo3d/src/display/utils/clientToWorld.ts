3ca492b794d3416c1253e5eab60647426c7f0d15
lingosandi <lai@lingo3d.com>
2023-01-01 18:36:53 +0800

    bug fix

--- lingo3d/src/display/utils/clientToWorld.ts
+++ lingo3d/src/display/utils/clientToWorld.ts
@@ -9,11 +9,12 @@ export default (clientX: number, clientY: number, distance = 500) => {
     const [xNorm, yNorm] = normalizeClientPosition(clientX, clientY)
 
     const camera = getCameraRendered()
+    const cameraPosition = getWorldPosition(camera)
+
     vector3.set(xNorm, yNorm, 0.5)
     vector3.unproject(camera)
-
-    const cameraPosition = getWorldPosition(camera)
     vector3.sub(cameraPosition).normalize()
+    
     const vec = cameraPosition.add(vector3.multiplyScalar(distance * CM2M))
     return vec2Point(vec)
 }
