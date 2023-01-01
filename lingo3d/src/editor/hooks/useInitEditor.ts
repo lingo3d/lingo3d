bb619638e56828d0f751327b1079e631a7d2fbb5
lingosandi <lai@lingo3d.com>
2023-01-01 21:37:21 +0800

    refactored useInitEditor

--- lingo3d/src/editor/hooks/useInitEditor.ts
+++ lingo3d/src/editor/hooks/useInitEditor.ts
@@ -1,30 +1,12 @@
 import { useLayoutEffect } from "preact/hooks"
-import settings from "../../api/settings"
-import mainCamera from "../../engine/mainCamera"
-import { onLoadFile } from "../../events/onLoadFile"
-import { setEditorCamera } from "../../states/useEditorCamera"
 import { setEditorMounted } from "../../states/useEditorMounted"
-import { setOrbitControls } from "../../states/useOrbitControls"
-import { setWorldPlay } from "../../states/useWorldPlay"
 
 export default () => {
     useLayoutEffect(() => {
-        setEditorCamera(mainCamera)
-        setOrbitControls(true)
         setEditorMounted(true)
-        setWorldPlay(false)
-
-        settings.gridHelper = true
-        const handle = onLoadFile(() => (settings.gridHelper = false))
 
         return () => {
-            setEditorCamera(undefined)
-            setOrbitControls(false)
-            settings.gridHelper = false
             setEditorMounted(false)
-            setWorldPlay(true)
-
-            handle.cancel()
         }
     }, [])
 }
--- lingo3d/src/states/useEditorMounted.ts
+++ lingo3d/src/states/useEditorMounted.ts
@@ -1,3 +1,29 @@
-import store from "@lincode/reactivity"
+import store, { createEffect } from "@lincode/reactivity"
+import settings from "../api/settings"
+import mainCamera from "../engine/mainCamera"
+import { onLoadFile } from "../events/onLoadFile"
+import { setEditorCamera } from "./useEditorCamera"
+import { setOrbitControls } from "./useOrbitControls"
+import { setWorldPlay } from "./useWorldPlay"
 
 export const [setEditorMounted, getEditorMounted] = store(false)
+
+createEffect(() => {
+    if (!getEditorMounted()) return
+
+    setEditorCamera(mainCamera)
+    setOrbitControls(true)
+    setWorldPlay(false)
+
+    settings.gridHelper = true
+    const handle = onLoadFile(() => (settings.gridHelper = false))
+
+    return () => {
+        setEditorCamera(undefined)
+        setOrbitControls(false)
+        settings.gridHelper = false
+        setWorldPlay(true)
+
+        handle.cancel()
+    }
+}, [getEditorMounted])
