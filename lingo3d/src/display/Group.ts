fb594cb13aca740eaec0f7767c2fbf286cd0b597
lingosandi <lai@lingo3d.com>
2023-01-02 18:51:24 +0800

    refactored multiple selection logic

--- lingo3d/src/display/Group.ts
+++ lingo3d/src/display/Group.ts
@@ -1,15 +1,6 @@
-import { createEffect } from "@lincode/reactivity"
-import { Group as ThreeGroup, Object3D } from "three"
-import { hiddenAppendables } from "../api/core/collections"
-import scene from "../engine/scene"
-import { onEditorGroupItems } from "../events/onEditorGroupItems"
-import { emitSelectionTarget } from "../events/onSelectionTarget"
+import { Group as ThreeGroup } from "three"
 import IGroup, { groupDefaults, groupSchema } from "../interface/IGroup"
-import { getMultipleSelectionTargets } from "../states/useMultipleSelectionTargets"
-import { setSelectionTarget } from "../states/useSelectionTarget"
-import SimpleObjectManager from "./core/SimpleObjectManager"
 import VisibleObjectManager from "./core/VisibleObjectManager"
-import { box3, vector3 } from "./utils/reusables"
 
 export default class Group
     extends VisibleObjectManager<ThreeGroup>
@@ -23,49 +14,3 @@ export default class Group
         super(new ThreeGroup())
     }
 }
-
-createEffect(() => {
-    const [targets] = getMultipleSelectionTargets()
-    if (!targets.size) return
-
-    const group = new ThreeGroup()
-    scene.add(group)
-
-    const groupManager = new SimpleObjectManager(group)
-    hiddenAppendables.add(groupManager)
-    setSelectionTarget(groupManager)
-
-    const parentEntries: Array<[Object3D, Object3D]> = []
-    for (const { outerObject3d } of targets) {
-        if (!outerObject3d.parent) continue
-        parentEntries.push([outerObject3d, outerObject3d.parent])
-        group.attach(outerObject3d)
-    }
-
-    box3.setFromObject(group)
-    for (const [object, parent] of parentEntries) parent.attach(object)
-
-    group.position.copy(box3.getCenter(vector3))
-    for (const [object] of parentEntries) group.attach(object)
-
-    let consolidated = false
-    const handle = onEditorGroupItems(() => {
-        if (!targets.size || consolidated) return
-        consolidated = true
-
-        const consolidatedGroup = new Group()
-        consolidatedGroup.position.copy(group.position)
-        for (const target of targets) consolidatedGroup.attach(target)
-
-        emitSelectionTarget(consolidatedGroup)
-    })
-
-    return () => {
-        if (!groupManager.done && !consolidated)
-            for (const [object, parent] of parentEntries) parent.attach(object)
-
-        groupManager.dispose()
-        scene.remove(group)
-        handle.cancel()
-    }
-}, [getMultipleSelectionTargets])
--- lingo3d/src/states/useMultipleSelectionTargets.ts
+++ lingo3d/src/states/useMultipleSelectionTargets.ts
@@ -3,6 +3,15 @@ import PositionedManager, {
     isPositionedManager
 } from "../display/core/PositionedManager"
 import { onDispose } from "../events/onDispose"
+import { Group as ThreeGroup, Object3D } from "three"
+import { hiddenAppendables } from "../api/core/collections"
+import SimpleObjectManager from "../display/core/SimpleObjectManager"
+import { box3, vector3 } from "../display/utils/reusables"
+import scene from "../engine/scene"
+import { onEditorGroupItems } from "../events/onEditorGroupItems"
+import { emitSelectionTarget } from "../events/onSelectionTarget"
+import { setSelectionTarget } from "./useSelectionTarget"
+import Group from "../display/Group"
 
 const [setMultipleSelectionTargets, getMultipleSelectionTargets] = store([
     new Set<PositionedManager>()
@@ -45,6 +54,52 @@ export const flushMultipleSelectionTargets = async (
     setMultipleSelectionTargets([targets])
     multipleSelectionTargetsFlushingPtr[0] = false
 }
+
+createEffect(() => {
+    const [targets] = getMultipleSelectionTargets()
+    if (!targets.size) return
+
+    const group = new ThreeGroup()
+    scene.add(group)
+
+    const groupManager = new SimpleObjectManager(group)
+    hiddenAppendables.add(groupManager)
+    setSelectionTarget(groupManager)
+
+    const parentEntries: Array<[Object3D, Object3D]> = []
+    for (const { outerObject3d } of targets) {
+        if (!outerObject3d.parent) continue
+        parentEntries.push([outerObject3d, outerObject3d.parent])
+        group.attach(outerObject3d)
+    }
+
+    box3.setFromObject(group)
+    for (const [object, parent] of parentEntries) parent.attach(object)
+
+    group.position.copy(box3.getCenter(vector3))
+    for (const [object] of parentEntries) group.attach(object)
+
+    let consolidated = false
+    const handle = onEditorGroupItems(() => {
+        if (!targets.size || consolidated) return
+        consolidated = true
+
+        const consolidatedGroup = new Group()
+        consolidatedGroup.position.copy(group.position)
+        for (const target of targets) consolidatedGroup.attach(target)
+
+        emitSelectionTarget(consolidatedGroup)
+    })
+
+    return () => {
+        if (!groupManager.done && !consolidated)
+            for (const [object, parent] of parentEntries) parent.attach(object)
+
+        groupManager.dispose()
+        scene.remove(group)
+        handle.cancel()
+    }
+}, [getMultipleSelectionTargets])
 
 createEffect(() => {
     const [targets] = getMultipleSelectionTargets()
