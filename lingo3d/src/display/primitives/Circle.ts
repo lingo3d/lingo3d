f960903a15b383f9b940fa40991f062dd4517801
lingosandi <lai@lingo3d.com>
2023-01-06 17:54:28 +0800

    geometry reference counting

--- lingo3d/src/display/primitives/Circle.ts
+++ lingo3d/src/display/primitives/Circle.ts
@@ -1,14 +1,54 @@
 import { CircleGeometry } from "three"
 import Primitive from "../core/Primitive"
 import ICircle, { circleDefaults, circleSchema } from "../../interface/ICircle"
+import { Class, forceGet } from "@lincode/utils"
 import { deg2Rad } from "@lincode/math"
-import { forceGetInstance } from "@lincode/utils"
 
-const circleGeometry = new CircleGeometry(0.5)
+const classMapsMap = new WeakMap<
+    Class,
+    [Map<string, any>, Record<string, number>]
+>()
 
-const thetaGeometryMap = new Map<number, CircleGeometry>()
-const allocate = (theta: number) => {
-    return forceGetInstance(thetaGeometryMap, theta, CircleGeometry)
+const increaseCount = <T extends Class>(
+    ClassVal: T,
+    params: Readonly<ConstructorParameters<T>>
+): InstanceType<T> => {
+    const [paramsInstanceMap, paramCountRecord] = forceGet(
+        classMapsMap,
+        ClassVal,
+        () => [new Map<string, any>(), {} as Record<string, number>]
+    )
+    const paramString = JSON.stringify(params)
+    if (
+        (paramCountRecord[paramString] =
+            (paramCountRecord[paramString] ?? 0) + 1) === 1
+    ) {
+        const result = new ClassVal(...params)
+        paramsInstanceMap.set(paramString, result)
+        return result as InstanceType<T>
+    }
+    return paramsInstanceMap.get(paramString)
+}
+
+const decreaseCount = <T extends Class>(
+    ClassVal: T,
+    params: Readonly<ConstructorParameters<T>>
+) => {
+    const [paramsInstanceMap, paramCountRecord] = forceGet(
+        classMapsMap,
+        ClassVal,
+        () => [new Map<string, any>(), {} as Record<string, number>]
+    )
+    const paramString = JSON.stringify(params)
+    const count = (paramCountRecord[paramString] ?? 0) - 1
+    if (count === -1) return
+    if (count === 0) {
+        paramsInstanceMap.get(paramString).dispose()
+        paramsInstanceMap.delete(paramString)
+        delete paramCountRecord[paramString]
+        return
+    }
+    paramCountRecord[paramString] = count
 }
 
 export default class Circle extends Primitive implements ICircle {
@@ -16,8 +56,12 @@ export default class Circle extends Primitive implements ICircle {
     public static override defaults = circleDefaults
     public static override schema = circleSchema
 
+    private params: Readonly<ConstructorParameters<typeof CircleGeometry>>
+
     public constructor() {
-        super(circleGeometry)
+        const params = <const>[0.5, 32, 0, 360 * deg2Rad]
+        super(increaseCount(CircleGeometry, params))
+        this.params = params
         this.object3d.scale.z = Number.EPSILON
     }
 
@@ -36,11 +80,10 @@ export default class Circle extends Primitive implements ICircle {
     }
     public set theta(val) {
         this._theta = val
-        const { geometry } = this.object3d
-        geometry !== circleGeometry && geometry.dispose()
-        this.object3d.geometry =
-            val === 360
-                ? circleGeometry
-                : new CircleGeometry(0.5, 32, 0, val * deg2Rad)
+        decreaseCount(CircleGeometry, this.params)
+        this.object3d.geometry = increaseCount(
+            CircleGeometry,
+            (this.params = [0.5, 32, 0, val * deg2Rad])
+        )
     }
 }
