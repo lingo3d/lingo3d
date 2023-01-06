c2721b0dc2f4a1f397ffe53dd1eb8af2fde0e8c1
lingosandi <lai@lingo3d.com>
2023-01-07 03:59:55 +0800

    simplified octahedron

--- lingo3d/src/display/primitives/Octahedron.ts
+++ lingo3d/src/display/primitives/Octahedron.ts
@@ -1,38 +1,12 @@
 import { OctahedronGeometry } from "three"
-import IOctahedron, {
-    octahedronDefaults,
-    octahedronSchema
-} from "../../interface/IOctahedron"
-import ConfigurablePrimitive, {
-    allocateDefaultInstance,
-    refreshParamsSystem
-} from "../core/ConfigurablePrimitive"
+import Primitive from "../core/Primitive"
 
-const defaultParams = <const>[0.5, 0]
-const geometry = allocateDefaultInstance(OctahedronGeometry, defaultParams)
+const geometry = new OctahedronGeometry(0.5)
 
-export default class Octahedron
-    extends ConfigurablePrimitive<typeof OctahedronGeometry>
-    implements IOctahedron
-{
+export default class Octahedron extends Primitive {
     public static componentName = "octahedron"
-    public static override defaults = octahedronDefaults
-    public static override schema = octahedronSchema
 
     public constructor() {
-        super(OctahedronGeometry, defaultParams, geometry)
-    }
-
-    protected override getParams() {
-        return <const>[0.5, this.detail]
-    }
-
-    private _detail?: number
-    public get detail() {
-        return this._detail ?? 0
-    }
-    public set detail(val) {
-        this._detail = val
-        refreshParamsSystem(this)
+        super(geometry)
     }
 }
--- lingo3d/src/display/primitives/Sphere.ts
+++ lingo3d/src/display/primitives/Sphere.ts
@@ -1,7 +1,7 @@
 import { SphereGeometry } from "three"
 import Primitive from "../core/Primitive"
 
-export const sphereGeometry = new SphereGeometry(0.5, 32, 32)
+export const sphereGeometry = new SphereGeometry(0.5)
 
 export default class Sphere extends Primitive {
     public static componentName = "sphere"
