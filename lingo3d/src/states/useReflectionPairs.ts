58cadf8c0cddb35e1d011badc2e392be18702416
lingosandi <lai@lingo3d.com>
2023-01-12 20:49:24 +0800

    updates

--- lingo3d/src/states/useReflectionPairs.ts
+++ lingo3d/src/states/useReflectionPairs.ts
@@ -1,14 +1,13 @@
 import store, { createEffect, pull, push } from "@lincode/reactivity"
 import { CubeCamera } from "three"
-import Loaded from "../display/core/Loaded"
-import Model from "../display/Model"
+import MeshAppendable from "../api/core/MeshAppendable"
 import getWorldPosition from "../display/utils/getWorldPosition"
 import scene from "../engine/scene"
 import { onRenderHalfRate } from "../events/onRenderHalfRate"
 import { getRenderer } from "./useRenderer"
 
 const [setReflectionPairs, getReflectionPairs] = store<
-    Array<[Model | Loaded, CubeCamera]>
+    Array<[MeshAppendable, CubeCamera]>
 >([])
 
 export const pushReflectionPairs = push(setReflectionPairs, getReflectionPairs)
@@ -20,18 +19,12 @@ createEffect(() => {
     if (!renderer || !pairs.length) return
 
     const handle = onRenderHalfRate(() => {
-        for (const [manager] of pairs) {
-            manager.outerObject3d.visible = false
-            if ("loadedGroup" in manager) manager.loadedGroup.visible = false
-        }
+        for (const [manager] of pairs) manager.outerObject3d.visible = false
         for (const [manager, cubeCamera] of pairs) {
             cubeCamera.position.copy(getWorldPosition(manager.outerObject3d))
             cubeCamera.update(renderer, scene)
         }
-        for (const [manager] of pairs) {
-            manager.outerObject3d.visible = true
-            if ("loadedGroup" in manager) manager.loadedGroup.visible = true
-        }
+        for (const [manager] of pairs) manager.outerObject3d.visible = true
     })
     return () => {
         handle.cancel()
