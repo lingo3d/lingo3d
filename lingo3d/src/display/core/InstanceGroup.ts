4f0cfda772036006443874373615552304c6a1ec
lingosandi <lai@lingo3d.com>
2023-01-13 01:12:12 +0800

    InstaceGroup component

---
+++ lingo3d/src/display/core/InstanceGroup.ts
@@ -0,0 +1,5 @@
+import Appendable from "../../api/core/Appendable"
+
+export default class InstanceGroup extends Appendable {
+    public static componentName = "instanceGroup"
+}
--- lingo3d/src/tests/testInstancing.ts
+++ lingo3d/src/tests/testInstancing.ts
@@ -1,22 +1,26 @@
 import { InstancedMesh, Mesh, Object3D } from "three"
 import Model from "../display/Model"
-import { standardMaterial } from "../display/utils/reusables"
 import scene from "../engine/scene"
 
 export default {}
 
-const model = new Model(true)
-model.src = "casa2.glb"
+// for (let i = 0; i < 100; ++i) {
+//     const model = new Model()
+//     model.src = "tree1.glb"
+// }
 
-model.onLoad = () => {
-    const loaded = model.loadedGroup.children[0]
-    loaded.traverse((child: Object3D | Mesh) => {
-        if (!("geometry" in child)) return
-        const instancedMesh = new InstancedMesh(
-            child.geometry,
-            standardMaterial,
-            100
-        )
-        scene.add(instancedMesh)
-    })
-}
+// const model = new Model(true)
+// model.src = "tree1.glb"
+
+// model.onLoad = () => {
+//     const loaded = model.loadedGroup.children[0]
+//     loaded.traverse((child: Object3D | Mesh) => {
+//         if (!("geometry" in child)) return
+//         const instancedMesh = new InstancedMesh(
+//             child.geometry,
+//             child.material,
+//             100
+//         )
+//         scene.add(instancedMesh)
+//     })
+// }
