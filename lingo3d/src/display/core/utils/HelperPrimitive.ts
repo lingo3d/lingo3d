ea0f7de91d7c5eeea6fc8103781d34cfb9b69405
lingosandi <lai@lingo3d.com>
2023-01-14 02:22:48 +0800

    helper transformcontrol callback override

--- lingo3d/src/display/core/utils/HelperPrimitive.ts
+++ lingo3d/src/display/core/utils/HelperPrimitive.ts
@@ -34,4 +34,24 @@ export default abstract class HelperPrimitive extends Primitive {
         if (this.target)
             this.target.outerObject3d.userData.onTranslateControl = cb
     }
+
+    public override get onRotateControl() {
+        return this.outerObject3d.userData.onRotateControl
+    }
+    public override set onRotateControl(
+        cb: ((phase: TransformControlsPhase) => void) | undefined
+    ) {
+        super.onRotateControl = cb
+        if (this.target) this.target.outerObject3d.userData.onRotateControl = cb
+    }
+
+    public override get onScaleControl() {
+        return this.outerObject3d.userData.onScaleControl
+    }
+    public override set onScaleControl(
+        cb: ((phase: TransformControlsPhase) => void) | undefined
+    ) {
+        super.onScaleControl = cb
+        if (this.target) this.target.outerObject3d.userData.onScaleControl = cb
+    }
 }
--- lingo3d/src/display/core/utils/HelperSprite.ts
+++ lingo3d/src/display/core/utils/HelperSprite.ts
@@ -35,4 +35,24 @@ export default class HelperSprite extends Sprite {
         if (this.target)
             this.target.outerObject3d.userData.onTranslateControl = cb
     }
+
+    public override get onRotateControl() {
+        return this.outerObject3d.userData.onRotateControl
+    }
+    public override set onRotateControl(
+        cb: ((phase: TransformControlsPhase) => void) | undefined
+    ) {
+        super.onRotateControl = cb
+        if (this.target) this.target.outerObject3d.userData.onRotateControl = cb
+    }
+
+    public override get onScaleControl() {
+        return this.outerObject3d.userData.onScaleControl
+    }
+    public override set onScaleControl(
+        cb: ((phase: TransformControlsPhase) => void) | undefined
+    ) {
+        super.onScaleControl = cb
+        if (this.target) this.target.outerObject3d.userData.onScaleControl = cb
+    }
 }
