1ed340a269f423b941e2d8e877989e977c7b287c
lingosandi <lai@lingo3d.com>
2023-01-16 09:11:17 +0800

    enum fn

--- lingo3d/src/states/usePhysX.ts
+++ lingo3d/src/states/usePhysX.ts
@@ -1,4 +1,5 @@
 import store from "@lincode/reactivity"
+import fn from "../interface/utils/fn"
 
 type PhysX = {
     NativeArrayHelpers: any
@@ -43,90 +44,90 @@ type PhysX = {
     PxShapeExt: any
     PxCreateDynamic: any
     PxCapsuleClimbingModeEnum: {
-        eEASY: any
-        eCONSTRAINED: any
+        eEASY: () => any
+        eCONSTRAINED: () => any
     }
     PxControllerBehaviorFlagEnum: {
-        eCCT_CAN_RIDE_ON_OBJECT: any
-        eCCT_SLIDE: any
-        eCCT_USER_DEFINED_RIDE: any
+        eCCT_CAN_RIDE_ON_OBJECT: () => any
+        eCCT_SLIDE: () => any
+        eCCT_USER_DEFINED_RIDE: () => any
     }
     PxControllerCollisionFlagEnum: {
-        eCOLLISION_SIDES: any
-        eCOLLISION_UP: any
-        eCOLLISION_DOWN: any
+        eCOLLISION_SIDES: () => any
+        eCOLLISION_UP: () => any
+        eCOLLISION_DOWN: () => any
     }
     PxControllerNonWalkableModeEnum: {
-        ePREVENT_CLIMBING: any
-        ePREVENT_CLIMBING_AND_FORCE_SLIDING: any
+        ePREVENT_CLIMBING: () => any
+        ePREVENT_CLIMBING_AND_FORCE_SLIDING: () => any
     }
     PxControllerShapeTypeEnum: {
-        eBOX: any
-        eCAPSULE: any
+        eBOX: () => any
+        eCAPSULE: () => any
     }
     PxForceModeEnum: {
-        eFORCE: any
-        eIMPULSE: any
-        eVELOCITY_CHANGE: any
-        eACCELERATION: any
+        eFORCE: () => any
+        eIMPULSE: () => any
+        eVELOCITY_CHANGE: () => any
+        eACCELERATION: () => any
     }
     PxActorFlagEnum: {
-        eDISABLE_GRAVITY: any
-        eSEND_SLEEP_NOTIFIES: any
-        eDISABLE_SIMULATION: any
-        eVISUALIZATION: any
+        eDISABLE_GRAVITY: () => any
+        eSEND_SLEEP_NOTIFIES: () => any
+        eDISABLE_SIMULATION: () => any
+        eVISUALIZATION: () => any
     }
     PxArticulationJointTypeEnum: {
-        ePRISMATIC: any
-        eREVOLUTE: any
-        eSPHERICAL: any
-        eFIX: any
-        eUNDEFINED: any
+        ePRISMATIC: () => any
+        eREVOLUTE: () => any
+        eSPHERICAL: () => any
+        eFIX: () => any
+        eUNDEFINED: () => any
     }
     PxArticulationAxisEnum: {
-        eTWIST: any
-        eSWING1: any
-        eSWING2: any
-        eX: any
-        eY: any
-        eZ: any
-        eCOUNT: any
+        eTWIST: () => any
+        eSWING1: () => any
+        eSWING2: () => any
+        eX: () => any
+        eY: () => any
+        eZ: () => any
+        eCOUNT: () => any
     }
     PxArticulationMotionEnum: {
-        eLOCKED: any
-        eLIMITED: any
-        eFREE: any
+        eLOCKED: () => any
+        eLIMITED: () => any
+        eFREE: () => any
     }
     PxRigidBodyFlagEnum: {
-        eKINEMATIC: any
-        eUSE_KINEMATIC_TARGET_FOR_SCENE_QUERIES: any
-        eENABLE_CCD: any
-        eENABLE_CCD_FRICTION: any
-        eENABLE_POSE_INTEGRATION_PREVIEW: any
-        eENABLE_SPECULATIVE_CCD: any
-        eENABLE_CCD_MAX_CONTACT_IMPULSE: any
-        eRETAIN_ACCELERATIONS: any
+        eKINEMATIC: () => any
+        eUSE_KINEMATIC_TARGET_FOR_SCENE_QUERIES: () => any
+        eENABLE_CCD: () => any
+        eENABLE_CCD_FRICTION: () => any
+        eENABLE_POSE_INTEGRATION_PREVIEW: () => any
+        eENABLE_SPECULATIVE_CCD: () => any
+        eENABLE_CCD_MAX_CONTACT_IMPULSE: () => any
+        eRETAIN_ACCELERATIONS: () => any
     }
     PxSphericalJointFlagEnum: {
-        eLIMIT_ENABLED: any
+        eLIMIT_ENABLED: () => any
     }
     PxArticulationCacheFlagEnum: {
-        eALL: any
-        ePOSITION: any
+        eALL: () => any
+        ePOSITION: () => any
     }
     PxD6AxisEnum: {
-        eX: any
-        eY: any
-        eZ: any
-        eSWING1: any
-        eSWING2: any
-        eTWIST: any
-        eCOUNT: any
+        eX: () => any
+        eY: () => any
+        eZ: () => any
+        eSWING1: () => any
+        eSWING2: () => any
+        eTWIST: () => any
+        eCOUNT: () => any
     }
     PxD6MotionEnum: {
-        eLOCKED: any
-        eLIMITED: any
-        eFREE: any
+        eLOCKED: () => any
+        eLIMITED: () => any
+        eFREE: () => any
     }
 }
 
@@ -168,90 +169,90 @@ export const [setPhysX, getPhysX] = store<PhysX>({
     PxShapeExt: undefined,
     PxCreateDynamic: undefined,
     PxCapsuleClimbingModeEnum: {
-        eEASY: undefined,
-        eCONSTRAINED: undefined
+        eEASY: fn,
+        eCONSTRAINED: fn
     },
     PxControllerBehaviorFlagEnum: {
-        eCCT_CAN_RIDE_ON_OBJECT: undefined,
-        eCCT_SLIDE: undefined,
-        eCCT_USER_DEFINED_RIDE: undefined
+        eCCT_CAN_RIDE_ON_OBJECT: fn,
+        eCCT_SLIDE: fn,
+        eCCT_USER_DEFINED_RIDE: fn
     },
     PxControllerCollisionFlagEnum: {
-        eCOLLISION_SIDES: undefined,
-        eCOLLISION_UP: undefined,
-        eCOLLISION_DOWN: undefined
+        eCOLLISION_SIDES: fn,
+        eCOLLISION_UP: fn,
+        eCOLLISION_DOWN: fn
     },
     PxControllerNonWalkableModeEnum: {
-        ePREVENT_CLIMBING: undefined,
-        ePREVENT_CLIMBING_AND_FORCE_SLIDING: undefined
+        ePREVENT_CLIMBING: fn,
+        ePREVENT_CLIMBING_AND_FORCE_SLIDING: fn
     },
     PxControllerShapeTypeEnum: {
-        eBOX: undefined,
-        eCAPSULE: undefined
+        eBOX: fn,
+        eCAPSULE: fn
     },
     PxForceModeEnum: {
-        eFORCE: undefined,
-        eIMPULSE: undefined,
-        eVELOCITY_CHANGE: undefined,
-        eACCELERATION: undefined
+        eFORCE: fn,
+        eIMPULSE: fn,
+        eVELOCITY_CHANGE: fn,
+        eACCELERATION: fn
     },
     PxActorFlagEnum: {
-        eDISABLE_GRAVITY: undefined,
-        eSEND_SLEEP_NOTIFIES: undefined,
-        eDISABLE_SIMULATION: undefined,
-        eVISUALIZATION: undefined
+        eDISABLE_GRAVITY: fn,
+        eSEND_SLEEP_NOTIFIES: fn,
+        eDISABLE_SIMULATION: fn,
+        eVISUALIZATION: fn
     },
     PxArticulationJointTypeEnum: {
-        ePRISMATIC: undefined,
-        eREVOLUTE: undefined,
-        eSPHERICAL: undefined,
-        eFIX: undefined,
-        eUNDEFINED: undefined
+        ePRISMATIC: fn,
+        eREVOLUTE: fn,
+        eSPHERICAL: fn,
+        eFIX: fn,
+        eUNDEFINED: fn
     },
     PxArticulationAxisEnum: {
-        eTWIST: undefined,
-        eSWING1: undefined,
-        eSWING2: undefined,
-        eX: undefined,
-        eY: undefined,
-        eZ: undefined,
-        eCOUNT: undefined
+        eTWIST: fn,
+        eSWING1: fn,
+        eSWING2: fn,
+        eX: fn,
+        eY: fn,
+        eZ: fn,
+        eCOUNT: fn
     },
     PxArticulationMotionEnum: {
-        eLOCKED: undefined,
-        eLIMITED: undefined,
-        eFREE: undefined
+        eLOCKED: fn,
+        eLIMITED: fn,
+        eFREE: fn
     },
     PxRigidBodyFlagEnum: {
-        eKINEMATIC: undefined,
-        eUSE_KINEMATIC_TARGET_FOR_SCENE_QUERIES: undefined,
-        eENABLE_CCD: undefined,
-        eENABLE_CCD_FRICTION: undefined,
-        eENABLE_POSE_INTEGRATION_PREVIEW: undefined,
-        eENABLE_SPECULATIVE_CCD: undefined,
-        eENABLE_CCD_MAX_CONTACT_IMPULSE: undefined,
-        eRETAIN_ACCELERATIONS: undefined
+        eKINEMATIC: fn,
+        eUSE_KINEMATIC_TARGET_FOR_SCENE_QUERIES: fn,
+        eENABLE_CCD: fn,
+        eENABLE_CCD_FRICTION: fn,
+        eENABLE_POSE_INTEGRATION_PREVIEW: fn,
+        eENABLE_SPECULATIVE_CCD: fn,
+        eENABLE_CCD_MAX_CONTACT_IMPULSE: fn,
+        eRETAIN_ACCELERATIONS: fn
     },
     PxSphericalJointFlagEnum: {
-        eLIMIT_ENABLED: undefined
+        eLIMIT_ENABLED: fn
     },
     PxArticulationCacheFlagEnum: {
-        eALL: undefined,
-        ePOSITION: undefined
+        eALL: fn,
+        ePOSITION: fn
     },
     PxD6AxisEnum: {
-        eX: undefined,
-        eY: undefined,
-        eZ: undefined,
-        eSWING1: undefined,
-        eSWING2: undefined,
-        eTWIST: undefined,
-        eCOUNT: undefined
+        eX: fn,
+        eY: fn,
+        eZ: fn,
+        eSWING1: fn,
+        eSWING2: fn,
+        eTWIST: fn,
+        eCOUNT: fn
     },
     PxD6MotionEnum: {
-        eLOCKED: undefined,
-        eLIMITED: undefined,
-        eFREE: undefined
+        eLOCKED: fn,
+        eLIMITED: fn,
+        eFREE: fn
     }
 })
 
