6145ec25c9b6bf766fff4d735c21cebbf0e05eae
lingosandi <lai@lingo3d.com>
2023-01-16 18:08:19 +0800

    sphere physics

--- lingo3d/src/display/primitives/Sphere.ts
+++ lingo3d/src/display/primitives/Sphere.ts
@@ -1,5 +1,10 @@
 import { SphereGeometry } from "three"
+import { PhysicsOptions } from "../../interface/IPhysicsObjectManager"
+import { physXPtr } from "../../states/usePhysX"
+import cookConvexGeometry from "../core/PhysicsObjectManager/physx/cookConvexGeometry"
+import destroy from "../core/PhysicsObjectManager/physx/destroy"
 import Primitive from "../core/Primitive"
+import getActualScale from "../utils/getActualScale"
 
 export const sphereGeometry = new SphereGeometry(0.5)
 
@@ -10,21 +15,43 @@ export default class Sphere extends Primitive {
         super(sphereGeometry)
     }
 
-    // public override getPxShape(_: PhysicsOptions, actor: any) {
-    //     const { material, shapeFlags, physics, PxBoxGeometry, pxFilterData } =
-    //         physXPtr[0]
+    public override getPxShape(_: PhysicsOptions, actor: any) {
+        const {
+            material,
+            shapeFlags,
+            physics,
+            PxSphereGeometry,
+            pxFilterData,
+            PxRigidActorExt
+        } = physXPtr[0]
 
-    //     const { x, y, z } = getActualScale(this).multiplyScalar(0.5)
-    //     const pxGeometry = new PxBoxGeometry(x, y, z)
-    //     const shape = physics.createShape(
-    //         pxGeometry,
-    //         material,
-    //         true,
-    //         shapeFlags
-    //     )
-    //     shape.setSimulationFilterData(pxFilterData)
-    //     destroy(pxGeometry)
-    //     actor.attachShape(shape)
-    //     return shape
-    // }
+        const { x, y, z } = getActualScale(this).multiplyScalar(0.5)
+        if (x === y && x === z) {
+            const pxGeometry = new PxSphereGeometry(x)
+            const shape = physics.createShape(
+                pxGeometry,
+                material,
+                true,
+                shapeFlags
+            )
+            shape.setSimulationFilterData(pxFilterData)
+            destroy(pxGeometry)
+            actor.attachShape(shape)
+            return shape
+        }
+
+        const pxGeometry = cookConvexGeometry(
+            `sphere(${x},${y},${z})`,
+            this.object3d,
+            this
+        )
+        const shape = PxRigidActorExt.prototype.createExclusiveShape(
+            actor,
+            pxGeometry,
+            material,
+            shapeFlags
+        )
+        shape.setSimulationFilterData(pxFilterData)
+        return shape
+    }
 }
