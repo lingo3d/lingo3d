906596f308c8f545b50d545720dfbc80cf330f1c
lingosandi <lai@lingo3d.com>
2023-01-19 16:39:34 +0800

    limit configJointSystem

--- lingo3d/src/display/joints/PrismaticJoint.ts
+++ lingo3d/src/display/joints/PrismaticJoint.ts
@@ -1,15 +1,35 @@
+import { deg2Rad } from "@lincode/math"
+import { CM2M } from "../../globals"
 import IPrismaticJoint, {
     prismaticJointDefaults,
     prismaticJointSchema
 } from "../../interface/IPrismaticJoint"
+import debounceSystem from "../../utils/debounceSystem"
 import JointBase from "../core/JointBase"
 import PhysicsObjectManager from "../core/PhysicsObjectManager"
+import destroy from "../core/PhysicsObjectManager/physx/destroy"
 import { physxPtr } from "../core/PhysicsObjectManager/physx/physxPtr"
 
 const createPrismatic = (actor0: any, pose0: any, actor1: any, pose1: any) => {
     const { physics, Px } = physxPtr[0]
     return Px.PrismaticJointCreate(physics, actor0, pose0, actor1, pose1)
 }
+
+const configJointSystem = debounceSystem((target: PrismaticJoint) => {
+    const { pxJoint, limitLow, limitHigh } = target
+    if (!pxJoint) return
+
+    const { PxJointLinearLimitPair } = physxPtr[0]
+
+    const linearLimit = new PxJointLinearLimitPair(
+        limitLow * CM2M,
+        limitHigh * CM2M
+    )
+    // linearLimit.stiffness = linearStiffnessX
+    // linearLimit.damping = linearDampingX
+    // pxJoint.setLinearLimit(PxD6AxisEnum.eX(), linearLimit)
+    destroy(linearLimit)
+})
 
 export default class PrismaticJoint
     extends JointBase
@@ -31,5 +51,23 @@ export default class PrismaticJoint
             toManager.actor,
             toPxTransform
         )
+    }
+
+    private _limitLow?: number
+    public get limitLow() {
+        return this._limitLow ?? -100
+    }
+    public set limitLow(val) {
+        this._limitLow = val
+        configJointSystem(this)
+    }
+
+    private _limitHigh?: number
+    public get limitHigh() {
+        return this._limitHigh ?? -100
+    }
+    public set limitHigh(val) {
+        this._limitHigh = val
+        configJointSystem(this)
     }
 }
--- lingo3d/src/interface/IPrismaticJoint.ts
+++ lingo3d/src/interface/IPrismaticJoint.ts
@@ -1,14 +1,27 @@
 import IJointBase, { jointBaseDefaults, jointBaseSchema } from "./IJointBase"
 import { extendDefaults } from "./utils/Defaults"
 import { ExtractProps } from "./utils/extractProps"
+import Range from "./utils/Range"
 
-export default interface IPrismaticJoint extends IJointBase {}
+export default interface IPrismaticJoint extends IJointBase {
+    limitLow: number
+    limitHigh: number
+}
 
 export const prismaticJointSchema: Required<ExtractProps<IPrismaticJoint>> = {
-    ...jointBaseSchema
+    ...jointBaseSchema,
+    limitLow: Number,
+    limitHigh: Number
 }
 
 export const prismaticJointDefaults = extendDefaults<IPrismaticJoint>(
     [jointBaseDefaults],
-    {}
+    {
+        limitLow: -100,
+        limitHigh: 100
+    },
+    {
+        limitLow: new Range(-200, 200),
+        limitHigh: new Range(-200, 200)
+    }
 )
