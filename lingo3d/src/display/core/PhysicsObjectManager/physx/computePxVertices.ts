00cef862cd713949fe090ee9b1fe8055bb34fca0
lingosandi <lai@lingo3d.com>
2023-01-24 00:16:12 +0800

    updates

--- lingo3d/src/display/core/PhysicsObjectManager/physx/computePxVertices.ts
+++ lingo3d/src/display/core/PhysicsObjectManager/physx/computePxVertices.ts
@@ -9,24 +9,18 @@ export default (manager: MeshAppendable | Loaded) => {
     let vertexCount = 0
     const geometries: Array<BufferGeometry> = []
 
-    if ("loadedObject3d" in manager) {
-        const loaded = manager.loadedObject3d!
-        loaded.updateMatrixWorld()
-        const { x, y, z } = manager.position
-        loaded.traverse((c: Object3D | Mesh) => {
-            if (!("geometry" in c)) return
-            const clone = c.geometry.clone()
-            clone.applyMatrix4(c.matrixWorld)
-            clone.translate(-x, -y, -z)
-            clone.dispose()
-            geometries.push(clone)
-            vertexCount += clone.attributes.position.count
-        })
-    } else if ("geometry" in manager.object3d) {
-        const { geometry } = manager.object3d
-        geometries.push(geometry)
-        vertexCount += geometry.attributes.position.count
-    }
+    const loaded =
+        "loadedObject3d" in manager ? manager.loadedObject3d! : manager.object3d
+    const { x, y, z } = manager.position
+    loaded.traverse((c: Object3D | Mesh) => {
+        if (!("geometry" in c)) return
+        const clone = c.geometry.clone()
+        clone.applyMatrix4(c.matrixWorld)
+        clone.translate(-x, -y, -z)
+        clone.dispose()
+        geometries.push(clone)
+        vertexCount += clone.attributes.position.count
+    })
 
     const vec3Vector = new Vector_PxVec3(vertexCount)
 
