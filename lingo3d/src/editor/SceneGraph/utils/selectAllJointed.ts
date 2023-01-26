4513141d4250af5697e0bee15f4887599481b542
lingosandi <lai@lingo3d.com>
2023-01-27 04:04:30 +0800

    selectAllJointed

--- lingo3d/src/editor/SceneGraph/utils/selectAllJointed.ts
+++ lingo3d/src/editor/SceneGraph/utils/selectAllJointed.ts
@@ -1,5 +1,33 @@
 import PhysicsObjectManager from "../../../display/core/PhysicsObjectManager"
+import { joints } from "../../../display/core/JointBase"
+import { addMultipleSelectionTargets } from "../../../states/useMultipleSelectionTargets"
+
+const findAllJointed = (
+    target: PhysicsObjectManager,
+    jointedSet = new Set<PhysicsObjectManager>(),
+    jointsCopy = new Set(joints)
+) => {
+    jointedSet.add(target)
+    for (const joint of jointsCopy)
+        if (joint.fromManager === target) {
+            jointsCopy.delete(joint)
+            addMultipleSelectionTargets(joint)
+            if (joint.toManager) {
+                jointedSet.add(joint.toManager)
+                findAllJointed(joint.toManager, jointedSet, jointsCopy)
+            }
+        } else if (joint.toManager === target) {
+            jointsCopy.delete(joint)
+            addMultipleSelectionTargets(joint)
+            if (joint.fromManager) {
+                jointedSet.add(joint.fromManager)
+                findAllJointed(joint.fromManager, jointedSet, jointsCopy)
+            }
+        }
+    return jointedSet
+}
 
 export default (target: PhysicsObjectManager) => {
-    
+    for (const manager of findAllJointed(target))
+        addMultipleSelectionTargets(manager)
 }
