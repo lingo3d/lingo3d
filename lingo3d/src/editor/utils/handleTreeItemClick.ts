204cc04df8c2ac4117e8395d2a2d04e7a7500825
lingosandi <lai@lingo3d.com>
2023-01-31 22:10:07 +0800

    nativeParent

--- lingo3d/src/editor/utils/handleTreeItemClick.ts
+++ lingo3d/src/editor/utils/handleTreeItemClick.ts
@@ -1,23 +1,21 @@
 import { Object3D } from "three"
 import Appendable from "../../api/core/Appendable"
 import { toggleRightClickPtr } from "../../api/mouse"
-import { isPositionedManager } from "../../display/core/PositionedManager"
 import { emitSelectionTarget } from "../../events/onSelectionTarget"
 import { setSelectionNativeTarget } from "../../states/useSelectionNativeTarget"
-import { getSelectionTarget } from "../../states/useSelectionTarget"
 import { setWorldPlay } from "../../states/useWorldPlay"
 
 export default (
     target?: Appendable | Object3D,
     rightClick?: boolean,
-    parent?: Appendable
+    nativeParent?: Appendable
 ) => {
     setWorldPlay(false)
     queueMicrotask(() => {
         rightClick && toggleRightClickPtr()
-        if (isPositionedManager(parent) && getSelectionTarget() !== parent)
-            emitSelectionTarget(parent, true)
-        if (target instanceof Object3D) setSelectionNativeTarget(target)
-        else emitSelectionTarget(target, true)
+        if (target instanceof Object3D) {
+            emitSelectionTarget(nativeParent, true)
+            setSelectionNativeTarget(target)
+        } else emitSelectionTarget(target, true)
     })
 }
