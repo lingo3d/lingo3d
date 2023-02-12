176fab7268e01d65b412028ed68af2cb6463d7fd
lingosandi <lai@lingo3d.com>
2023-02-12 21:09:11 +0800

    updates

--- lingo3d/src/editor/Editor/icons/connectorIcon.ts
+++ lingo3d/src/editor/Editor/icons/connectorIcon.ts
@@ -2,6 +2,6 @@ import createElement from "../../../utils/createElement"
 
 export default createElement(`
     <div style="width: 10px; height: 10px" class="lingo3d-flexcenter">
-        <div style="width: 4px; height: 4px; background: white"></div>
+        <div style="width: 4px; height: 4px; background: white; pointer-events: none"></div>
     </div>
 `)
