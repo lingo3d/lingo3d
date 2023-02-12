d2d549ad4d5e061a077d2e50d85c631091f4ff11
lingosandi <lai@lingo3d.com>
2023-02-12 22:21:23 +0800

    memo

--- lingo3d/src/editor/GameGraphEditor/Node.tsx
+++ lingo3d/src/editor/GameGraphEditor/Node.tsx
@@ -1,5 +1,6 @@
 import { Point } from "@lincode/math"
 import { valueof } from "@lincode/utils"
+import { memo } from "preact/compat"
 import { useEffect, useMemo, useState } from "preact/hooks"
 import { uuidMap } from "../../api/core/collections"
 import { EDITOR_WIDTH } from "../../globals"
@@ -20,93 +21,103 @@ type NodeProps = {
     uuid: string
     data: valueof<GameGraphData>
     onPan?: (e: PanEvent) => void
+    getContainerBounds: () => DOMRect
 }
 
-const Node = ({ uuid, data, onPan }: NodeProps) => {
-    const manager = useMemo(() => uuidMap.get(uuid), [uuid])
-    const displayName = useMemo(
-        () => manager && getDisplayName(manager),
-        [manager]
-    )
-    const pressRef = usePan({ onPan })
-    const [pane, setContainer, container] = usePane()
-    const [includeKeys, setIncludeKeys] = useState<Array<string>>([])
-    const [bezierStart, setBezierStart] = useState<Point>()
-    const [bezierEnd, setBezierEnd] = useState<Point>()
+const Node = memo(
+    ({ uuid, data, onPan, getContainerBounds }: NodeProps) => {
+        console.log("rerender")
 
-    const bezierBounds = useMemo(() => {
-        if (!bezierStart || !bezierEnd) return
-        return {
-            xMin: Math.min(bezierStart.x, bezierEnd.x),
-            xMax: Math.max(bezierStart.x, bezierEnd.x),
-            yMin: Math.min(bezierStart.y, bezierEnd.y),
-            yMax: Math.max(bezierStart.y, bezierEnd.y)
-        }
-    }, [bezierStart, bezierEnd])
+        const manager = useMemo(() => uuidMap.get(uuid), [uuid])
+        const displayName = useMemo(
+            () => manager && getDisplayName(manager),
+            [manager]
+        )
+        const pressRef = usePan({ onPan })
+        const [pane, setContainer, container] = usePane()
+        const [includeKeys, setIncludeKeys] = useState<Array<string>>([])
+        const [bezierStart, setBezierStart] = useState<Point>()
+        const [bezierEnd, setBezierEnd] = useState<Point>()
 
-    useEffect(() => {
-        if (!manager || !pane) return
-        const handle = addTargetInputs(pane, manager, includeKeys, true, {
-            onDragStart: (e) => setBezierStart(getPosition(e, container)),
-            onDrag: (e) => setBezierEnd(getPosition(e, container)),
-            onDragEnd: (e) => {
-                setBezierStart(undefined)
-                setBezierEnd(undefined)
-            },
-            onDrop: (e, draggingItem) => {}
-        })
-        return () => {
-            handle.cancel()
-        }
-    }, [manager, includeKeys, pane])
+        const bezierBounds = useMemo(() => {
+            if (!bezierStart || !bezierEnd) return
+            return {
+                xMin: Math.min(bezierStart.x, bezierEnd.x),
+                xMax: Math.max(bezierStart.x, bezierEnd.x),
+                yMin: Math.min(bezierStart.y, bezierEnd.y),
+                yMax: Math.max(bezierStart.y, bezierEnd.y)
+            }
+        }, [bezierStart, bezierEnd])
 
-    return (
-        <>
-            <div
-                style={{
-                    width: EDITOR_WIDTH,
-                    minHeight: 100,
-                    position: "absolute",
-                    left: data.x,
-                    top: data.y
-                }}
-            >
-                <div style={{ fontSize: 16, marginTop: -24 }} ref={pressRef}>
-                    {displayName}
-                </div>
-                <SearchBox
-                    fullWidth
-                    style={{ marginTop: 8 }}
-                    onChange={(val) => {
-                        if (!val || !manager) {
-                            setIncludeKeys([])
-                            return
-                        }
-                        val = val.toLowerCase()
-                        setIncludeKeys(
-                            Object.keys(
-                                unsafeGetValue(manager, "constructor").schema
-                            ).filter((key) => key.toLowerCase().includes(val))
-                        )
-                    }}
-                />
-                <div ref={setContainer} style={{ width: "100%" }} />
-            </div>
-            {bezierBounds && (
-                <svg
-                    ref={console.log}
+        useEffect(() => {
+            if (!manager || !pane) return
+            const handle = addTargetInputs(pane, manager, includeKeys, true, {
+                onDragStart: (e) => setBezierStart(getPosition(e, container)),
+                onDrag: (e) => setBezierEnd(getPosition(e, container)),
+                onDragEnd: (e) => {
+                    // setBezierStart(undefined)
+                    // setBezierEnd(undefined)
+                },
+                onDrop: (e, draggingItem) => {}
+            })
+            return () => {
+                handle.cancel()
+            }
+        }, [manager, includeKeys, pane])
+
+        return (
+            <>
+                <div
                     style={{
-                        left: bezierBounds.xMin,
-                        top: bezierBounds.yMin,
+                        width: EDITOR_WIDTH,
+                        minHeight: 100,
                         position: "absolute",
-                        background: "yellow"
+                        left: data.x,
+                        top: data.y
                     }}
-                    width={bezierBounds.xMax - bezierBounds.xMin}
-                    height={bezierBounds.yMax - bezierBounds.yMin}
-                />
-            )}
-        </>
-    )
-}
-
+                >
+                    <div
+                        style={{ fontSize: 16, marginTop: -24 }}
+                        ref={pressRef}
+                    >
+                        {displayName}
+                    </div>
+                    <SearchBox
+                        fullWidth
+                        style={{ marginTop: 8 }}
+                        onChange={(val) => {
+                            if (!val || !manager) {
+                                setIncludeKeys([])
+                                return
+                            }
+                            val = val.toLowerCase()
+                            setIncludeKeys(
+                                Object.keys(
+                                    unsafeGetValue(manager, "constructor")
+                                        .schema
+                                ).filter((key) =>
+                                    key.toLowerCase().includes(val)
+                                )
+                            )
+                        }}
+                    />
+                    <div ref={setContainer} style={{ width: "100%" }} />
+                </div>
+                {bezierBounds && (
+                    <svg
+                        style={{
+                            left: bezierBounds.xMin + getContainerBounds().left,
+                            top: bezierBounds.yMin + getContainerBounds().top,
+                            position: "absolute",
+                            background: "yellow"
+                        }}
+                        width={bezierBounds.xMax - bezierBounds.xMin}
+                        height={bezierBounds.yMax - bezierBounds.yMin}
+                    />
+                )}
+            </>
+        )
+    },
+    () => true
+)
 export default Node
--- lingo3d/src/editor/GameGraphEditor/index.tsx
+++ lingo3d/src/editor/GameGraphEditor/index.tsx
@@ -132,6 +132,7 @@ const GameGraphEditor = () => {
                                         }
                                     })
                                 }
+                                getContainerBounds={getContainerBounds}
                             />
                         ))}
                     </div>
