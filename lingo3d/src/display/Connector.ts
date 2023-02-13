7ed7249e4b16233fb7096762be1195e5b36e752e
lingosandi <lai@lingo3d.com>
2023-02-14 03:42:07 +0800

    refactored getReactive

--- lingo3d/src/display/Connector.ts
+++ lingo3d/src/display/Connector.ts
@@ -1,61 +1,7 @@
 import { Reactive } from "@lincode/reactivity"
 import Appendable, { getAppendables } from "../api/core/Appendable"
-import unsafeGetValue from "../utils/unsafeGetValue"
+import getReactive from "../utils/getReactive"
 import unsafeSetValue from "../utils/unsafeSetValue"
-
-const prototypeDescriptorMap = new WeakMap<object, PropertyDescriptor>()
-
-const getPropertyDescriptor = (
-    obj: object,
-    key: string,
-    traversed: Array<object> = []
-): PropertyDescriptor | undefined => {
-    if (!obj) return
-    if (prototypeDescriptorMap.has(obj)) return prototypeDescriptorMap.get(obj)
-    traversed.push(obj)
-    const desc = Object.getOwnPropertyDescriptor(obj, key)
-    if (desc) {
-        for (const obj of traversed) prototypeDescriptorMap.set(obj, desc)
-        return desc
-    }
-    return getPropertyDescriptor(Object.getPrototypeOf(obj), key, traversed)
-}
-
-const getReactive = (manager: Appendable, key: string): Reactive<any> => {
-    const stateKey = `${key}State`
-    let reactive = unsafeGetValue(manager, stateKey)
-    if (reactive) return reactive
-
-    unsafeSetValue(
-        manager,
-        stateKey,
-        (reactive = new Reactive<any>(unsafeGetValue(manager, key)))
-    )
-
-    const desc = getPropertyDescriptor(manager, key)
-    if (!desc) return reactive
-
-    if ("value" in desc)
-        Object.defineProperty(manager, key, {
-            get() {
-                return reactive.get()
-            },
-            set(value) {
-                reactive.set(value)
-            }
-        })
-    else
-        Object.defineProperty(manager, key, {
-            get() {
-                return desc.get!.call(manager)
-            },
-            set(value) {
-                reactive.set(value)
-                desc.set!.call(manager, value)
-            }
-        })
-    return reactive
-}
 
 export default class Connector extends Appendable {
     public constructor() {
---
+++ lingo3d/src/utils/getReactive.ts
@@ -0,0 +1,59 @@
+import { Reactive } from "@lincode/reactivity"
+import { assert } from "@lincode/utils"
+import Appendable from "../api/core/Appendable"
+import unsafeGetValue from "./unsafeGetValue"
+import unsafeSetValue from "./unsafeSetValue"
+
+const prototypeDescriptorMap = new WeakMap<object, PropertyDescriptor>()
+
+const getPropertyDescriptor = (
+    obj: object,
+    key: string,
+    traversed: Array<object> = []
+): PropertyDescriptor | undefined => {
+    if (!obj) return
+    if (prototypeDescriptorMap.has(obj)) return prototypeDescriptorMap.get(obj)
+    traversed.push(obj)
+    const desc = Object.getOwnPropertyDescriptor(obj, key)
+    if (desc) {
+        for (const obj of traversed) prototypeDescriptorMap.set(obj, desc)
+        return desc
+    }
+    return getPropertyDescriptor(Object.getPrototypeOf(obj), key, traversed)
+}
+
+export default (manager: Appendable, key: string): Reactive<any> => {
+    const stateKey = `${key}State`
+    let reactive = unsafeGetValue(manager, stateKey)
+    if (reactive) return reactive
+
+    unsafeSetValue(
+        manager,
+        stateKey,
+        (reactive = new Reactive<any>(unsafeGetValue(manager, key)))
+    )
+
+    const desc = getPropertyDescriptor(manager, key)
+    assert(desc, `Property "${key}" not found`)
+
+    if ("value" in desc)
+        Object.defineProperty(manager, key, {
+            get() {
+                return reactive.get()
+            },
+            set(value) {
+                reactive.set(value)
+            }
+        })
+    else
+        Object.defineProperty(manager, key, {
+            get() {
+                return desc.get!.call(manager)
+            },
+            set(value) {
+                reactive.set(value)
+                desc.set!.call(manager, value)
+            }
+        })
+    return reactive
+}
