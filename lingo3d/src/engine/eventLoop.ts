598881958cac5df2c54198e6656d21435505ed78
lingosandi <lai@lingo3d.com>
2023-02-21 17:55:30 +0800

    removed paused

--- lingo3d/src/engine/eventLoop.ts
+++ lingo3d/src/engine/eventLoop.ts
@@ -1,12 +1,11 @@
 import { Cancellable } from "@lincode/promiselikes"
-import store, { createEffect } from "@lincode/reactivity"
+import { createEffect } from "@lincode/reactivity"
 import { Clock } from "three"
 import { getRenderer } from "../states/useRenderer"
 import { getFps } from "../states/useFps"
 import { getFirstLoad } from "../states/useFirstLoad"
 import { getFirstLoadBeforeRender } from "../states/useFirstLoadBeforeRender"
 import { emitRenderHalfRate } from "../events/onRenderHalfRate"
-import { setPaused } from "../states/usePaused"
 import { SEC2FRAME } from "../globals"
 import { getWorldPlayComputed } from "../states/useWorldPlayComputed"
 import { onAfterRender } from "../events/onAfterRender"
@@ -17,44 +16,8 @@ const handle = onAfterRender(() => {
     handle.cancel()
 })
 
-let paused = false
-const checkPaused = (val?: boolean) =>
-    setPaused(
-        (paused = firstRender
-            ? val ?? (document.hidden || !document.hasFocus())
-            : false)
-    )
-
 let play = true
 getWorldPlayComputed((val) => (play = val))
-
-const [setLocked, getLocked] = store(false)
-createEffect(() => {
-    const locked = getLocked()
-    if (locked) return
-
-    const cb0 = () => checkPaused(true)
-    const cb1 = () => checkPaused()
-    const cb2 = () => {
-        checkPaused(false)
-        setLocked(true)
-        setTimeout(() => setLocked(false), 1000)
-    }
-    window.addEventListener("blur", cb0)
-    window.addEventListener("focus", cb1)
-    document.addEventListener("visibilitychange", cb1)
-    window.addEventListener("resize", cb2)
-    const interval = setInterval(cb1, 1000)
-    cb1()
-
-    return () => {
-        window.removeEventListener("blur", cb0)
-        window.removeEventListener("focus", cb1)
-        document.removeEventListener("visibilitychange", cb1)
-        window.removeEventListener("resize", cb2)
-        clearInterval(interval)
-    }
-}, [getLocked])
 
 export const timer = (
     time: number,
@@ -64,7 +27,7 @@ export const timer = (
 ) => {
     let count = 0
     const handle = setInterval(() => {
-        if (paused || !(play || editor)) return
+        if (!play && !editor) return
         cb()
         if (repeat !== -1 && ++count >= repeat) clearInterval(handle)
     }, time)
@@ -88,7 +51,6 @@ createEffect(() => {
     const targetDelta = (1 / getFps()) * 0.9
 
     renderer.setAnimationLoop(() => {
-        if (paused) return
         delta += clock.getDelta()
         if (delta > 0.2) delta = 0
         if (delta < targetDelta) return
