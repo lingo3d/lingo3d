5c0a66fe1965aaafbedeb77f19c8da733a6b7e3a
lingosandi <lai@lingo3d.com>
2023-02-22 17:50:01 +0800

    MathNode updates

--- lingo3d/src/tests/testGameGraph.ts
+++ lingo3d/src/tests/testGameGraph.ts
@@ -1,4 +1,3 @@
-import { setAssetsPath } from "../api/assetsPath"
 import Dummy from "../display/Dummy"
 import Joystick from "../ui/Joystick"
 import MathNode from "../visualScripting/MathNode"
@@ -7,5 +6,5 @@ const dummy = new Dummy()
 
 const joystick = new Joystick()
 
-// const mathnode = new MathNode()
-// mathnode.expression = "hello.world = 3"
+const mathnode = new MathNode()
+mathnode.expression = "hello.world = 3"
--- lingo3d/src/visualScripting/MathNode.ts
+++ lingo3d/src/visualScripting/MathNode.ts
@@ -4,6 +4,27 @@ import IMathNode, {
     mathNodeSchema
 } from "../interface/IMathNode"
 
+const numbers = "0123456789._"
+const operators = "+-*/="
+const parentheses = "()"
+const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+const typeMap = new Map<
+    string,
+    "number" | "operator" | "parenthesis" | "letter"
+>()
+for (const char of numbers) typeMap.set(char, "number")
+for (const char of operators) typeMap.set(char, "operator")
+for (const char of parentheses) typeMap.set(char, "parenthesis")
+for (const char of letters) typeMap.set(char, "letter")
+
+class Token {
+    public constructor(
+        public type: "number" | "operator" | "parenthesis" | "letter",
+        public value: string
+    ) {}
+}
+
 export default class MathNode extends Appendable implements IMathNode {
     public static componentName = "mathNode"
     public static defaults = mathNodeDefaults
@@ -16,6 +37,28 @@ export default class MathNode extends Appendable implements IMathNode {
     }
     public set expression(val) {
         this._expression = val
+        if (!val) return
+
+        let typeOld = ""
+        let tokenValue = ""
+        const tokens: Array<Token> = []
+        for (const char of val) {
+            if (char === " ") continue
+            const type = typeMap.get(char)
+            if (!type) {
+                console.warn(
+                    `invalid character "${char}" in expression "${val}"`
+                )
+                return
+            }
+            if (typeOld === type) tokenValue += char
+            else if (tokenValue) {
+                tokens.push(new Token(typeOld as any, tokenValue))
+                tokenValue = char
+            } else tokenValue = char
+            typeOld = type
+        }
+        console.log(tokens)
     }
 
     public evaluate(scope?: Record<string, number>) {}
