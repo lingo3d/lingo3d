cf26a1fb191dc33dfe135afc439e42e7fdb00929
lingosandi <lai@lingo3d.com>
2023-02-23 16:25:05 +0800

    updates

--- lingo3d/src/interface/IMathNode.ts
+++ lingo3d/src/interface/IMathNode.ts
@@ -5,17 +5,20 @@ import IAppendable, {
 import { extendDefaults } from "./utils/Defaults"
 import { ExtractProps } from "./utils/extractProps"
 import Nullable from "./utils/Nullable"
+import { nullableDefault } from "./utils/NullableDefault"
 
 export default interface IMathNode extends IAppendable {
     expression: Nullable<string>
+    output: Nullable<number>
 }
 
 export const mathNodeSchema: Required<ExtractProps<IMathNode>> = {
     ...appendableSchema,
-    expression: String
+    expression: String,
+    output: Number
 }
 
 export const mathNodeDefaults = extendDefaults<IMathNode>(
     [appendableDefaults],
-    { expression: undefined }
+    { expression: undefined, output: nullableDefault(0) }
 )
