193b3569c83aaab15edca641418b82704d4396fa
lingosandi <lai@lingo3d.com>
2023-02-26 04:38:19 +0800

    updates

--- lingo3d/src/editor/GameGraphEditor/index.tsx
+++ lingo3d/src/editor/GameGraphEditor/index.tsx
@@ -5,11 +5,14 @@ import { APPBAR_HEIGHT, EDITOR_WIDTH, LIBRARY_WIDTH } from "../../globals"
 import { setGameGraph } from "../../states/useGameGraph"
 import AppBar from "../component/bars/AppBar"
 import CloseableTab from "../component/tabs/CloseableTab"
+import addTargetInputs from "../Editor/addTargetInputs"
+import usePane from "../Editor/usePane"
 import useInitCSS from "../hooks/useInitCSS"
 import useInitEditor from "../hooks/useInitEditor"
 import Library from "../Library"
 import LibraryIcon from "./icons/LibraryIcon"
 import Stage from "./Stage"
+import SearchBox from "../component/SearchBox"
 
 const GameGraphEditor = () => {
     useInitCSS()
@@ -18,6 +21,16 @@ const GameGraphEditor = () => {
     const [showLibrary, setShowLibrary] = useState(true)
     const selectedSignal = useSignal<string | undefined>(undefined)
     const [editTarget, setEditTarget] = useState<Appendable>()
+    const [pane, setContainer] = usePane()
+
+    useEffect(() => {
+        if (!editTarget || !pane) return
+
+        const handle = addTargetInputs(pane, editTarget, undefined)
+        return () => {
+            handle.cancel()
+        }
+    }, [editTarget, pane])
 
     useEffect(() => {
         const timeout = setTimeout(() => setShowLibrary(false), 500)
@@ -44,6 +57,22 @@ const GameGraphEditor = () => {
                     onPanStart={() => setShowLibrary(false)}
                     onEdit={setEditTarget}
                 />
+                <div
+                    style={{
+                        height: "100%",
+                        width: EDITOR_WIDTH,
+                        position: "absolute",
+                        left: 0,
+                        transition: "transform 500ms",
+                        transform: `translateX(${editTarget ? 0 : -100}%)`,
+                        background: `rgb(${18 * 0.75}, ${19 * 0.75}, ${
+                            22 * 0.75
+                        })`
+                    }}
+                >
+                    <SearchBox />
+                    <div ref={setContainer} />
+                </div>
                 <div
                     style={{
                         height: "100%",
