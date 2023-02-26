e5647ab50d9a3ba351bcbad30936d9fd1384caaf
lingosandi <lai@lingo3d.com>
2023-02-26 17:01:29 +0800

    updates

--- lingo3d/src/editor/GameGraphEditor/GameGraphEditPanel.tsx
+++ lingo3d/src/editor/GameGraphEditor/GameGraphEditPanel.tsx
@@ -24,6 +24,7 @@ const GameGraphEditPanel = ({ targetSignal }: GameGraphEditPanelProps) => {
 
     return (
         <div
+            className="lingo3d-flexcol"
             style={{
                 height: "100%",
                 width: EDITOR_WIDTH,
@@ -31,12 +32,42 @@ const GameGraphEditPanel = ({ targetSignal }: GameGraphEditPanelProps) => {
                 left: 0,
                 transition: "transform 500ms",
                 transform: `translateX(${targetSignal.value ? 0 : -100}%)`,
-                background: `rgb(${18 * 0.75}, ${19 * 0.75}, ${22 * 0.75})`,
-                padding: 8
+                background: `rgb(${18 * 0.75}, ${19 * 0.75}, ${22 * 0.75})`
             }}
         >
-            <SearchBox fullWidth />
-            <div ref={setContainer} />
+            <div
+                style={{
+                    display: "flex",
+                    gap: 6,
+                    padding: 8,
+                    paddingBottom: 0
+                }}
+            >
+                <SearchBox
+                    fullWidth
+                    style={{ width: undefined, flexGrow: 1 }}
+                />
+                <div
+                    className="lingo3d-flexcenter"
+                    style={{
+                        height: 22,
+                        padding: 4,
+                        background: "rgba(255, 255, 255, 0.1)"
+                    }}
+                    onClick={() => (targetSignal.value = undefined)}
+                >
+                    close
+                </div>
+            </div>
+            <div
+                ref={setContainer}
+                style={{
+                    overflowY: "scroll",
+                    flexGrow: 1,
+                    paddingLeft: 8,
+                    paddingRight: 8
+                }}
+            />
         </div>
     )
 }
