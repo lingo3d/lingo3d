413acb3364e8245964b11bf25dd4381ed96c9a48
lingosandi <lai@lingo3d.com>
2023-02-27 22:09:23 +0800

    click to hide

--- lingo3d/src/editor/GameGraphEditor/GameGraphEditPanel.tsx
+++ lingo3d/src/editor/GameGraphEditor/GameGraphEditPanel.tsx
@@ -31,43 +31,58 @@ const GameGraphEditPanel = ({ targetSignal }: GameGraphEditPanelProps) => {
     }, [targetSignal.value, pane, includeKeys])
 
     return (
-        <div
-            className="lingo3d-flexcol lingo3d-bg"
-            style={{
-                height: "100%",
-                width: EDITOR_WIDTH + 50,
-                position: "absolute",
-                left: 0,
-                transition: "transform 500ms",
-                transform: `translateX(${targetSignal.value ? 0 : -100}%)`
-            }}
-        >
-            <SearchBox
-                style={{ marginTop: 8 }}
-                onChange={(val) => {
-                    if (!val || !targetSignal.value) {
-                        setIncludeKeys(undefined)
-                        return
-                    }
-                    val = val.toLowerCase()
-                    setIncludeKeys(
-                        Object.keys(
-                            unsafeGetValue(targetSignal.value, "constructor")
-                                .schema
-                        ).filter((key) => key.toLowerCase().includes(val))
-                    )
+        <div className="lingo3d-absfull" style={{ pointerEvents: "none" }}>
+            <div
+                className="lingo3d-absfull"
+                style={{
+                    background: "black",
+                    opacity: targetSignal.value ? 0.5 : 0,
+                    transition: "opacity 500ms",
+                    pointerEvents: targetSignal.value ? "auto" : "none"
                 }}
-                clearOnChange={targetSignal.value}
+                onClick={() => (targetSignal.value = undefined)}
             />
             <div
-                ref={setContainer}
+                className="lingo3d-flexcol lingo3d-bg"
                 style={{
-                    overflowY: "scroll",
-                    flexGrow: 1,
-                    paddingLeft: 8,
-                    paddingRight: 8
+                    height: "100%",
+                    width: EDITOR_WIDTH + 50,
+                    position: "absolute",
+                    left: 0,
+                    transition: "transform 500ms",
+                    transform: `translateX(${targetSignal.value ? 0 : -100}%)`,
+                    pointerEvents: "auto"
                 }}
-            />
+            >
+                <SearchBox
+                    style={{ marginTop: 8 }}
+                    onChange={(val) => {
+                        if (!val || !targetSignal.value) {
+                            setIncludeKeys(undefined)
+                            return
+                        }
+                        val = val.toLowerCase()
+                        setIncludeKeys(
+                            Object.keys(
+                                unsafeGetValue(
+                                    targetSignal.value,
+                                    "constructor"
+                                ).schema
+                            ).filter((key) => key.toLowerCase().includes(val))
+                        )
+                    }}
+                    clearOnChange={targetSignal.value}
+                />
+                <div
+                    ref={setContainer}
+                    style={{
+                        overflowY: "scroll",
+                        flexGrow: 1,
+                        paddingLeft: 8,
+                        paddingRight: 8
+                    }}
+                />
+            </div>
         </div>
     )
 }
