0652b6b992dd2233684c71165d651f95eccfd98d
lingosandi <lai@lingo3d.com>
2023-03-06 22:52:06 +0800

    updates

--- lingo3d/src/editor/Editor/icons/connectorOutIcon.ts
+++ lingo3d/src/editor/Editor/icons/connectorOutIcon.ts
@@ -1,7 +1,7 @@
 import createElement from "../../../utils/createElement"
 
 export default createElement(`
-    <div style="width: 14px; height: 14px; margin-left: 2px; flex-shrink: 0; z-index: 1; cursor: pointer" class="lingo3d-flexcenter">
+    <div style="width: 14px; height: 14px; margin-left: 2px; margin-right: 2px; flex-shrink: 0; z-index: 1; cursor: pointer" class="lingo3d-flexcenter">
         <div style="width: 4px; height: 4px; background: white; pointer-events: none"></div>
     </div>
 `)
