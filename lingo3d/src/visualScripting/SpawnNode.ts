2490d217e5153633ec979070e0240bb4e9490752
lingosandi <lai@lingo3d.com>
2023-03-08 22:27:04 +0800

    spawn function

--- lingo3d/src/visualScripting/SpawnNode.ts
+++ lingo3d/src/visualScripting/SpawnNode.ts
@@ -1,13 +1,20 @@
 import { omit } from "@lincode/utils"
+import Appendable from "../api/core/Appendable"
 import createObject from "../api/serializer/createObject"
 import nonSerializedProperties from "../api/serializer/nonSerializedProperties"
 import { serializeAppendable } from "../api/serializer/serialize"
+import { AppendableNode, GameObjectType } from "../api/serializer/types"
 import ISpawnNode, {
     spawnNodeDefaults,
     spawnNodeSchema
 } from "../interface/ISpawnNode"
 import { findConnected } from "./Connector"
 import GameGraphChild from "./GameGraphChild"
+
+const spawnCache = new WeakMap<
+    Appendable,
+    [GameObjectType, Partial<AppendableNode>]
+>()
 
 export default class SpawnNode extends GameGraphChild implements ISpawnNode {
     public static componentName = "spawnNode"
@@ -17,9 +24,15 @@ export default class SpawnNode extends GameGraphChild implements ISpawnNode {
 
     public spawn() {
         for (const connected of findConnected(this)) {
+            if (spawnCache.has(connected)) {
+                const [type, properties] = spawnCache.get(connected)!
+                Object.assign(createObject(type), properties)
+                continue
+            }
             const node = serializeAppendable(connected, true)
-            const object = createObject(node.type)
-            Object.assign(object, omit(node, nonSerializedProperties))
+            const properties = omit(node, nonSerializedProperties)
+            Object.assign(createObject(node.type), properties)
+            spawnCache.set(connected, [node.type, properties])
         }
     }
 }
