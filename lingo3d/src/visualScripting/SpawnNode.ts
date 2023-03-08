9e392706fa4a35cc359c4c89a98396e793b5df4b
lingosandi <lai@lingo3d.com>
2023-03-09 01:17:12 +0800

    refactored spawn logic

--- lingo3d/src/visualScripting/SpawnNode.ts
+++ lingo3d/src/visualScripting/SpawnNode.ts
@@ -1,4 +1,5 @@
 import { omit } from "@lincode/utils"
+import Appendable from "../api/core/Appendable"
 import createObject from "../api/serializer/createObject"
 import nonSerializedProperties from "../api/serializer/nonSerializedProperties"
 import { serializeAppendable } from "../api/serializer/serialize"
@@ -10,22 +11,46 @@ import ISpawnNode, {
 import Connector, { findConnected, managerConnectorsMap } from "./Connector"
 import GameGraphChild from "./GameGraphChild"
 
+type CacheData = Array<[Appendable, GameObjectType, Partial<AppendableNode>]>
+type Cache = { data: CacheData; connectors: Set<Connector> }
+
+const spawnConnectors = (
+    connectors: Set<Connector>,
+    connectedUUIDs: Map<string, string>
+) => {
+    for (const { from, to, fromProp, toProp, xyz } of connectors)
+        Object.assign(new Connector(), {
+            from: connectedUUIDs.get(from!),
+            fromProp,
+            to: connectedUUIDs.get(to!),
+            toProp,
+            xyz
+        })
+}
+
+const spawnCached = (cache: Cache) => {
+    const { data, connectors } = cache
+    const connectedUUIDs = new Map<string, string>()
+    for (const [connected, type, properties] of data) {
+        const manager = Object.assign(createObject(type), properties)
+        connectedUUIDs.set(connected.uuid, manager.uuid)
+    }
+    spawnConnectors(connectors, connectedUUIDs)
+}
+
 export default class SpawnNode extends GameGraphChild implements ISpawnNode {
     public static componentName = "spawnNode"
     public static defaults = spawnNodeDefaults
     public static schema = spawnNodeSchema
     public static includeKeys = ["spawn"]
 
-    private cache?: Array<[GameObjectType, Partial<AppendableNode>]>
+    private cache?: Cache
 
     public spawn() {
-        if (this.cache) {
-            for (const [type, properties] of this.cache)
-                Object.assign(createObject(type), properties)
-            return
-        }
-        this.cache = []
-        const connectors = new Set<Connector>()
+        if (this.cache) return spawnCached(this.cache)
+        this.cache = { data: [], connectors: new Set<Connector>() }
+
+        const { data, connectors } = this.cache
         const connectedUUIDs = new Map<string, string>()
         for (const connected of findConnected(this)) {
             for (const connector of managerConnectorsMap.get(connected) ?? [])
@@ -35,15 +60,8 @@ export default class SpawnNode extends GameGraphChild implements ISpawnNode {
             const properties = omit(node, nonSerializedProperties)
             const manager = Object.assign(createObject(node.type), properties)
             connectedUUIDs.set(connected.uuid, manager.uuid)
-            this.cache.push([node.type, properties])
+            data.push([connected, node.type, properties])
         }
-        for (const { from, to, fromProp, toProp, xyz } of connectors)
-            Object.assign(new Connector(), {
-                from: connectedUUIDs.get(from!),
-                fromProp,
-                to: connectedUUIDs.get(to!),
-                toProp,
-                xyz
-            })
+        spawnConnectors(connectors, connectedUUIDs)
     }
 }
