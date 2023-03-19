e6b31a38c2a158ca5e18c50140cd82968c7b7507
lingosandi <lai@lingo3d.com>
2023-03-20 02:02:20 +0800

    templates

--- lingo3d/src/editor/Library/Templates.tsx
+++ lingo3d/src/editor/Library/Templates.tsx
@@ -1,5 +1,25 @@
+import { useMemo } from "preact/hooks"
+import { appendableRoot, hiddenAppendables } from "../../api/core/collections"
+import { isTemplate } from "../../display/Template"
+import useSceneGraphRefresh from "../hooks/useSceneGraphRefresh"
+import TreeItem from "../SceneGraph/TreeItem"
+
 const Templates = () => {
-    return <div>templates</div>
+    const refresh = useSceneGraphRefresh()
+    const appendables = useMemo(
+        () =>
+            [...appendableRoot].filter(
+                (item) => !hiddenAppendables.has(item) && isTemplate(item)
+            ),
+        [refresh]
+    )
+    return (
+        <>
+            {appendables.map((appendable) => (
+                <TreeItem key={appendable.uuid} appendable={appendable} />
+            ))}
+        </>
+    )
 }
 
 export default Templates
