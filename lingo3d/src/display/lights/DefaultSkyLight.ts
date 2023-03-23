0ad5b2ba0afc2a694be36325106e331454ee4276
lingosandi <lai@lingo3d.com>
2023-03-23 17:57:50 +0800

    updates

--- lingo3d/src/display/lights/DefaultSkyLight.ts
+++ lingo3d/src/display/lights/DefaultSkyLight.ts
@@ -25,7 +25,6 @@ export default class DefaultSkyLight
         super()
         this.y = FAR
         this.z = FAR
-        this.intensity = 0.5
         defaultSkyLight?.dispose()
         defaultSkyLight = this
     }
