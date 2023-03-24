3c610825cbf17478b24837698fc109a2aa6edd03
lingosandi <lai@lingo3d.com>
2023-03-25 03:08:33 +0800

    createJSON

--- lingo3d/src/api/files/createJSON.ts
+++ lingo3d/src/api/files/createJSON.ts
@@ -3,23 +3,34 @@ import { getFileBrowserDir } from "../../states/useFileBrowserDir"
 import { setFileCurrent } from "../../states/useFileCurrent"
 import { getFiles } from "../../states/useFiles"
 import unsafeSetValue from "../../utils/unsafeSetValue"
+import { VERSION } from "../../globals"
 
-const createJSON = async () => {
+export default async () => {
     const dir = getFileBrowserDir()
     const f = getFiles()?.find((f) => dirPath(f.webkitRelativePath) === dir)
     if (!f) return
 
+    const { default: prettier } = await import("prettier/standalone")
+    const { default: parser } = await import("prettier/parser-babel")
     const { fileSave } = await import("browser-fs-access")
 
-    const fileHandle = await fileSave(new Blob([""], { type: "text/plain" }), {
-        fileName: "scene.json",
-        startIn: f.directoryHandle as any
-    })
+    const code = prettier.format(
+        JSON.stringify([{ type: "lingo3d", version: VERSION }]),
+        {
+            parser: "json",
+            plugins: [parser]
+        }
+    )
+    const fileHandle = await fileSave(
+        new Blob([code], { type: "text/plain" }),
+        {
+            fileName: "scene.json",
+            startIn: f.directoryHandle as any
+        }
+    )
     const file = await fileHandle?.getFile()
     if (!file) return
 
     unsafeSetValue(file, "handle", fileHandle)
     setFileCurrent(file)
 }
-
-export default createJSON
