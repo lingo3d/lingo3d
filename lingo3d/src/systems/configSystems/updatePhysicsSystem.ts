eaab03add1ee9d79add6ad2e7fe80416e91f6f0b
lingosandi <lai@lingo3d.com>
2023-04-01 14:31:42 +0800

    updates

--- lingo3d/src/systems/configSystems/updatePhysicsSystem.ts
+++ lingo3d/src/systems/configSystems/updatePhysicsSystem.ts
@@ -1,9 +1,11 @@
 import MeshAppendable from "../../api/core/MeshAppendable"
 import PhysicsObjectManager from "../../display/core/PhysicsObjectManager"
 import configSystem from "../utils/configSystem"
+import { addTransformChangedSystem } from "./transformChangedSystem"
 
 export const [addUpdatePhysicsSystem] = configSystem(
     (self: MeshAppendable | PhysicsObjectManager) => {
+        addTransformChangedSystem(self)
         if (!("updatePhysicsTransform" in self)) return
         if (self.userData.updatePhysicsShape) {
             self.userData.updatePhysicsShape = false
