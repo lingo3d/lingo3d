e3538a7c85186ffb4f28803d88fda693431cb1fc
lingosandi <lai@lingo3d.com>
2023-04-01 17:10:14 +0800

    getFrustum

---
+++ lingo3d/src/display/utils/getFrustum.ts
@@ -0,0 +1,17 @@
+import { Frustum, PerspectiveCamera } from "three"
+import computePerFrame from "../../utils/computePerFrame"
+import { forceGetInstance } from "@lincode/utils"
+import { matrix4 } from "./reusables"
+
+const cameraFrustumMap = new WeakMap<PerspectiveCamera, Frustum>()
+
+export default computePerFrame((camera: PerspectiveCamera) => {
+    const frustum = forceGetInstance(cameraFrustumMap, camera, Frustum)
+    frustum.setFromProjectionMatrix(
+        matrix4.multiplyMatrices(
+            camera.projectionMatrix,
+            camera.matrixWorldInverse
+        )
+    )
+    return frustum
+})
