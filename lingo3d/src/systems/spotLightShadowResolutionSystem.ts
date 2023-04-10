f413327999abf47ed98dbb2099df7152e26198aa
lingosandi <lai@lingo3d.com>
2023-04-10 15:05:54 +0800

    SpotLightShadowResolutionSystem

--- lingo3d/src/systems/spotLightShadowResolutionSystem.ts
+++ lingo3d/src/systems/spotLightShadowResolutionSystem.ts
@@ -2,8 +2,8 @@ import renderSystemWithData from "./utils/renderSystemWithData"
 import { getDistanceFromCamera } from "../utilsCached/getDistanceFromCamera"
 import SpotLight from "../display/lights/SpotLight"
 
-const resolutions = [512, 256, 128, 32, 16, 512]
-const biases = [-0.01, -0.02, -0.03, -0.04, -0.05, -0.005]
+const resolutions = [1024, 512, 256, 128]
+const biases = [-0.003, -0.003, -0.003, -0.003]
 
 export const [
     addSpotLightShadowResolutionSystem,
@@ -11,16 +11,14 @@ export const [
 ] = renderSystemWithData(
     (self: SpotLight, data: { step: number | undefined }) => {
         const distance = getDistanceFromCamera(self)
-        let step = 4
-        if (self.distance > 3000) step = 5
-        else {
-            if (distance < 10) step = 0
-            else if (distance < 20) step = 1
-            else if (distance < 30) step = 2
-            else if (distance < 40) step = 3
-        }
+        let step = 3
+        if (distance < 10) step = 0
+        else if (distance < 20) step = 1
+        else if (distance < 30) step = 2
         if (step === data.step) return
         data.step = step
+
+        console.log(step)
 
         const { light } = self
         const { shadow } = light
