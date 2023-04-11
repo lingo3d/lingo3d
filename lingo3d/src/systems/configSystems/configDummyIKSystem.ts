18cebea178c8631e1b4eb91747317ad15893e51a
lingosandi <lai@lingo3d.com>
2023-04-12 01:51:49 +0800

    updates

--- lingo3d/src/systems/configSystems/configDummyIKSystem.ts
+++ lingo3d/src/systems/configSystems/configDummyIKSystem.ts
@@ -1,6 +1,7 @@
 import { uuidMap } from "../../collections/uuidCollections"
 import DummyIK from "../../display/DummyIK"
 import Model from "../../display/Model"
+import { indexChilrenNames } from "../../utilsCached/indexChildrenNames"
 import configSystem from "../utils/configSystem"
 
 export const [addConfigDummyIKSystem] = configSystem((self: DummyIK) => {
@@ -17,7 +18,15 @@ export const [addConfigDummyIKSystem] = configSystem((self: DummyIK) => {
         return
     }
 
+    const nameMeshMap = indexChilrenNames(dummy.loadedObject3d)
+
     if (hips && spine0) {
-        // const hipsMesh = dummy.loadedObject3d
+        const hipsMesh = nameMeshMap.get(hips)
+        const spine0Mesh = nameMeshMap.get(spine0)
+
+        if (hips && spine0) {
+            console.log("here")
+            console.log(hipsMesh, spine0Mesh)
+        }
     }
 })
--- lingo3d/src/tests/testCharacter.ts
+++ lingo3d/src/tests/testCharacter.ts
@@ -2,6 +2,3 @@ import Dummy from "../display/Dummy"
 
 const player = new Dummy()
 player.animationPaused = true
-player.onLoad = () => {
-    console.log("loaded")
-}
