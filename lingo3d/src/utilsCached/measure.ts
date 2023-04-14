1dd5b33adbf328be9f45ca40dad677496d2d96db
lingosandi <lai@lingo3d.com>
2023-04-14 16:34:39 +0800

    improved measure

--- lingo3d/src/utilsCached/measure.ts
+++ lingo3d/src/utilsCached/measure.ts
@@ -1,12 +1,39 @@
-import { Object3D, Vector3 } from "three"
-import { box3 } from "../display/utils/reusables"
+import { Box3, Object3D, SkinnedMesh, Vector3 } from "three"
+import { box3, vector3 } from "../display/utils/reusables"
 import computeOnceWithData from "./utils/computeOnceWithData"
+
+const _box = new Box3()
+
+const expandByObject = (object: any) => {
+    object.updateMatrixWorld()
+
+    const { geometry } = object
+    if (geometry) {
+        if (object instanceof SkinnedMesh) {
+            const position = geometry.attributes.position
+            for (let i = 0, il = position.count; i < il; i++) {
+                vector3.fromBufferAttribute(position, i)
+                object.boneTransform(i, vector3)
+                object.localToWorld(vector3)
+                box3.expandByPoint(vector3)
+            }
+        } else {
+            if (geometry.boundingBox === null) geometry.computeBoundingBox()
+            _box.copy(geometry.boundingBox)
+            _box.applyMatrix4(object.matrixWorld)
+            box3.union(_box)
+        }
+    }
+    const children = object.children
+    for (let i = 0, l = children.length; i < l; i++) expandByObject(children[i])
+}
 
 export const measure = computeOnceWithData(
     (_: string, data: { target: Object3D }) => {
         const size = new Vector3()
         const center = new Vector3()
-        box3.setFromObject(data.target)
+        box3.makeEmpty()
+        expandByObject(data.target)
         box3.getSize(size)
         box3.getCenter(center)
         return [size, center]
