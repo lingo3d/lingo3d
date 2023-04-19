86e1c5560fd60141cc372927ad9f6d01ced1366d
lingosandi <lai@lingo3d.com>
2023-04-19 14:20:32 +0800

    updates

--- lingo3d/src/systems/lightIntensitySystem.ts
+++ lingo3d/src/systems/lightIntensitySystem.ts
@@ -1,5 +1,4 @@
 import { mapRange } from "@lincode/math"
-import { CM2M } from "../globals"
 import { getDistanceFromCamera } from "../utilsCached/getDistanceFromCamera"
 import renderSystem from "./utils/renderSystem"
 import PointLight from "../display/lights/PointLight"
@@ -8,18 +7,10 @@ import SpotLight from "../display/lights/SpotLight"
 export const [addLightIntensitySystem, deleteLightIntensitySystem] =
     renderSystem((self: PointLight | SpotLight) => {
         const distance = getDistanceFromCamera(self)
-        self._intensityFactor = mapRange(
-            distance - self.distance * CM2M,
-            0,
-            50,
-            1,
-            0,
-            true
-        )
+        self._intensityFactor = mapRange(distance, 0, 50, 1, 0, true)
         self._enabledFactor = !!self._intensityFactor
         self.object3d.intensity = self.intensity * self._intensityFactor
         self.object3d.visible = !!(
             (self.enabled as any) * (self._enabledFactor as any)
         )
-        console.log(self.object3d.visible)
     })
