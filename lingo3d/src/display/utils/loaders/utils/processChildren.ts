7d7841cabec8d6593ccb9c83e35f23af451b6722
lingosandi <lai@lingo3d.com>
2023-04-21 00:24:12 +0800

    updates

--- lingo3d/src/display/utils/loaders/utils/processChildren.ts
+++ lingo3d/src/display/utils/loaders/utils/processChildren.ts
@@ -14,23 +14,18 @@ export default (group: Object3D, noBonePtr: [boolean]) => {
         if (child instanceof Light) lights.push(child)
         else if (noBonePtr[0] && child instanceof Bone) noBonePtr[0] = false
 
-        if ("material" in child) {
-            if (Array.isArray(child.material))
-                child.material = child.material[0]
+        if (!("material" in child)) return
+        if (Array.isArray(child.material)) child.material = child.material[0]
 
-            child.material.side = DoubleSide
+        child.material.side = DoubleSide
 
-            if (child.material.opacity === 1) {
-                child.castShadow = true
-                child.receiveShadow = true
-            }
-            child.material.userData.TextureManager = createTextureManager(
-                child.material as MeshStandardMaterial
-            )
-            return
+        if (child.material.opacity === 1) {
+            child.castShadow = true
+            child.receiveShadow = true
         }
-        child.castShadow = true
-        child.receiveShadow = true
+        child.material.userData.TextureManager = createTextureManager(
+            child.material as MeshStandardMaterial
+        )
     })
     for (const light of lights) light.parent?.remove(light)
 }
