b468d249bda4b0c331a1073a58c1875eac1e3a7a
lingosandi <lai@lingo3d.com>
2023-04-24 16:18:12 +0800

    addOutline

--- lingo3d/src/systems/configLoadedSystems/configOutlineSystem.ts
+++ lingo3d/src/systems/configLoadedSystems/configOutlineSystem.ts
@@ -8,8 +8,8 @@ export const [addConfigOutlineSystem] = configLoadedSystemWithDispose(
     (self) => {
         const target =
             "loadedObject3d" in self ? self.loadedObject3d! : self.object3d
-        self.bloom ? addOutline(target) : deleteOutline(target)
-        return self.bloom
+        self.outline ? addOutline(target) : deleteOutline(target)
+        return self.outline
     },
     (self) => {
         deleteOutline(
