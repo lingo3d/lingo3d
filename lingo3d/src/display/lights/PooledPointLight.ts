3f177e27f1dfd14ea4d7d52a37b1616634be0da2
lingosandi <lai@lingo3d.com>
2023-04-28 23:19:52 +0800

    initPointLight

--- lingo3d/src/display/lights/PooledPointLight.ts
+++ lingo3d/src/display/lights/PooledPointLight.ts
@@ -1,7 +1,20 @@
+import { lazy } from "@lincode/utils"
 import ObjectManager from "../core/ObjectManager"
+import {
+    releasePointLight,
+    requestPointLight
+} from "../../pools/objectPools/pointLightPool"
+
+const initPointLight = lazy(() => {
+    for (let i = 0; i < 4; ++i) {
+        const pointLight = requestPointLight([], "")
+        releasePointLight(pointLight)
+    }
+})
 
 export default class PooledPointLight extends ObjectManager {
     public constructor() {
         super()
+        initPointLight()
     }
 }
--- lingo3d/src/pools/objectPools/pointLightPool.ts
+++ lingo3d/src/pools/objectPools/pointLightPool.ts
@@ -1,1 +1,12 @@
-// export const [requestPointLight, releasePointLight] =
+import PointLight from "../../display/lights/PointLight"
+import createObjectPool from "../utils/createObjectPool"
+
+export const [requestPointLight, releasePointLight] = createObjectPool<
+    PointLight,
+    []
+>(() => {
+    const light = new PointLight()
+    light.disableSceneGraph = true
+    light.disableSerialize = true
+    return light
+})
