39148e6a8b1a895def727b05e110380305a14c8a
lingosandi <lai@lingo3d.com>
2023-04-30 23:09:38 +0800

    deleteSystem

--- lingo3d/src/systems/utils/renderSystemWithLifeCycleAndData.ts
+++ lingo3d/src/systems/utils/renderSystemWithLifeCycleAndData.ts
@@ -1,5 +1,6 @@
 import { Cancellable } from "@lincode/promiselikes"
 import { onBeforeRender } from "../../events/onBeforeRender"
+import Appendable from "../../api/core/Appendable"
 
 export default <T, Data extends Record<string, any>>(
     cb: (target: T, data: Data) => void,
@@ -16,17 +17,21 @@ export default <T, Data extends Record<string, any>>(
     }
 
     let handle: Cancellable | undefined
-    const start = () => {
-        handle = ticker(execute)
+    const start = () => (handle = ticker(execute))
+
+    const deleteSystem = (item: T) => {
+        if (!queued.delete(item)) return
+        item instanceof Appendable && item.$deleteSystemSet.delete(deleteSystem)
+        queued.size === 0 && handle?.cancel()
     }
     return <const>[
         (item: T, data: Data) => {
             if (queued.has(item)) return
             queued.set(item, data)
+            item instanceof Appendable &&
+                item.$deleteSystemSet.add(deleteSystem)
             if (queued.size === 1) start()
         },
-        (item: T) => {
-            if (queued.delete(item) && queued.size === 0) handle?.cancel()
-        }
+        deleteSystem
     ]
 }
