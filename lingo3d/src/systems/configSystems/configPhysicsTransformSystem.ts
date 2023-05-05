a4c1f25052a49da0a4311e75b3bea8bd21f136f3
lingosandi <lai@lingo3d.com>
2023-05-05 18:01:28 +0800

    updates

--- lingo3d/src/systems/configSystems/configPhysicsTransformSystem.ts
+++ lingo3d/src/systems/configSystems/configPhysicsTransformSystem.ts
@@ -1,24 +1,16 @@
 import { managerControllerMap } from "../../collections/pxCollections"
 import PhysicsObjectManager from "../../display/core/PhysicsObjectManager"
-import { assignPxTransform, setPxVec } from "../../engine/physx/pxMath"
-import { dtPtr } from "../../pointers/dtPtr"
-import { physxPtr } from "../../pointers/physxPtr"
+import {
+    assignPxTransform,
+    assignPxVec,
+    setPxVec
+} from "../../engine/physx/pxMath"
 import configSystem from "../utils/configSystem"
 
 export const [addConfigPhysicsTransformSystem] = configSystem(
     (manager: PhysicsObjectManager) => {
         const controller = managerControllerMap.get(manager)
-        if (controller) {
-            const { x: px, y: py, z: pz } = manager.position
-            const { x: cx, y: cy, z: cz } = controller.getPosition()
-            controller.move(
-                setPxVec(px - cx, py - cy, pz - cz),
-                0.001,
-                dtPtr[0],
-                physxPtr[0].pxControllerFilters
-            )
-            return
-        }
+        controller?.setPosition(assignPxVec(manager.position))
         manager.$actor.setGlobalPose(assignPxTransform(manager))
         if (!("setLinearVelocity" in manager.$actor)) return
         manager.$actor.setLinearVelocity(setPxVec(0, 0, 0))
