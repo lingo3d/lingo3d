2f4daf06c9320e64429e8a2536ffeb3fcf4d0ec1
lingosandi <lai@lingo3d.com>
2023-05-08 15:03:26 +0800

    updates

--- lingo3d/src/main.ts
+++ lingo3d/src/main.ts
@@ -1,5 +1,5 @@
 import { settings } from "."
-import "./tests/testSpriteSheet"
+import "./tests/testLight"
 import LingoEditor from "./editor"
 
 settings.autoMount = true
--- lingo3d/src/tests/testLight.ts
+++ lingo3d/src/tests/testLight.ts
@@ -2,6 +2,7 @@ import {
     DefaultSkyLight,
     Dummy,
     PooledPointLight,
+    Sprite,
 } from ".."
 import Cube from "../display/primitives/Cube"
 
@@ -14,6 +15,12 @@ ground.scaleX = 1000
 ground.scaleZ = 1000
 
 const light = new PooledPointLight()
+
+const aura = new Sprite()
+aura.scale = 10
+aura.texture = "particle.jpg"
+aura.blending = "additive"
+aura.depthTest = false
 
 // settings.shadowMode = "physics"
 
