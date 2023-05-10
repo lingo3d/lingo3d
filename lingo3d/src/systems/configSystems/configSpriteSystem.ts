dc6180c5c3ab6e17aa07183e6b70a09a15cb420b
lingosandi <lai@lingo3d.com>
2023-05-11 05:43:26 +0800

    updates

--- lingo3d/src/systems/configSystems/configSpriteSystem.ts
+++ lingo3d/src/systems/configSystems/configSpriteSystem.ts
@@ -5,22 +5,20 @@ import configSystemWithCleanUp2 from "../utils/configSystemWithCleanUp2"
 
 export const [addConfigSpriteSystem] = configSystemWithCleanUp2(
     (self: Sprite) => {
-        if (self.texture) {
+        if (self.texture)
             self.$material.map = requestTexture([
                 self.texture,
                 self.textureRepeat,
                 self.textureFlipY,
                 self.textureRotation
             ])
-        }
-        if (self.alphaMap) {
+        if (self.alphaMap)
             self.$material.alphaMap = requestTexture([
                 self.alphaMap,
                 self.textureRepeat,
                 self.textureFlipY,
                 self.textureRotation
             ])
-        }
     },
     (self) => {
         if (self.$material.map) {
