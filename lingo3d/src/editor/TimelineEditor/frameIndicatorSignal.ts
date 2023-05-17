706a82cf41b580be7d027cbcdc92ebc205de062d
lingosandi <lai@lingo3d.com>
2023-05-17 22:06:57 +0800

    updates

--- lingo3d/src/editor/TimelineEditor/frameIndicatorSignal.ts
+++ lingo3d/src/editor/TimelineEditor/frameIndicatorSignal.ts
@@ -1,6 +1,9 @@
-import { Point } from "@lincode/math"
-import { signal } from "@preact/signals"
+import { Signal, signal } from "@preact/signals"
 import { getTimelineLayer } from "../../states/useTimelineLayer"
+import { PointType } from "../../utils/isPoint"
 
-export const frameIndicatorSignal = signal<Point | undefined>(undefined)
+export const frameIndicatorSignal: Signal<PointType | undefined> = signal<
+    PointType | undefined
+>(undefined)
+
 getTimelineLayer(() => (frameIndicatorSignal.value = undefined))
