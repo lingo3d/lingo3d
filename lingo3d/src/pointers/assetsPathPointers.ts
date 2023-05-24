53eccb8e970b2d37658c3b9b9796cd77a6c674dd
lingosandi <lai@lingo3d.com>
2023-05-24 17:59:22 +0800

    same origin assets

--- lingo3d/src/pointers/assetsPathPointers.ts
+++ lingo3d/src/pointers/assetsPathPointers.ts
@@ -19,6 +19,8 @@ export const setAssetsPath = (val: string) => {
     wasmUrlPtr[0] = assetsPathPtr[0] + "wasm/"
 }
 
-setAssetsPath(
-    "http://ec2-69-230-242-89.cn-northwest-1.compute.amazonaws.com.cn:8080/"
-)
+setAssetsPath("assets")
+
+// setAssetsPath(
+//     "http://ec2-69-230-242-89.cn-northwest-1.compute.amazonaws.com.cn:8080/"
+// )
