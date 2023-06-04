ef5ca9918946fad1e7affdff7ce8c9d376c9b5df
lingosandi <lai@lingo3d.com>
2023-06-04 17:25:38 +0800

    updates

--- lingo3d/src/states/useWorldPlay.ts
+++ lingo3d/src/states/useWorldPlay.ts
@@ -1,7 +1,3 @@
 import store from "@lincode/reactivity"
-import { USE_RUNTIME } from "../globals"
-import { setEditorRuntime } from "./useEditorRuntime"
 
 export const [setWorldPlay, getWorldPlay] = store<boolean | "script">(true)
-
-getWorldPlay((val) => setEditorRuntime(USE_RUNTIME && !!val))
