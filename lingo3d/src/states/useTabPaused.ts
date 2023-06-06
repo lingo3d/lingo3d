65499f7772328b6c8a0f5a9d2192eaa2601f8f19
lingosandi <lai@lingo3d.com>
2023-06-06 20:22:48 +0800

    updates

--- lingo3d/src/states/useTabPaused.ts
+++ lingo3d/src/states/useTabPaused.ts
@@ -2,6 +2,7 @@ import store, { createEffect } from "@lincode/reactivity"
 import { getTabVisible } from "./useTabVisible"
 import { getTabFocused } from "./useTabFocused"
 import { getEditorBehavior } from "./useEditorBehavior"
+import { IS_MOBILE } from "../globals"
 
 const [setTabPaused, getTabPaused] = store(false)
 export { getTabPaused }
@@ -9,3 +10,9 @@ export { getTabPaused }
 createEffect(() => {
     setTabPaused(getEditorBehavior() && (!getTabVisible() || !getTabFocused()))
 }, [getTabVisible, getTabFocused, getEditorBehavior])
+
+!IS_MOBILE &&
+    setInterval(
+        () => setTabPaused(document.hidden || !document.hasFocus()),
+        1000
+    )
