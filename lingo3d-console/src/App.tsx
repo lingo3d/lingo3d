94088b905712066f3e1c3faaa5a1f666e55fd14a
lingosandi <lai@lingo3d.com>
2023-06-09 03:31:12 +0800

    lingo3d-console

---
+++ lingo3d-console/src/App.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { Hook, Unhook, Console, Decode } from "console-feed";
+
+const setFrameProperty = (key: string, value: any) =>
+  //@ts-ignore
+  window.frameElement && (window.frameElement[key] = value);
+
+function App() {
+  const [logs, setLogs] = useState<Array<any>>([]);
+
+  useEffect(() => {
+    setFrameProperty("$hook", (myConsole: typeof console) => {
+      Hook(myConsole, (log) => {
+        setLogs((currLogs) => [...currLogs, Decode(log)]);
+      });
+    });
+    setFrameProperty("$unhook", Unhook);
+  }, []);
+
+  return (
+    <div
+      style={{
+        position: "absolute",
+        left: 0,
+        top: 0,
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <Console logs={logs} variant="dark" />
+    </div>
+  );
+}
+
+export default App;
