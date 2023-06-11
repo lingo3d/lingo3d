8ffae1cc1773f8fcafbbb984e340199d863fa8c7
lingosandi <lai@lingo3d.com>
2023-06-12 01:41:11 +0800

    uploadFolder

--- lingo3d/src/api/files/uploadFolder.ts
+++ lingo3d/src/api/files/uploadFolder.ts
@@ -1,4 +1,33 @@
+import { pathDirectoryHandleMap } from "../../collections/pathDirectoryHandleMap"
+import { getFileBrowserDir } from "../../states/useFileBrowserDir"
+
+const copyDirectory = async (
+    sourceHandle: FileSystemDirectoryHandle,
+    destinationHandle: FileSystemDirectoryHandle
+) => {
+    //@ts-ignore
+    for await (const entry of sourceHandle.values()) {
+        if (entry.kind === "file") {
+            const fileHandle = await sourceHandle.getFileHandle(entry.name)
+            const file = await fileHandle.getFile()
+            //@ts-ignore
+            await destinationHandle.writeFile(entry.name, file)
+        } else if (entry.kind === "directory") {
+            const directoryHandle = await sourceHandle.getDirectoryHandle(
+                entry.name
+            )
+            const newDirectoryHandle =
+                await destinationHandle.getDirectoryHandle(entry.name, {
+                    create: true
+                })
+            await copyDirectory(directoryHandle, newDirectoryHandle)
+        }
+    }
+}
+
 export default async () => {
     //@ts-ignore
     const sourceHandle = await window.showDirectoryPicker()
+    const destinationHandle = pathDirectoryHandleMap.get(getFileBrowserDir())!
+    await copyDirectory(sourceHandle, destinationHandle)
 }
--- lingo3d/src/editor/FileBrowser/FileBrowserAddContextMenu.tsx
+++ lingo3d/src/editor/FileBrowser/FileBrowserAddContextMenu.tsx
@@ -3,6 +3,7 @@ import { Signal, signal } from "@preact/signals"
 import ContextMenu from "../component/ContextMenu"
 import MenuButton from "../component/MenuButton"
 import createFolder from "../../api/files/createFolder"
+import uploadFolder from "../../api/files/uploadFolder"
 
 export const fileBrowserAddContextMenuSignal: Signal<
     (Point & { createFolder?: boolean }) | undefined
@@ -44,6 +45,7 @@ const FileBrowserAddContextMenu = () => {
             </MenuButton>
             <MenuButton
                 onClick={() => {
+                    uploadFolder()
                     fileBrowserAddContextMenuSignal.value = undefined
                 }}
             >
