7bca345dba93be52e0471817b29ce7abae495558
lingosandi <lai@lingo3d.com>
2023-06-12 01:33:10 +0800

    updates

--- lingo3d/src/api/files/createFolder.ts
+++ lingo3d/src/api/files/createFolder.ts
@@ -2,10 +2,8 @@ import { getFileBrowserDir } from "../../states/useFileBrowserDir"
 import { pathDirectoryHandleMap } from "../../collections/pathDirectoryHandleMap"
 
 export default async (folderName: string) => {
-    const parentDirectoryHandle = pathDirectoryHandleMap.get(
-        getFileBrowserDir()
-    )
-    const newDirectoryHandle = await parentDirectoryHandle!.getDirectoryHandle(
+    const directoryHandle = pathDirectoryHandleMap.get(getFileBrowserDir())
+    const newDirectoryHandle = await directoryHandle!.getDirectoryHandle(
         folderName,
         { create: true }
     )
