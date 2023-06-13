42c89960a38cd5084bca03862fcfb9dc9060a0fc
lingosandi <lai@lingo3d.com>
2023-06-13 18:52:16 +0800

    updates

--- lingo3d/src/display/utils/loaders/loadVideoTexture.ts
+++ lingo3d/src/display/utils/loaders/loadVideoTexture.ts
@@ -1,34 +1,35 @@
 import { RepeatWrapping, Texture, VideoTexture } from "three"
-import { forceGet } from "@lincode/utils"
 import { isSelector } from "../../../utils/isSelector"
 
-const cache = new Map<string, VideoTexture>()
-
-export default (url: string) =>
-    forceGet(cache, url, () => {
-        if (isSelector(url)) {
-            const video = document.querySelector(url)
-            if (video instanceof HTMLVideoElement)
-                return new VideoTexture(
-                    video,
-                    undefined,
-                    RepeatWrapping,
-                    RepeatWrapping
-                )
-            return new Texture()
-        }
-        const video = document.createElement("video")
-        video.crossOrigin = "anonymous"
-        video.src = url
-        video.loop = true
-        video.autoplay = true
-        video.muted = true
-        video.playsInline = true
-        video.play()
-        return new VideoTexture(
-            video,
-            undefined,
-            RepeatWrapping,
-            RepeatWrapping
-        )
-    }).clone()
+export default (url: string) => {
+    if (isSelector(url)) {
+        const video = document.querySelector(url)
+        if (video instanceof HTMLVideoElement)
+            return new VideoTexture(
+                video,
+                undefined,
+                RepeatWrapping,
+                RepeatWrapping
+            )
+        return new Texture()
+    }
+    const video = document.createElement("video")
+    video.crossOrigin = "anonymous"
+    video.src = url
+    video.loop = true
+    video.autoplay = true
+    video.muted = true
+    video.playsInline = true
+    video.play()
+    const texture = new VideoTexture(
+        video,
+        undefined,
+        RepeatWrapping,
+        RepeatWrapping
+    )
+    const { dispose } = texture
+    texture.dispose = () => {
+        dispose.call(texture)
+        video.pause()
+    }
+}
