c11a2812235c6b0d906f093ca612056b79304f09
lingosandi <lai@lingo3d.com>
2023-06-16 02:05:32 +0800

    updates

--- lingo3d/src/memo/isOpaque.ts
+++ lingo3d/src/memo/isOpaque.ts
@@ -4,5 +4,5 @@ import computePerFrame from "./utils/computePerFrame"
 export default computePerFrame((target: Object3D) => {
     const material = (target as Mesh).material as Material
     if (!material) return true
-    return material.opacity >= 1
+    return material.transparent
 })
