041082681bc4958fd0edb7ce5e9e48705ed89a3c
lingosandi <lai@lingo3d.com>
2023-06-20 20:17:41 +0800

    updates

--- lingo3d/src/systems/eventSystems/characterCameraTransformEditSystem.ts
+++ lingo3d/src/systems/eventSystems/characterCameraTransformEditSystem.ts
@@ -2,22 +2,24 @@ import CharacterCamera from "../../display/core/CharacterCamera"
 import { onTransformEdit } from "../../events/onTransformEdit"
 import { LockTargetRotationValue } from "../../interface/ICharacterCamera"
 import getContext from "../../memo/getContext"
-import eventSystem from "../utils/eventSystem"
+import createInternalSystem from "../utils/createInternalSystem"
 
-export const characterCameraTransformEditSystem = eventSystem(
+export const characterCameraTransformEditSystem = createInternalSystem(
     "characterCameraTransformEditSystem",
-    (self: CharacterCamera, { target, phase, mode }) => {
-        if (mode !== "rotate" || self !== target) return
+    {
+        update: (self: CharacterCamera, _, { target, phase, mode }) => {
+            if (mode !== "rotate" || self !== target) return
 
-        const context = getContext(self) as {
-            lockTargetRotation: LockTargetRotationValue
-        }
-        if (phase === "start") {
-            context.lockTargetRotation = self.lockTargetRotation
-            self.lockTargetRotation = "follow"
-            return
-        }
-        self.lockTargetRotation = context.lockTargetRotation
-    },
-    onTransformEdit
+            const context = getContext(self) as {
+                lockTargetRotation: LockTargetRotationValue
+            }
+            if (phase === "start") {
+                context.lockTargetRotation = self.lockTargetRotation
+                self.lockTargetRotation = "follow"
+                return
+            }
+            self.lockTargetRotation = context.lockTargetRotation
+        },
+        updateTicker: onTransformEdit
+    }
 )
--- lingo3d/src/systems/eventSystems/curveHelperSphereTransformEditSystem.ts
+++ lingo3d/src/systems/eventSystems/curveHelperSphereTransformEditSystem.ts
@@ -3,17 +3,20 @@ import Curve from "../../display/Curve"
 import HelperSphere from "../../display/core/utils/HelperSphere"
 import { onTransformEdit } from "../../events/onTransformEdit"
 import { configCurveSystemPtr } from "../../pointers/configCurveSystemPtr"
-import eventSystem from "../utils/eventSystem"
+import createInternalSystem from "../utils/createInternalSystem"
 
-export const curveHelperSphereTransformEditSystem = eventSystem(
+export const curveHelperSphereTransformEditSystem = createInternalSystem(
     "curveHelperSphereTransformEditSystem",
-    (self: HelperSphere, { target, phase, mode }) => {
-        if (self !== target || mode !== "translate" || phase !== "end") return
-        const pt = curveHelperSpherePointMap.get(self)!
-        pt.x = self.x
-        pt.y = self.y
-        pt.z = self.z
-        configCurveSystemPtr[0].add(self.parent as Curve)
-    },
-    onTransformEdit
+    {
+        update: (self: HelperSphere, _, { target, phase, mode }) => {
+            if (self !== target || mode !== "translate" || phase !== "end")
+                return
+            const pt = curveHelperSpherePointMap.get(self)!
+            pt.x = self.x
+            pt.y = self.y
+            pt.z = self.z
+            configCurveSystemPtr[0].add(self.parent as Curve)
+        },
+        updateTicker: onTransformEdit
+    }
 )
