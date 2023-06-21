210c9041be39e4f38abb14f12e788f5fea7b3525
lingosandi <lai@lingo3d.com>
2023-06-21 14:49:21 +0800

    updates

--- lingo3d/src/systems/flySystem.ts
+++ lingo3d/src/systems/flySystem.ts
@@ -7,27 +7,33 @@ export const flySystem = createInternalSystem("flySystem", {
         if (keyPressSet.has("Meta") || keyPressSet.has("Control")) return
 
         const speed = keyPressSet.has("Shift") ? 50 : 10
+        let pressed = false
 
-        if (keyPressSet.has("w")) manager.translateZ(-speed)
-        else if (keyPressSet.has("s")) manager.translateZ(speed)
-
-        if (keyPressSet.has("a") || keyPressSet.has("ArrowLeft"))
-            manager.moveRight(-speed)
-        else if (keyPressSet.has("d") || keyPressSet.has("ArrowRight"))
-            manager.moveRight(speed)
-
-        if (
-            keyPressSet.has("w") ||
-            keyPressSet.has("s") ||
-            keyPressSet.has("a") ||
-            keyPressSet.has("d")
-        ) {
-            const worldPos = manager.getWorldPosition()
-            manager.innerZ = 0
-            manager.placeAt(worldPos)
+        if (keyPressSet.has("w")) {
+            manager.translateZ(-speed)
+            pressed = true
+        } else if (keyPressSet.has("s")) {
+            manager.translateZ(speed)
+            pressed = true
         }
+        if (keyPressSet.has("a") || keyPressSet.has("ArrowLeft")) {
+            manager.moveRight(-speed)
+            pressed = true
+        } else if (keyPressSet.has("d") || keyPressSet.has("ArrowRight")) {
+            manager.moveRight(speed)
+            pressed = true
+        }
+        if (keyPressSet.has("ArrowDown")) {
+            manager.y -= speed
+            pressed = true
+        } else if (keyPressSet.has("ArrowUp")) {
+            manager.y += speed
+            pressed = true
+        }
+        if (!pressed || !manager.innerZ) return
         
-        if (keyPressSet.has("ArrowDown")) manager.y -= speed
-        else if (keyPressSet.has("ArrowUp")) manager.y += speed
+        const worldPos = manager.getWorldPosition()
+        manager.innerZ = 0
+        manager.placeAt(worldPos)
     }
 })
