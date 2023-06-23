d10e96e41dbb5663674ef3bf8ecb25290fd51fe2
lingosandi <lai@lingo3d.com>
2023-06-23 17:29:01 +0800

    updates

--- lingo3d/src/systems/pixelRatioSystem.ts
+++ lingo3d/src/systems/pixelRatioSystem.ts
@@ -7,6 +7,7 @@ import { rendererPtr } from "../pointers/rendererPtr"
 import { resolutionPtr } from "../pointers/resolutionPtr"
 import { setPixelRatio } from "../states/usePixelRatio"
 import { fpsPtr } from "../pointers/fpsPtr"
+import { STANDARD_FRAME } from "../globals"
 
 const clampPixelRatio = (pixelCount: number, pixelRatio: number) => {
     const clampMin = math.mapRange(pixelCount, 200000, 2000000, 0.75, 0.5, true)
@@ -14,22 +15,23 @@ const clampPixelRatio = (pixelCount: number, pixelRatio: number) => {
 }
 
 const sortPixelRatio = (a: number, b: number) => a - b
+const SAMPLES = 10
 
 export const pixelRatioSystem = createSystem("pixelRatioSystem", {
     data: () => ({
         pixelCount: resolutionPtr[0][0] * resolutionPtr[0][1],
         pixelRatio: Infinity,
-        ratio: math.mapRange(fpsPtr[0], 0, 60, 0, 1),
+        ratio: math.mapRange(fpsPtr[0], 0, STANDARD_FRAME, 0, 1),
         pixelRatioArray: [] as Array<number>
     }),
     update: (_: WebGLRenderer, data) => {
         data.pixelRatioArray.push(
             clampPixelRatio(data.pixelCount, 1 / (fpsRatioPtr[0] * data.ratio))
         )
-        if (data.pixelRatioArray.length < 10) return
+        if (data.pixelRatioArray.length < SAMPLES) return
 
         data.pixelRatioArray.sort(sortPixelRatio)
-        const median = data.pixelRatioArray[4]
+        const median = data.pixelRatioArray[Math.floor(SAMPLES * 0.5)]
         data.pixelRatioArray.length = 0
 
         if (median >= data.pixelRatio) return
