28baf55ade18045944ebd96c270422b4a9c53966
lingosandi <lai@lingo3d.com>
2023-06-25 04:27:35 +0800

    updates

--- lingo3d/src/systems/configSystems/configPhysicsTransformSystem.ts
+++ lingo3d/src/systems/configSystems/configPhysicsTransformSystem.ts
@@ -4,16 +4,29 @@ import {
     assignPxTransform,
     setPxVec
 } from "../../engine/physx/pxMath"
+import { dtPtr } from "../../pointers/dtPtr"
 import { physxLoopPtr } from "../../pointers/physxLoopPtr"
+import { physxPtr } from "../../pointers/physxPtr"
 import createInternalSystem from "../utils/createInternalSystem"
 
 export const configPhysicsTransformSystem = createInternalSystem(
     "configPhysicsTransformSystem",
     {
         effect: (self: PhysicsObjectManager) => {
-            if (self.$controller) {
-                if (physxLoopPtr[0]) return
-                self.$controller.setPosition(assignPxExtendedVec(self.position))
+            const controller = self.$controller
+            if (controller) {
+                if (physxLoopPtr[0]) {
+                    const { x: px, y: py, z: pz } = self.position
+                    const { x: cx, y: cy, z: cz } = controller.getPosition()
+                    controller.move(
+                        setPxVec(px - cx, py - cy, pz - cz),
+                        0.001,
+                        dtPtr[0],
+                        physxPtr[0].pxControllerFilters
+                    )
+                    return
+                }
+                controller.setPosition(assignPxExtendedVec(self.position))
             }
             self.$actor.setGlobalPose(assignPxTransform(self))
             if (!("setLinearVelocity" in self.$actor)) return
