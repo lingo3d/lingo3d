3f273dc45070516a7a1514daec452aeef00fef6e
lingosandi <lai@lingo3d.com>
2023-06-26 02:11:35 +0800

    updates

--- lingo3d/src/systems/updateShadowSystem.ts
+++ lingo3d/src/systems/updateShadowSystem.ts
@@ -3,22 +3,27 @@ import PointLightBase from "../display/core/PointLightBase"
 import { shadowResolutionPtr } from "../pointers/shadowResolutionPtr"
 import createInternalSystem from "./utils/createInternalSystem"
 import getIntensityFactor from "../memo/getIntensityFactor"
+import { mapRange } from "@lincode/math"
+import frameSync from "../api/frameSync"
 
 const maxResolution = 2048
 
 export const updateShadowSystem = createInternalSystem("updateShadowSystem", {
+    data: { skipped: 0 },
     afterTick: () => {
         shadowResolutionPtr[0] = 0
     },
-    update: (self: PointLightBase<any>) => {
+    update: (self: PointLightBase<any>, data) => {
+        const intensityFactor = getIntensityFactor(self)
+        if (!intensityFactor || !self.shadows) return
         if (
-            !self.object3d.intensity ||
-            !self.shadows ||
-            shadowResolutionPtr[0] >= maxResolution
+            shadowResolutionPtr[0] >= maxResolution &&
+            ++data.skipped < frameSync(mapRange(intensityFactor, 0, 1, 10, 0))
         )
             return
 
         updateShadow(self.object3d.shadow)
+        data.skipped = 0
     },
     sort: (a, b) => getIntensityFactor(b) - getIntensityFactor(a)
 })
