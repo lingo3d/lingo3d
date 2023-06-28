edead5ee4798c6325cdc45a96b7e33cccbb68d08
lingosandi <lai@lingo3d.com>
2023-06-28 23:39:09 +0800

    updates

--- lingo3d/src/systems/lightIntensitySystem.ts
+++ lingo3d/src/systems/lightIntensitySystem.ts
@@ -3,6 +3,7 @@ import PointLightBase from "../display/core/PointLightBase"
 import createInternalSystem from "./utils/createInternalSystem"
 import { mapRange } from "@lincode/math"
 import frameSync from "../api/frameSync"
+import { fpsPtr } from "../pointers/fpsPtr"
 
 export const lightIntensitySystem = createInternalSystem(
     "lightIntensitySystem",
@@ -10,11 +11,11 @@ export const lightIntensitySystem = createInternalSystem(
         data: { count: 0 },
         update: (self: PointLightBase, data) => {
             const targetIntensity = self.intensity * getIntensityFactor(self)
-            if (data.count < 30) {
+            if (data.count < fpsPtr[0]) {
                 self.object3d.intensity = mapRange(
                     data.count,
                     0,
-                    30,
+                    fpsPtr[0],
                     0,
                     targetIntensity
                 )
