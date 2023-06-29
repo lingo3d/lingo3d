f106f7519e604aaa1a8d8434be8ecd5ccda0ad30
lingosandi <lai@lingo3d.com>
2023-06-29 21:55:49 +0800

    updates

--- lingo3d/src/pools/objectPools/pointLightPool.ts
+++ lingo3d/src/pools/objectPools/pointLightPool.ts
@@ -2,26 +2,29 @@ import PointLight from "../../display/lights/PointLight"
 import PooledPointLight from "../../display/lights/PooledPointLight"
 import createObjectPool from "../utils/createObjectPool"
 
-export const pointLightPool =
-    createObjectPool<PointLight, [], PooledPointLight>(
-        () => {
-            const light = new PointLight()
-            light.intensity = 0
-            light.$ghost()
-            return light
-        },
-        (light) => {
-            light.dispose()
-        },
-        (light, self) => {
-            light.distance = self.distance
-            light.intensity = self.intensity
-            light.color = self.color
-            light.shadows = self.shadows
-            light.fade = self.fade
-        },
-        (light) => {
-            light.fade = false
-            light.intensity = 0
-        }
-    )
+export const pointLightPool = createObjectPool<
+    PointLight,
+    [],
+    PooledPointLight
+>(
+    () => {
+        const light = new PointLight()
+        light.intensity = 0
+        light.$ghost()
+        return light
+    },
+    (light) => {
+        light.dispose()
+    },
+    (light, self) => {
+        light.distance = self.distance
+        light.intensity = self.intensity
+        light.color = self.color
+        light.shadows = self.shadows
+        light.fade = self.fade
+    },
+    (light) => {
+        light.fade = false
+        light.intensity = 0
+    }
+)
