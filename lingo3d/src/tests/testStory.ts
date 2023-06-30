afc4f02dd7dc1bc80f05dc4bc711b6ba5b2f94cb
lingosandi <lai@lingo3d.com>
2023-07-01 02:58:35 +0800

    updates

--- lingo3d/src/tests/testStory.ts
+++ lingo3d/src/tests/testStory.ts
@@ -41,21 +41,21 @@ tommy.rotationY = 45.0
 tommy.rotationX = -180
 tommy.rotationZ = -180
 
-keyboard.onKeyPress = (ev) => {
-    if (ev.keys.has("w")) {
-        player.strideForward = -5
-    } else if (ev.keys.has("s")) {
-        player.strideForward = 5
-    }
-    if (canSpeak && ev.keys.has("Space")) setSpeaking(true)
-}
+// keyboard.onKeyPress = (ev) => {
+//     if (ev.keys.has("w")) {
+//         player.strideForward = -5
+//     } else if (ev.keys.has("s")) {
+//         player.strideForward = 5
+//     }
+//     if (canSpeak && ev.keys.has("Space")) setSpeaking(true)
+// }
 
-keyboard.onKeyUp = (ev) => {
-    player.strideForward = 0
-    player.strideRight = 0
+// keyboard.onKeyUp = (ev) => {
+//     player.strideForward = 0
+//     player.strideRight = 0
 
-    if (!ev.keys.has("Space")) setSpeaking(false)
-}
+//     if (!ev.keys.has("Space")) setSpeaking(false)
+// }
 
 //camera
 const cam = new ThirdPersonCamera()
@@ -67,70 +67,70 @@ cam.innerY = 50
 cam.innerX = 50
 cam.lockTargetRotation = "dynamic-lock"
 
-let canSpeak = false
-const [setSpeaking, getSpeaking] = store(false)
+// let canSpeak = false
+// const [setSpeaking, getSpeaking] = store(false)
 
-tommy.onLoop = () => {
-    if (tommy.position.distanceTo(player.position) < 1) {
-        canSpeak = true
-    } else {
-        canSpeak = false
-    }
-}
+// tommy.onLoop = () => {
+//     if (tommy.position.distanceTo(player.position) < 1) {
+//         canSpeak = true
+//     } else {
+//         canSpeak = false
+//     }
+// }
 
-const selectedVoice = speechSynthesis
-    .getVoices()
-    .find((voice) => voice.name.includes("Albert"))
+// const selectedVoice = speechSynthesis
+//     .getVoices()
+//     .find((voice) => voice.name.includes("Albert"))
 
-const speakText = (text: string) => {
-    if ("speechSynthesis" in window) {
-        const utterance = new SpeechSynthesisUtterance(text)
-        utterance.voice = selectedVoice!
-        speechSynthesis.speak(utterance)
-    } else {
-        console.error("Speech synthesis is not supported in this browser.")
-    }
-}
+// const speakText = (text: string) => {
+//     if ("speechSynthesis" in window) {
+//         const utterance = new SpeechSynthesisUtterance(text)
+//         utterance.voice = selectedVoice!
+//         speechSynthesis.speak(utterance)
+//     } else {
+//         console.error("Speech synthesis is not supported in this browser.")
+//     }
+// }
 
 //@ts-ignore
-const recognition = new window.webkitSpeechRecognition()
-recognition.lang = "en-US"
-recognition.continuous = true
+// const recognition = new window.webkitSpeechRecognition()
+// recognition.lang = "en-US"
+// recognition.continuous = true
 
-const sendChat = async (text: string) => {
-    const result = await axios.post("http://127.0.0.1:8000/chat", {
-        name: "Roy Winterson",
-        text
-    })
-    return result.data
-}
+// const sendChat = async (text: string) => {
+//     const result = await axios.post("http://127.0.0.1:8000/chat", {
+//         name: "Roy Winterson",
+//         text
+//     })
+//     return result.data
+// }
 
-recognition.onresult = async (event: any) => {
-    let interimTranscript = ""
-    let finalTranscript = ""
+// recognition.onresult = async (event: any) => {
+//     let interimTranscript = ""
+//     let finalTranscript = ""
 
-    for (let i = event.resultIndex; i < event.results.length; i++) {
-        const transcript = event.results[i][0].transcript
-        if (event.results[i].isFinal) {
-            finalTranscript += transcript + " "
-        } else {
-            interimTranscript += transcript
-        }
-    }
-    console.log(finalTranscript)
-    const result = await sendChat(finalTranscript)
-    speakText(result)
-}
+//     for (let i = event.resultIndex; i < event.results.length; i++) {
+//         const transcript = event.results[i][0].transcript
+//         if (event.results[i].isFinal) {
+//             finalTranscript += transcript + " "
+//         } else {
+//             interimTranscript += transcript
+//         }
+//     }
+//     console.log(finalTranscript)
+//     const result = await sendChat(finalTranscript)
+//     speakText(result)
+// }
 
-recognition.onerror = (event: any) => {
-    console.error("Speech recognition error occurred: ", event.error)
-}
+// recognition.onerror = (event: any) => {
+//     console.error("Speech recognition error occurred: ", event.error)
+// }
 
-createEffect(() => {
-    if (!getSpeaking()) return
+// createEffect(() => {
+//     if (!getSpeaking()) return
 
-    recognition.start()
-    return () => {
-        recognition.stop()
-    }
-}, [getSpeaking])
+//     recognition.start()
+//     return () => {
+//         recognition.stop()
+//     }
+// }, [getSpeaking])
