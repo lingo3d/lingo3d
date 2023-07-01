feab4a00c4333ca31dce0c7ebfea0195486b6ef3
lingosandi <lai@lingo3d.com>
2023-07-01 16:36:44 +0800

    updates

--- lingo3d/src/api/files/openJSON.ts
+++ lingo3d/src/api/files/openJSON.ts
@@ -2,5 +2,5 @@ import loadFile from "./loadFile"
 
 export default async () => {
     const { fileOpen } = await import("browser-fs-access")
-    return loadFile(await fileOpen({ extensions: [".json"], id: "lingo3d" }))
+    return loadFile(await fileOpen({ extensions: [".json"], id: "lingo3d-file" }))
 }
