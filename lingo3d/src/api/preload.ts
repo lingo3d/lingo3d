d7f5fc602ef1dbaa89b423b987417c64ee43b5cf
lingosandi <lai@lingo3d.com>
2023-07-02 17:33:20 +0800

    updates

--- lingo3d/src/api/preload.ts
+++ lingo3d/src/api/preload.ts
@@ -48,7 +48,7 @@ export default async (
             if (isBusy()) return
             clearInterval(interval)
             resolve()
-        }, 500)
+        }, 100)
     })
     onProgress?.(100)
 }
