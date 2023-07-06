f7adf16eaf4e264c4740158bce21270ca83fcb6b
lingosandi <lai@lingo3d.com>
2023-07-07 03:52:53 +0800

    updates

--- lingo3d/src/throttle/utils/throttleFrameTrailing.ts
+++ lingo3d/src/throttle/utils/throttleFrameTrailing.ts
@@ -4,12 +4,14 @@ export default <Args extends Array<unknown>, Result>(
     fn: (...args: Args) => Result
 ) => {
     let scheduled = false
+    let latestArgs: Args
     return (...args: Args) => {
+        latestArgs = args
         if (scheduled) return
         scheduled = true
         onAfterRender(() => {
             scheduled = false
-            fn(...args)
+            fn(...latestArgs)
         }, true)
     }
 }
