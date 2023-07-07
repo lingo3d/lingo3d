01ca16c501a5ca3a9b068d7c84c1ddaf57ec0d2a
lingosandi <lai@lingo3d.com>
2023-07-07 16:29:57 +0800

    characterRigMenuButton

---
+++ lingo3d/src/editor/SceneGraph/SceneGraphContextMenu/menuButtons/characterRigMenuButton.tsx
@@ -0,0 +1,34 @@
+import { memo } from "preact/compat"
+import { sceneGraphContextMenuSignal } from ".."
+import { componentNameMenuButtonMap } from "../../../../collections/componentNameMenuButtonMap"
+import { selectionTargetPtr } from "../../../../pointers/selectionTargetPtr"
+import MenuButton from "../../../component/MenuButton"
+import {
+    getCharacterRig,
+    setCharacterRig
+} from "../../../../states/useCharacterRig"
+
+componentNameMenuButtonMap.set(
+    "characterRig",
+    memo(
+        () => {
+            const selectionTarget = selectionTargetPtr[0] as any
+            const characterRig = getCharacterRig()
+
+            return (
+                <MenuButton
+                    disabled={selectionTarget === characterRig}
+                    onClick={() => {
+                        setCharacterRig(selectionTarget)
+                        sceneGraphContextMenuSignal.value = undefined
+                    }}
+                >
+                    {selectionTarget === characterRig
+                        ? "Already editing"
+                        : "Edit CharacterRig"}
+                </MenuButton>
+            )
+        },
+        () => true
+    )
+)
