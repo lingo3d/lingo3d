d6f2e244097767348fb3905468f6e46ac5d0961d
lingosandi <lai@lingo3d.com>
2023-07-09 19:19:24 +0800

    updates

--- lingo3d/src/systems/configLoadedSystems/configCharacterRigSystem.ts
+++ lingo3d/src/systems/configLoadedSystems/configCharacterRigSystem.ts
@@ -1,41 +1,36 @@
-import { forceGetInstance } from "@lincode/utils"
 import { uuidMap } from "../../collections/idCollections"
 import CharacterRig from "../../display/CharacterRig"
-import ICharacterRig from "../../interface/ICharacterRig"
 import { createLoadedEffectSystem } from "../utils/createLoadedEffectSystem"
 import { vector3 } from "../../display/utils/reusables"
-import MeshAppendable from "../../display/core/MeshAppendable"
 import { Point3dType } from "../../typeGuards/isPoint"
 import RigJoint from "../../display/CharacterRig/RigJoint"
 import Model from "../../display/Model"
 
-type JointName = keyof ICharacterRig
-
-const parentChildrenNameMap = new Map<JointName, Array<JointName>>()
-const childParentNameMap = new Map<JointName, JointName>()
-const setParenting = (names: Array<JointName>) => {
-    let parentName: JointName | undefined
-    for (const childName of names) {
-        if (parentName) {
-            forceGetInstance(parentChildrenNameMap, parentName, Array).push(
-                childName
-            )
-            childParentNameMap.set(childName, parentName)
-        }
-        parentName = childName
-    }
-}
-setParenting(["hips", "spine0", "spine1", "spine2", "neck"])
-setParenting(["spine2", "leftShoulder", "leftArm", "leftForeArm", "leftHand"])
-setParenting([
-    "spine2",
-    "rightShoulder",
-    "rightArm",
-    "rightForeArm",
-    "rightHand"
-])
-setParenting(["hips", "leftThigh", "leftLeg", "leftFoot", "leftForeFoot"])
-setParenting(["hips", "rightThigh", "rightLeg", "rightFoot", "rightForeFoot"])
+// const parentChildrenNameMap = new Map<JointName, Array<JointName>>()
+// const childParentNameMap = new Map<JointName, JointName>()
+// const setParenting = (names: Array<JointName>) => {
+//     let parentName: JointName | undefined
+//     for (const childName of names) {
+//         if (parentName) {
+//             forceGetInstance(parentChildrenNameMap, parentName, Array).push(
+//                 childName
+//             )
+//             childParentNameMap.set(childName, parentName)
+//         }
+//         parentName = childName
+//     }
+// }
+// setParenting(["hips", "spine0", "spine1", "spine2", "neck"])
+// setParenting(["spine2", "leftShoulder", "leftArm", "leftForeArm", "leftHand"])
+// setParenting([
+//     "spine2",
+//     "rightShoulder",
+//     "rightArm",
+//     "rightForeArm",
+//     "rightHand"
+// ])
+// setParenting(["hips", "leftThigh", "leftLeg", "leftFoot", "leftForeFoot"])
+// setParenting(["hips", "rightThigh", "rightLeg", "rightFoot", "rightForeFoot"])
 
 const getDirection = (fromPoint: Point3dType, toPoint: Point3dType) =>
     vector3
@@ -43,12 +38,9 @@ const getDirection = (fromPoint: Point3dType, toPoint: Point3dType) =>
         .sub(fromPoint as any)
         .normalize()
 
-const rotateJoint = (target: MeshAppendable, joint: MeshAppendable) =>
-    joint.setRotationFromDirection(getDirection(target, joint))
-
-const setupJoint = (child: RigJoint, parent: RigJoint) => {
-    rotateJoint(child, parent)
-    parent.attach(child)
+const setupJoint = (childJoint: RigJoint, parentJoint: RigJoint) => {
+    parentJoint.setRotationFromDirection(getDirection(childJoint, parentJoint))
+    parentJoint.attach(childJoint)
 }
 
 export const configCharacterRigSystem = createLoadedEffectSystem(
