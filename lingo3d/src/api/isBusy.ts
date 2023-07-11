f8b0bc8b059d847693262ca04eb111f5b743a166
lingosandi <lai@lingo3d.com>
2023-07-12 01:52:23 +0800

    updates

--- lingo3d/src/api/isBusy.ts
+++ lingo3d/src/api/isBusy.ts
@@ -1,7 +1,4 @@
 import { busyProcesses } from "../collections/busyProcesses"
 import { busyCookingPtr } from "../pointers/busyCookingPtr"
-import { worldModePtr } from "../pointers/worldModePtr"
 
-export default () =>
-    busyProcesses.size > 0 ||
-    (busyCookingPtr[0] > 0 && worldModePtr[0] === "default")
+export default () => busyProcesses.size > 0 || busyCookingPtr[0] > 0
