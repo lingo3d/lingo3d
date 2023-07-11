5f7f6d3ae2339e613b117a10affbf11892b66edd
lingosandi <lai@lingo3d.com>
2023-07-11 20:52:07 +0800

    configModelAnimationSystem

--- lingo3d/src/systems/configLoadedSystems/configModelAnimationSystem.ts
+++ lingo3d/src/systems/configLoadedSystems/configModelAnimationSystem.ts
@@ -1,3 +1,4 @@
+import isBusy from "../../api/isBusy"
 import Model from "../../display/Model"
 import AnimationManager from "../../display/core/AnimatedObjectManager/AnimationManager"
 import { createLoadedEffectSystem } from "../utils/createLoadedEffectSystem"
@@ -7,25 +8,24 @@ export const configModelAnimationSystem = createLoadedEffectSystem(
     {
         data: { animations: [] as Array<AnimationManager> },
         effect: (self: Model, data) => {
-            if (self.$animationClips)
-                for (const [name, clip] of Object.entries(
-                    self.$animationClips
-                )) {
-                    const animation = (self.animations[name] =
-                        new AnimationManager(
-                            name,
-                            self.$loadedObject,
-                            self.$animationStates
-                        ))
-                    animation.$clip = clip
-                    self.append(animation)
-                    data.animations.push(animation)
-                    if (name === self.animation) self.animation = name
-                }
+            if (!self.$animationClips) return false
+
+            for (const [name, clip] of Object.entries(self.$animationClips)) {
+                const animation = (self.animations[name] = new AnimationManager(
+                    name,
+                    self.$loadedObject,
+                    self.$animationStates
+                ))
+                animation.$clip = clip
+                self.append(animation)
+                data.animations.push(animation)
+                if (name === self.animation) self.animation = name
+            }
         },
         cleanup: (_, data) => {
             for (const animation of data.animations) animation.dispose()
             data.animations = []
-        }
+        },
+        loading: (self) => !self.$loadedObject || isBusy()
     }
 )
