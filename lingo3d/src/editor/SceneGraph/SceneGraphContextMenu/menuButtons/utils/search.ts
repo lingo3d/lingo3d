a600ea991b50cdcce2bd94b48725fddfd7965b21
lingosandi <lai@lingo3d.com>
2023-07-12 16:32:21 +0800

    updates

--- lingo3d/src/editor/SceneGraph/SceneGraphContextMenu/menuButtons/utils/search.ts
+++ lingo3d/src/editor/SceneGraph/SceneGraphContextMenu/menuButtons/utils/search.ts
@@ -2,17 +2,13 @@ import { sceneGraphExpand } from "../../../../../states/useSceneGraphExpanded"
 import Model from "../../../../../display/Model"
 import Appendable from "../../../../../display/core/Appendable"
 import { emitSelectionTarget } from "../../../../../events/onSelectionTarget"
-import { getFoundManager } from "../../../../../display/core/utils/getFoundManager"
 
 export default (n: string, target: Appendable) => {
     if (!(target instanceof Model)) return
-    const name = n.toLowerCase()
-    let found = target.findFirst((childName) =>
-        childName.toLowerCase().includes(name)
+    const found = target.findFirst((name) =>
+        name.toLowerCase().includes(n.toLowerCase())
     )
     if (!found) return
-    if (found.$object.parent?.name.toLowerCase().includes(name))
-        found = getFoundManager(found.$object.parent, target)
     emitSelectionTarget(found)
     sceneGraphExpand(found.$object)
 }
