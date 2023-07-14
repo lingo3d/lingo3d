a8d69efc6c4e4f271033026b89e4296f4c35b77f
lingosandi <lai@lingo3d.com>
2023-07-14 16:58:52 +0800

    updates

--- lingo3d/src/memo/parseCharacter.ts
+++ lingo3d/src/memo/parseCharacter.ts
@@ -10,38 +10,48 @@ const testLeftForeArm = makePredicate(/left.*(fore|up).*arm/i)
 const testLeftArm = makePredicate(/left.*arm/i)
 const testLeftShoulder = makePredicate(/left.*shoulder/i)
 
+const testRightHand = makePredicate(/right.*hand/i)
+const testRightForeArm = makePredicate(/right.*(fore|up).*arm/i)
+const testRightArm = makePredicate(/right.*arm/i)
+const testRightShoulder = makePredicate(/right.*shoulder/i)
+
+const parseChain = (
+    map: Map<CharacterRigJointName, FoundManager>,
+    target: Model,
+    chain: Array<[CharacterRigJointName, typeof testLeftHand]>
+) => {
+    const tests = new Map(chain)
+    const [[name, test]] = tests
+    let found = target.findFirst(test)
+    if (!found) return
+    map.set(name, found)
+    tests.delete(name)
+    if (!tests.size) return
+    while (true) {
+        found = found?.parentNode
+        if (!found) break
+        for (const [name, test] of tests) {
+            if (!test(found.name!)) continue
+            map.set(name, found)
+            tests.delete(name)
+            if (!tests.size) break
+        }
+    }
+}
+
 export const parseCharacter = computeOnce((target: Model) => {
     const map = new Map<CharacterRigJointName, FoundManager>()
-
-    const leftHandTests = new Map<CharacterRigJointName, typeof testLeftHand>([
+    parseChain(map, target, [
         ["leftHand", testLeftHand],
         ["leftForeArm", testLeftForeArm],
         ["leftArm", testLeftArm],
         ["leftShoulder", testLeftShoulder]
     ])
-    let found: FoundManager | undefined
-    let first = true
-    while (true) {
-        if (first) {
-            first = false
-            const [[name, test]] = leftHandTests
-            found = target.findFirst(test)
-            if (!found) break
-            map.set(name, found)
-            leftHandTests.delete(name)
-            if (!leftHandTests.size) break
-            continue
-        }
-        found = found?.parentNode
-        if (!found) break
-        for (const [name, test] of leftHandTests) {
-            if (!test(found.name!)) continue
-            map.set(name, found)
-            leftHandTests.delete(name)
-            if (!leftHandTests.size) break
-            break
-        }
-    }
-
-    console.log(map)
+    parseChain(map, target, [
+        ["rightHand", testRightHand],
+        ["rightForeArm", testRightForeArm],
+        ["rightArm", testRightArm],
+        ["rightShoulder", testRightShoulder]
+    ])
+    return map
 })
