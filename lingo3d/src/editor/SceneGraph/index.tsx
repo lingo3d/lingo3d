982f597d37153a2cbe7ba69fafc9c58dcdbf1003
lingosandi <lai@lingo3d.com>
2023-07-16 19:50:17 +0800

    updates

--- lingo3d/src/editor/SceneGraph/index.tsx
+++ lingo3d/src/editor/SceneGraph/index.tsx
@@ -8,14 +8,19 @@ import mergeRefs from "../hooks/mergeRefs"
 import { enableHotKeysOnElement } from "../../engine/hotkeys"
 import { toggleRightClick } from "../../engine/mouse"
 import { sceneGraphWidthSignal } from "../signals/sizeSignals"
+import useSyncState from "../hooks/useSyncState"
+import { getFileCurrent } from "../../states/useFileCurrent"
+import getUUID from "../../memo/getUUID"
 
 const SceneGraph = () => {
     useInitCSS()
     useInitEditor()
+    const fileCurrent = useSyncState(getFileCurrent)
 
     return (
         <>
             <div
+                key={fileCurrent && getUUID(fileCurrent)}
                 ref={mergeRefs(stopPropagation, enableHotKeysOnElement)}
                 className="lingo3d-ui lingo3d-bg lingo3d-scenegraph"
                 onClick={() => emitSelectionTarget(undefined)}
